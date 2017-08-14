84415327f2f0c5e56512c83f47ededf7a8dcd258
Jonathan Lifflander <jliffla@sandia.gov>
2017-08-14 13:29:18 -0700

    remove old commented code

--- message.h
+++ message.h
@@ -7,22 +7,6 @@
 
 namespace runtime {
 
-// template <typename T>
-// struct Message {
-//   using envelope_t = Envelope;
-//   using user_type_t = T;
-
-//   envelope_t env;
-//   user_type_t user_data;
-
-//   Message() = default;
-
-//   Message(
-//     envelope_t&& in_env, user_type_t&& in_user_data
-//   ) : env(std::move(in_env)), user_data(std::move(in_user_data))
-//   { }
-// };
-
 struct Message {
   using envelope_t = Envelope;
 
