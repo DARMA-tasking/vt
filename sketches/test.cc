283092ba27d7a860ebb0d0f192e687de77d12685
Jonathan Lifflander <jliffla@sandia.gov>
2017-09-02 16:01:36 -0700

    sketches: move small sketches to separate directory

---
+++ sketches/test.cc
@@ -0,0 +1,117 @@
+
+#include "transport.h"
+#include <cstdlib>
+
+using namespace runtime;
+
+struct ParticleCollection : runtime::VirtualCollectionContext {
+  std::vector<Particle> particles;
+
+  node_t initial_map_to_node(index i) {
+    
+  }
+};
+
+struct MainContext : runtime::VirtualContext {
+  int some_info;
+};
+
+void initialize_particles(
+  runtime::BaseMessage* in_msg, IndexedVirtualContext* virtual_ctx
+) {
+  // adds ome parties to virtual_ctx
+}
+
+void main_function() {
+  auto collection_proxy = the_virtual->register_new_collection<ParticleCollection>(10000);
+  the_virtual->broadcast_msg(collection_proxy, initialize_particles_handles);
+
+  auto collection_proxy_elem_t = the_virtual->get_index(collection_proxy, index);
+  the_virtual->migrate_to_node(collection_proxy_elem_t, 5);
+}
+
+
+// int main(int argc, char** argv) {
+//   CollectiveOps::initialize_context(argc, argv);
+//   CollectiveOps::initialize_runtime();
+
+//   hello_world_han = CollectiveOps::register_handler(hello_world);
+
+//   auto const& my_node = the_context->get_node();
+//   auto const& num_nodes = the_context->get_num_nodes();
+
+//   if (num_nodes == 1) {
+//     fprintf(stderr, "Please run with at least two ranks!\n");
+//     fprintf(stderr, "\t mpirun-mpich-clang -n 2 %s\n", argv[0]);
+//     exit(1);
+//   }
+
+//   if (my_node == 0) {
+//     HelloMsg* msg = new HelloMsg(my_node);
+//     the_msg->broadcast_msg(hello_world_han, msg, [=]{ delete msg; });
+
+//     // Example of  to use a system managed message with transfer of control
+//     // HelloMsg* msg = make_shared_message<HelloMsg>(my_node);
+//     // the_msg->broadcast_msg(hello_world_han, msg);
+//   }
+
+//   while (1) {
+//     the_msg->scheduler();
+//   }
+
+//   return 0;
+// }
+
+struct VirtualContextCollection {
+
+};
+
+void my_register_virtual_context_collection() {
+  vc_proxy_collection_t hello_proxy = the_virtual->make_vc_collection_collective(1000);
+
+  the_virtual->send_msg(hello_proxy, virtual_han, msg);
+}
+
+struct VirtualContextManager {
+  void
+  send_msg(..);
+
+private:
+  std::unordered_map<vc_proxy_t, VirtualContext*> local_contexts;
+};
+
+struct VirtualContext {
+  vc_proxy_t context_proxy;
+};
+
+struct MyHelloContext : runtime::VirtualContext {
+  int some_info;
+};
+
+static void hello_world_virtual(runtime::BaseMessage* in_msg, VirtualContext* virtual_ctx) {
+  HelloMsg& msg = *static_cast<HelloMsg*>(in_msg);
+  MyHelloContext& hello_context = *static_cast<MyHelloContext*>(virtual_ctx);
+
+  hello_context->some_info++;
+}
+
+
+void my_register_virtual_context() {
+  MyHelloContext* hello_context = new MyHelloContext();
+  vc_proxy_t hello_proxy = the_virtual->register_virtual_context(hello_context);
+  vc_proxy_handler_t virtual_han = the_virtual->register_virtual_handler(hello_world_virtual);
+  HelloMsg* msg = new HelloMsg(my_node);
+
+  the_virtual->send_msg(hello_proxy, virtual_han, msg);
+
+
+  // MyHelloContext* hello_context = new MyHelloContext();
+  // vc_proxy_t hello_proxy = the_virtual->make_virtual_context<MyHelloContext>(/*construct args*/);
+
+  // MyHelloContext* hello_context = new MyHelloContext();
+  // vc_proxy_t hello_proxy = the_virtual->make_virtual_context<MyHelloContext>(8);
+
+  //the_msg->send_msg(10, handler, msg);
+
+  // vc_proxy_t: 64-bits: (32 id) (16 node) (few other control bits)
+}
---
+++ sketches/test_program1.cc
@@ -0,0 +1,131 @@
+#include "transport.h"
+
+#include <iostream>
+#include <cstdio>
+
+using namespace runtime;
+
+node_t this_node = -1;
+node_t num_nodes = -1;
+handler_t test_msg_han = 0;
+handler_t test_msg_han2 = 0;
+epoch_t test_epoch = -1;
+
+struct TestMsg : runtime::Message {
+  int val;
+  int val2;
+  event_t event;
+
+  TestMsg(int const& in_val, int const& in_val2, event_t const& in_event)
+    : val(in_val), val2(in_val2), event(in_event)
+  { }
+};
+
+void handle_test_msg(runtime::Message* in_msg) {
+}
+
+void send_to_neighbor() {
+  TestMsg* msg = new TestMsg(this_node, num_nodes, -1);
+
+  int const next = this_node+1 < num_nodes ? this_node+1 : 0;
+
+  //the_msg->set_epoch_message(msg, test_epoch);
+
+  event_t evt = the_msg->send_msg(next, test_msg_han, msg, [=]{
+    //std::cout << "deleting msg" << std::endl;
+    delete msg;
+  });
+
+  TestMsg* msg2 = new TestMsg(this_node, num_nodes, evt);
+
+  event_t evt2 = the_msg->send_msg(next, test_msg_han2, msg2, [=]{
+    //std::cout << "deleting msg" << std::endl;
+    delete msg2;
+  });
+
+  auto const& own_node = the_event->get_owning_node(evt);
+
+  printf(
+    "this_node=%d, event_id=%lld, own_node=%d\n",
+    this_node, evt, own_node
+  );
+
+  // std::cout << "this_node=" << this_node << ", "
+  //           << "event_id=" << evt << ", "
+  //           << "own_node=" << own_node
+  //           << std::endl;
+}
+
+int main(int argc, char** argv) {
+  CollectiveOps::initialize_context(argc, argv);
+  CollectiveOps::initialize_runtime();
+
+  this_node = the_context->get_node();
+  num_nodes = the_context->get_num_nodes();
+
+  std::cout << "this_node=" << this_node << std::endl;
+
+  printf("sizeof(Envelope)=%ld\n", sizeof(Envelope));
+  printf("sizeof(EpochEnvelope)=%ld\n", sizeof(EpochEnvelope));
+  printf("sizeof(EpochTagEnvelope)=%ld\n", sizeof(EpochTagEnvelope));
+
+  printf("%d: calling wait_unnamed_barrier\n", this_node);
+  the_barrier->barrier();
+  printf("%d: out of wait_unnamed_barrier\n", this_node);
+
+  printf("%d: calling cont_unnamed_barrier\n", this_node);
+  the_barrier->barrier_then([=]{
+    printf("%d: out of cont_unnamed_barrier\n", this_node);
+  });
+
+  //test_msg_han = CollectiveOps::register_handler(handle_test_msg);
+
+  // test_epoch = the_term->new_epoch();
+  // the_term->attach_epoch_term_action(test_epoch, [=]{
+  //   printf(
+  //     "%d: EPOCH: finished: test_epoch=%d\n",
+  //     the_context->get_node(), test_epoch
+  //   );
+  // });
+
+  test_msg_han = CollectiveOps::register_handler([](runtime::BaseMessage* in_msg){
+    TestMsg& msg = *static_cast<TestMsg*>(in_msg);
+
+    printf(
+      "this_node=%d, from_node=%d, num_nodes=%d\n",
+      this_node, msg.val, msg.val2
+    );
+
+    if (this_node != 0) {
+      send_to_neighbor();
+    }
+  });
+
+  test_msg_han2 = CollectiveOps::register_handler([](runtime::BaseMessage* in_msg){
+    TestMsg& msg = *static_cast<TestMsg*>(in_msg);
+
+    printf(
+      "this_node=%d, evt=%lld, owner=%d\n",
+      this_node, msg.event, the_event->get_owning_node(msg.event)
+    );
+
+    the_event->attach_action(msg.event, [=]{
+      printf("triggering remote event\n");
+    });
+  });
+
+  if (this_node == 0) {
+    send_to_neighbor();
+
+    TestMsg* msg = new TestMsg(this_node, num_nodes, -1);
+
+    // the_msg->broadcast_msg(test_msg_han, msg, [=]{
+    //   //std::cout << "deleting msg" << std::endl;
+    //   delete msg;
+    // });
+  }
+
+  while (1) {
+    the_msg->scheduler();
+  }
+}
