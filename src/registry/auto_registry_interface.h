fda7f210fdbe3d2fda92c366fb23652d3a05be5d
Jonathan Lifflander <jliffla@sandia.gov>
2017-09-02 19:20:38 -0700

    auto-registry: fix incorrect interface

--- src/registry/auto_registry_interface.h
+++ src/registry/auto_registry_interface.h
@@ -14,7 +14,7 @@ handler_t make_auto_handler(MessageT* const msg);
 template <typename T, T value>
 handler_t make_auto_handler();
 
-template <typename T>
+template <typename FunctorT, typename MessageT>
 handler_t make_auto_handler_functor();
 
 auto_active_t get_auto_handler(handler_t const& handler);
