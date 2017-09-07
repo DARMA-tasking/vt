4257ccea633d88d83dab8cda1e3f77eece626f51
Jonathan Lifflander <jliffla@sandia.gov>
2017-09-07 09:48:59 -0700

    build script: add in compiler option

--- build_vt.sh
+++ build_vt.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/env bash
 
-if test $# -ne 1
+if test $# -lt 1
 then
-    echo "usage $0 <build-mode>"
+    echo "usage $0 <build-mode> <compiler>"
     exit 1;
 fi
 
-BUILD_MODE=$1
+build_mode=$1
 
-echo "Building virtual transport layer mode=$BUILD_MODE"
+if test $# -gt 1
+then
+    compiler=$2
+else
+    compiler="clang"
+fi
+
+echo "Building virtual transport layer mode=$build_mode"
 
 SOURCE_BASE_DIR=../virtual-transport/
 MAKE_VERBOSE=off
 
-CXX_COMPILER=mpic++-mpich-clang39 #/opt/local/bin/mpic++-mpich-devel-gcc6
-CC_COMPILER=mpicc-mpich-clang39 #/opt/local/bin/mpicc-mpich-devel-gcc6
+if test $compiler = "clang"
+then
+    CXX_COMPILER=mpic++-mpich-clang39
+    CC_COMPILER=mpicc-mpich-clang39
+else
+    CXX_COMPILER=mpic++-mpich-devel-gcc6
+    CC_COMPILER=mpicc-mpich-devel-gcc6
+fi
 
 cmake ${SOURCE_BASE_DIR} \
       -DCMAKE_INSTALL_PREFIX=`pwd` \
       -DCMAKE_CXX_FLAGS="-std=c++1y" \
-      -DCMAKE_BUILD_TYPE=${BUILD_MODE} \
+      -DCMAKE_BUILD_TYPE=${build_mode} \
       -DCMAKE_VERBOSE_MAKEFILE:BOOL=$MAKE_VERBOSE \
       -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
       -DCMAKE_C_COMPILER=${CC_COMPILER}
