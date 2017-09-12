b6fa48d79911eb240a3628f7b4b7d057169bddb7
Jonathan Lifflander <jliffla@sandia.gov>
2017-09-11 19:52:38 -0700

    trace: make trace_event.h conform to style, add in impl file: trace_event.cc

---
+++ src/trace/trace_event.cc
@@ -0,0 +1,54 @@
+
+#include "trace_event.h"
+
+#include <string>
+
+namespace runtime { namespace trace {
+
+EventClass::EventClass(std::string const& in_event)
+  : event(in_event)
+{
+  auto const& event_hash =  std::hash<std::string>{}(in_event);
+  this_event = event_hash;
+}
+
+TraceEntryIDType EventClass::get_event_id() const {
+  return this_event;
+}
+
+TraceEntryIDType EventClass::get_event_seq_id() const {
+  return this_event_seq;
+}
+
+std::string EventClass::get_event_name() const {
+  return event;
+}
+
+void EventClass::set_event_seq(TraceEntryIDType const& seq) {
+  this_event_seq = seq;
+}
+
+TraceEntryIDType EventClass::get_event_seq() const {
+  return this_event_seq;
+}
+
+Event::Event(std::string const& in_event, TraceEntryIDType const& in_event_type)
+  : EventClass(in_event), this_event_type(in_event_type)
+{
+  auto const& event_hash =  std::hash<std::string>{}(in_event);
+  this_event_type = event_hash;
+}
+
+TraceEntryIDType Event::get_event_type_id() const {
+  return this_event_type;
+}
+
+void Event::set_event_type_seq(TraceEntryIDType const& seq) {
+  this_event_type_seq = seq;
+}
+
+TraceEntryIDType Event::get_event_type_seq() const {
+  return this_event_type_seq;
+}
+
+}} //end namespace runtime::trace
--- src/trace/trace_event.h
+++ src/trace/trace_event.h
@@ -13,76 +13,33 @@
 namespace runtime { namespace trace {
 
 struct EventClass {
-  EventClass(std::string const& in_event)
-    : event(in_event)
-  {
-    auto const& event_hash =  std::hash<std::string>{}(in_event);
-    this_event = event_hash;
-  }
-
+  EventClass(std::string const& in_event);
   EventClass(EventClass const&) = default;
 
-  TraceEntryIDType
-  get_event_id() const {
-    return this_event;
-  }
+  TraceEntryIDType get_event_id() const;
+  TraceEntryIDType get_event_seq_id() const;
 
-  TraceEntryIDType
-  get_event_seq_id() const {
-    return this_event_seq;
-  }
-
-  std::string
-  get_event_name() const {
-    return event;
-  }
-
-  void
-  set_event_seq(TraceEntryIDType const& seq) {
-    this_event_seq = seq;
-  }
-
-  TraceEntryIDType
-  get_event_seq() const {
-    return this_event_seq;
-  }
+  std::string get_event_name() const;
+  void set_event_seq(TraceEntryIDType const& seq);
+  TraceEntryIDType get_event_seq() const;
 
 private:
   TraceEntryIDType this_event = no_trace_entry_id;
-
   TraceEntryIDType this_event_seq = no_trace_entry_id;
 
   std::string event;
 };
 
 struct Event : EventClass {
-  Event(std::string const& in_event, TraceEntryIDType const& in_event_type)
-    : EventClass(in_event), this_event_type(in_event_type)
-  {
-    auto const& event_hash =  std::hash<std::string>{}(in_event);
-    this_event_type = event_hash;
-  }
-
+  Event(std::string const& in_event, TraceEntryIDType const& in_event_type);
   Event(Event const&) = default;
 
-  TraceEntryIDType
-  get_event_type_id() const {
-    return this_event_type;
-  }
-
-  void
-  set_event_type_seq(TraceEntryIDType const& seq) {
-    this_event_type_seq = seq;
-  }
-
-  TraceEntryIDType
-  get_event_type_seq() const {
-    return this_event_type_seq;
-  }
+  TraceEntryIDType get_event_type_id() const;
+  void set_event_type_seq(TraceEntryIDType const& seq);
+  TraceEntryIDType get_event_type_seq() const;
 
 private:
   TraceEntryIDType this_event_type = no_trace_entry_id;
-
   TraceEntryIDType this_event_type_seq = no_trace_entry_id;
 };
 
