0616596e70387cbc1d84a81e348a24e951d4331e
Aram Markosyan <aram.math@gmail.com>
2017-09-14 08:02:39 -0700

    meld: update names

---
+++ src/tpl/meld/meld_meldheaders.h
@@ -0,0 +1,13 @@
+
+#if !defined INCLUDED_TPL_MELD_MELD
+#define INCLUDED_TPL_MELD_MELD
+
+// Meld is a simple macro library I've written to enable easy customization of
+// backend configurations, such as detailed debugging printing
+
+#include "meld_combinators.h"
+#include "meld_control.h"
+#include "meld_compare.h"
+#include "meld_map.h"
+
+#endif
