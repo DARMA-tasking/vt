a600fea62cc41e83c80be7549bbd5d95e26a1771
Aram Markosyan <aram.math@gmail.com>
2017-09-14 07:28:33 -0700

    meld: move to tpl

---
+++ src/tpl/meld/meld.h
@@ -0,0 +1,13 @@
+
+#if !defined INCLUDED_TPL_MELD_MELD
+#define INCLUDED_TPL_MELD_MELD
+
+// Meld is a simple macro library I've written to enable easy customization of
+// backend configurations, such as detailed debugging printing
+
+#include "combinators.h"
+#include "control.h"
+#include "compare.h"
+#include "map.h"
+
+#endif
