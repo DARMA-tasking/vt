c230915bc823c2122d1722062c99f74916c7295a
Jonathan Lifflander <jliffla@sandia.gov>
2017-09-14 21:14:52 -0700

    location_test: improve test to have several entities and node homes

--- examples/location_test.cc
+++ examples/location_test.cc
@@ -8,9 +8,10 @@ using EntityType = int32_t;
 
 struct EntityMsg : vt::Message {
   EntityType entity;
+  NodeType home;
 
-  EntityMsg(int const& in_entity)
-    : Message(), entity(in_entity)
+  EntityMsg(EntityType const& in_entity, NodeType const& in_home)
+    : Message(), entity(in_entity), home(in_home)
   { }
 };
 
@@ -19,8 +20,9 @@ static void entityTestHandler(EntityMsg* msg) {
     "%d: entityTestHandler entity=%d\n", theContext->getNode(), msg->entity
   );
 
-  theLocMan->virtual_loc->getLocation(msg->entity, 0, [](NodeType node){
+  theLocMan->virtual_loc->getLocation(msg->entity, msg->home, [](NodeType node){
     printf("%d: entityTestHandler: location=%d\n", theContext->getNode(), node);
+    theLocMan->virtual_loc->printCurrentCache();
   });
 }
 
@@ -37,15 +39,26 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  int32_t entity = 10;
+  int32_t entity = 10, entity2 = 11, entity3 = 12;
 
   if (my_node == 0) {
     theLocMan->virtual_loc->registerEntity(entity);
+    theLocMan->virtual_loc->registerEntity(entity2);
+  } else if (my_node == 1) {
+    theLocMan->virtual_loc->registerEntity(entity3);
   }
 
   if (my_node == 0) {
-    EntityMsg* msg = new EntityMsg(entity);
-    theMsg->broadcastMsg<EntityMsg, entityTestHandler>(msg, [=]{ delete msg; });
+    theMsg->broadcastMsg<EntityMsg, entityTestHandler>(
+      makeSharedMessage<EntityMsg>(entity, my_node)
+    );
+    theMsg->broadcastMsg<EntityMsg, entityTestHandler>(
+      makeSharedMessage<EntityMsg>(entity2, my_node)
+    );
+  } else if (my_node == 1) {
+    theMsg->broadcastMsg<EntityMsg, entityTestHandler>(
+      makeSharedMessage<EntityMsg>(entity3, my_node)
+    );
   }
 
   while (1) {
