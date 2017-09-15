33222b87e4f2f9b28e471c27937eb92bcc6850e6
Jonathan Lifflander <jliffla@sandia.gov>
2017-09-14 21:10:40 -0700

    broadcast_test: improve new test for broadcast

--- examples/broadcast_test.cc
+++ examples/broadcast_test.cc
@@ -4,24 +4,30 @@
 
 using namespace vt;
 
-static NodeType my_node = uninitialized_destination;
+#define BCAST_DEBUG 0
 
-struct TestMsg : vt::Message {
+static constexpr int32_t const num_bcasts = 4;
+static NodeType my_node = uninitialized_destination;
+static int32_t count = 0;
+
+struct Msg : vt::Message {
   NodeType broot;
 
-  TestMsg(NodeType const& in_broot) : Message(), broot(in_broot) { }
+  Msg(NodeType const& in_broot) : Message(), broot(in_broot) { }
 };
 
-static void bcastTest(TestMsg* msg) {
+static void bcastTest(Msg* msg) {
   auto const& root = msg->broot;
 
-  printf(
-    "%d: bcastTestHandler root=%d\n", theContext->getNode(), msg->broot
-  );
+  #if BCAST_DEBUG
+  printf("%d: bcastTestHandler root=%d\n", theContext->getNode(), msg->broot);
+  #endif
 
   assert(
     root != my_node and "Broadcast should deliver to all but this node"
   );
+
+  count++;
 }
 
 int main(int argc, char** argv) {
@@ -42,8 +48,21 @@ int main(int argc, char** argv) {
     from_node = atoi(argv[1]);
   }
 
+  int32_t const expected = num_bcasts *
+    (from_node == uninitialized_destination ? theContext->getNumNodes() - 1 : (
+      from_node == my_node ? 0 : 1
+    ));
+
+  theTerm->attachGlobalTermAction([=]{
+    printf("[%d] verify: count=%d, expected=%d\n", my_node, count, expected);
+    assert(count == expected);
+  });
+
   if (from_node == uninitialized_destination or from_node == my_node) {
-    theMsg->broadcastMsg<TestMsg, bcastTest>(makeSharedMessage<TestMsg>(my_node));
+    printf("[%d] broadcast_test: broadcasting %d times\n", my_node, num_bcasts);
+    for (int i = 0; i < num_bcasts; i++) {
+      theMsg->broadcastMsg<Msg, bcastTest>(makeSharedMessage<Msg>(my_node));
+    }
   }
 
   while (1) {
