ac0bdb265128ad3275b79430df47f091dc077686
Jonathan Lifflander <jliffla@sandia.gov>
2017-09-15 22:52:18 -0700

    pool: start implementing some basic unit test

---
+++ tests/unit/pool/test_pool.cc
@@ -0,0 +1,78 @@
+
+#include <cstring>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "test_harness.h"
+
+#include "transport.h"
+
+class TestPool : public TestHarness {
+  virtual void SetUp() {
+    TestHarness::SetUp();
+  }
+
+  virtual void TearDown() {
+    TestHarness::TearDown();
+  }
+};
+
+TEST_F(TestPool, pool_alloc) {
+  using namespace vt;
+
+  using CharType = unsigned char;
+
+  static constexpr size_t const max_bytes = 16384;
+  static constexpr CharType const init_val = 'z';
+
+  std::unique_ptr<pool::Pool> testPool = std::make_unique<pool::Pool>();
+
+  for (size_t cur_bytes = 1; cur_bytes < max_bytes; cur_bytes *= 2) {
+    void* ptr = testPool->alloc(cur_bytes);
+    std::memset(ptr, init_val, cur_bytes);
+    //printf("alloc %ld bytes, ptr=%p\n", cur_bytes, ptr);
+    ASSERT_TRUE(ptr != nullptr);
+    for (size_t i = 0; i < cur_bytes; i++) {
+      ASSERT_TRUE(static_cast<CharType*>(ptr)[i] == init_val);
+    }
+    testPool->dealloc(ptr);
+  }
+}
+
+namespace pool_message_alloc_test {
+
+static constexpr int64_t const min_bytes = 1;
+static constexpr int64_t const max_bytes = 16384;
+
+template <int64_t num_bytes>
+struct TestMsg : vt::ShortMessage {
+  std::array<char, num_bytes> _;
+  TestMsg() : vt::ShortMessage() { }
+};
+
+template <int64_t num_bytes>
+static void testPoolFun(TestMsg<num_bytes>* prev_msg) {
+  printf("testPoolFun: num_bytes=%lld\n", num_bytes);
+
+  auto msg = new TestMsg<num_bytes * 2>();
+  testPoolFun<num_bytes * 2>(msg);
+  delete msg;
+}
+
+template <>
+void testPoolFun<max_bytes>(TestMsg<max_bytes>* msg) {
+}
+
+} // end namespace pool_message_alloc_test
+
+TEST_F(TestPool, pool_message_alloc) {
+  using namespace vt;
+  using namespace pool_message_alloc_test;
+
+  std::unique_ptr<pool::Pool> testPool = std::make_unique<pool::Pool>();
+
+  auto msg = new TestMsg<min_bytes>();
+  testPoolFun<min_bytes>(msg);
+  delete msg;
+}
