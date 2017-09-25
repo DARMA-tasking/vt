25e66e05d1d9974e06e6d0ed6202fdb4fcee1760
Aram Markosyan <aram.math@gmail.com>
2017-09-25 11:21:24 -0700

    vrtcontextmanager: tests are passing

--- tests/unit/vrt/vrt_contextmanager.t.cc
+++ tests/unit/vrt/vrt_contextmanager.t.cc
@@ -28,30 +28,40 @@ struct HelloVrtContext : vt::vrt::VrtContext {
 TEST_F(TestVrtContextManager, Construction_and_API) {
   using namespace vt;
 
-  vrt::VrtContextManager vrtM;
-  EXPECT_EQ(vrtM.getNode(), theContext->getNode());
-  EXPECT_EQ(vrtM.getCurrentIdent(), 0);
+  EXPECT_EQ(theVrtCManager->getNode(), theContext->getNode());
+  EXPECT_EQ(theVrtCManager->getCurrentIdent(), 0);
 
-  auto vrtM_proxy = vrtM.newVrtContext();
-  EXPECT_EQ(vrtM.getCurrentIdent(), 1);
+  auto vrtc1 = theVrtCManager->constructVrtContext<HelloVrtContext>(10);
+  EXPECT_EQ(theVrtCManager->getCurrentIdent(), 1);
 
-  auto vrtH = HelloVrtContext(20);
-  vrtH.setIsMigratable(true);
-  vrtH.setIsCollection(true);
-  EXPECT_EQ(vrtH.isCollection(), true);
-  EXPECT_EQ(vrtH.isMigratable(), true);
+  auto temp1 = theVrtCManager->getVrtContextByID(vrtc1);
+  auto hello1 = static_cast<HelloVrtContext*>(temp1);
 
-  auto vrtH_proxy = vrtM.newVrtContext(&vrtH);
-  EXPECT_EQ(vrtH.isCollection(), true);
-  EXPECT_EQ(vrtH.isMigratable(), true);
-  EXPECT_EQ(vrtH.getVrtContextNode(), theContext->getNode());
-  EXPECT_EQ(vrtH.getVrtContextIdentifier(), 1);
+  auto temp2 = theVrtCManager->getVrtContextByID(vrtc1);
+  auto hello2 = static_cast<HelloVrtContext*>(temp2);
 
-  EXPECT_EQ(vrtH_proxy.isCollection(), true);
-  EXPECT_EQ(vrtH_proxy.isMigratable(), true);
-  EXPECT_EQ(vrtH_proxy.getVrtContextNode(), theContext->getNode());
-  EXPECT_EQ(vrtH_proxy.getVrtContextIdentifier(), 1);
+  EXPECT_EQ(hello1->from, 10);
+  EXPECT_EQ(hello1->getVrtContextNode(), theVrtCManager->getNode());
 
-  EXPECT_EQ(vrtH.from, 20);
+  hello1->setIsCollection(true);
+  EXPECT_EQ(hello2->isCollection(), true);
+
+  hello2->setIsCollection(false);
+  EXPECT_EQ(hello1->isCollection(), false);
+
+  ////////////////////////////////////////////////////////////////////
+
+  theVrtCManager->destroyVrtContextByID(vrtc1);
+  EXPECT_EQ(theVrtCManager->getVrtContextByID(vrtc1), nullptr);
+
+  auto vrtc2 = theVrtCManager->constructVrtContext<HelloVrtContext>(20);
+  auto temp21 = theVrtCManager->getVrtContextByID(vrtc2);
+  auto hello21 = static_cast<HelloVrtContext*>(temp21);
+
+  auto vrtc3 = 20;
+  EXPECT_EQ(theVrtCManager->getVrtContextByID(vrtc3), nullptr);
+
+  EXPECT_EQ(hello21->from, 20);
+  EXPECT_EQ(hello21->getVrtContextNode(), theVrtCManager->getNode());
 }
 
