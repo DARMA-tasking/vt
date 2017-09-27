954de6e32eb57aff1dae2635b0fb64299686f72a
Aram Markosyan <aram.math@gmail.com>
2017-09-27 07:21:53 -0700

    test: vrtContextProxy: add test

---
+++ tests/unit/context/context_vrtproxy.t.cc
@@ -0,0 +1,57 @@
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "test_harness.h"
+
+class TestVrtContextProxy : public TestHarness {
+  virtual void SetUp() {
+    TestHarness::SetUp();
+  }
+
+  virtual void TearDown() {
+    TestHarness::TearDown();
+  }
+};
+
+TEST_F(TestVrtContextProxy, Construction_AND_API) {
+  using namespace vt;
+  using namespace vt::vrt;
+
+  VrtContext_ProxyType proxy1 = VrtContextProxy::createNewProxy(200, 20);
+  EXPECT_EQ(VrtContextProxy::isCollection(proxy1), false);
+  EXPECT_EQ(VrtContextProxy::isMigratable(proxy1), false);
+  EXPECT_EQ(VrtContextProxy::getVrtContextNode(proxy1), 20);
+  EXPECT_EQ(VrtContextProxy::getVrtContextId(proxy1), 200);
+
+  VrtContextProxy::setIsCollection(proxy1, true);
+  EXPECT_EQ(VrtContextProxy::isCollection(proxy1), true);
+
+  VrtContextProxy::setIsMigratable(proxy1, true);
+  EXPECT_EQ(VrtContextProxy::isMigratable(proxy1), true);
+
+  VrtContextProxy::setVrtContextNode(proxy1, 2000);
+  EXPECT_EQ(VrtContextProxy::getVrtContextNode(proxy1), 2000);
+
+  VrtContextProxy::setVrtContextId(proxy1, 3000);
+  EXPECT_EQ(VrtContextProxy::getVrtContextId(proxy1), 3000);
+
+  EXPECT_EQ(VrtContextProxy::isCollection(proxy1), true);
+  EXPECT_EQ(VrtContextProxy::isMigratable(proxy1), true);
+  EXPECT_EQ(VrtContextProxy::getVrtContextNode(proxy1), 2000);
+  EXPECT_EQ(VrtContextProxy::getVrtContextId(proxy1), 3000);
+
+  VrtContext_ProxyType proxy2
+      = VrtContextProxy::createNewProxy(200, 20, true);
+  EXPECT_EQ(VrtContextProxy::isCollection(proxy2), true);
+  EXPECT_EQ(VrtContextProxy::isMigratable(proxy2), false);
+  EXPECT_EQ(VrtContextProxy::getVrtContextNode(proxy2), 20);
+  EXPECT_EQ(VrtContextProxy::getVrtContextId(proxy2), 200);
+
+  VrtContext_ProxyType proxy3 =
+      VrtContextProxy::createNewProxy(200, 20, true, true);
+  EXPECT_EQ(VrtContextProxy::isCollection(proxy3), true);
+  EXPECT_EQ(VrtContextProxy::isMigratable(proxy3), true);
+  EXPECT_EQ(VrtContextProxy::getVrtContextNode(proxy3), 20);
+  EXPECT_EQ(VrtContextProxy::getVrtContextId(proxy3), 200);
+}
