22d059cc75915777c34cdf8a51a74d41e618616c
Aram Markosyan <aram.math@gmail.com>
2017-09-27 07:21:23 -0700

    test: vrtContext: remove test
    Remove test, the API is changed

--- tests/unit/context/context_vrt.t.cc
+++ tests/unit/context/context_vrt.t.cc
@@ -16,41 +16,41 @@ class TestVrtContext : public TestHarness {
   }
 };
 
-
-TEST_F(TestVrtContext, Construction) {
-  using namespace vt;
-
-  vrt::VrtContext vrtc1(10);
-  EXPECT_EQ(vrtc1.isCollection(), false);
-  EXPECT_EQ(vrtc1.isMigratable(), false);
-  EXPECT_EQ(vrtc1.getVrtContextNode(), 10);
-
-  vrt::VrtContext vrtc2(100, false, true);
-  EXPECT_EQ(vrtc2.isCollection(), false);
-  EXPECT_EQ(vrtc2.isMigratable(), true);
-  EXPECT_EQ(vrtc2.getVrtContextNode(), 100);
-}
-
-
-TEST_F(TestVrtContext, public_API) {
-  using namespace vt;
-
-  vrt::VrtContext vrtc = vrt::VrtContext();
-  EXPECT_EQ(vrtc.isCollection(), false);
-  EXPECT_EQ(vrtc.isMigratable(), false);
-
-  vrtc.setVrtContextNode(100);
-  EXPECT_EQ(vrtc.isCollection(), false);
-  EXPECT_EQ(vrtc.isMigratable(), false);
-  EXPECT_EQ(vrtc.getVrtContextNode(), 100);
-
-  vrtc.setIsCollection(true);
-  EXPECT_EQ(vrtc.isCollection(), true);
-  EXPECT_EQ(vrtc.isMigratable(), false);
-  EXPECT_EQ(vrtc.getVrtContextNode(), 100);
-
-  vrtc.setIsMigratable(true);
-  EXPECT_EQ(vrtc.isCollection(), true);
-  EXPECT_EQ(vrtc.isMigratable(), true);
-  EXPECT_EQ(vrtc.getVrtContextNode(), 100);
-}
+//
+//TEST_F(TestVrtContext, Construction) {
+//  using namespace vt;
+//
+//  vrt::VrtContext vrtc1(10);
+//  EXPECT_EQ(vrtc1.isCollection(), false);
+//  EXPECT_EQ(vrtc1.isMigratable(), false);
+//  EXPECT_EQ(vrtc1.getVrtContextNode(), 10);
+//
+//  vrt::VrtContext vrtc2(100, false, true);
+//  EXPECT_EQ(vrtc2.isCollection(), false);
+//  EXPECT_EQ(vrtc2.isMigratable(), true);
+//  EXPECT_EQ(vrtc2.getVrtContextNode(), 100);
+//}
+//
+//
+//TEST_F(TestVrtContext, public_API) {
+//  using namespace vt;
+//
+//  vrt::VrtContext vrtc = vrt::VrtContext();
+//  EXPECT_EQ(vrtc.isCollection(), false);
+//  EXPECT_EQ(vrtc.isMigratable(), false);
+//
+//  vrtc.setVrtContextNode(100);
+//  EXPECT_EQ(vrtc.isCollection(), false);
+//  EXPECT_EQ(vrtc.isMigratable(), false);
+//  EXPECT_EQ(vrtc.getVrtContextNode(), 100);
+//
+//  vrtc.setIsCollection(true);
+//  EXPECT_EQ(vrtc.isCollection(), true);
+//  EXPECT_EQ(vrtc.isMigratable(), false);
+//  EXPECT_EQ(vrtc.getVrtContextNode(), 100);
+//
+//  vrtc.setIsMigratable(true);
+//  EXPECT_EQ(vrtc.isCollection(), true);
+//  EXPECT_EQ(vrtc.isMigratable(), true);
+//  EXPECT_EQ(vrtc.getVrtContextNode(), 100);
+//}
