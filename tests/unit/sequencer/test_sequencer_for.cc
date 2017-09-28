02a094860bb767f65cd3f6f8aa1e96db74291dfb
Jonathan Lifflander <jliffla@sandia.gov>
2017-09-28 00:12:14 -0700

    unit test: add test for sequenced for loop

---
+++ tests/unit/sequencer/test_sequencer_for.cc
@@ -0,0 +1,72 @@
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <cstdint>
+
+#include "test_parallel_harness.h"
+#include "data_message.h"
+
+#include "transport.h"
+
+namespace vt { namespace tests { namespace unit {
+
+using namespace vt;
+using namespace vt::tests::unit;
+
+struct TestSequencerFor : TestParallelHarness {
+  using TestMsg = TestStaticBytesNormalMsg<4>;
+  using OrderType = uint32_t;
+
+  SEQUENCE_REGISTER_HANDLER(TestSequencerFor::TestMsg, testSeqForHan);
+
+  static void testSeqForFn(SeqType const& seq_id) {
+    static std::atomic<OrderType> seq_ordering_{};
+
+    #if DEBUG_TEST_HARNESS_PRINT
+      printf("testSeqForFn seq_id=%d\n", seq_id);
+    #endif
+
+    if (seq_id == -1) {
+      EXPECT_EQ(seq_ordering_++, 11);
+      return;
+    }
+
+    EXPECT_EQ(seq_ordering_++, 0);
+
+    theSeq->for_loop(0, 10, 1, [](vt::seq::ForIndex i) {
+      theSeq->wait_closure<TestMsg, testSeqForHan>(no_tag, [=](TestMsg* msg){
+        #if DEBUG_TEST_HARNESS_PRINT
+          printf("testSeqForFn running wait\n");
+        #endif
+
+        EXPECT_EQ(seq_ordering_++, i+1);
+      });
+    });
+  }
+};
+
+TEST_F(TestSequencerFor, test_for) {
+  auto const& my_node = theContext->getNode();
+
+  #if DEBUG_TEST_HARNESS_PRINT
+    printf("test_seq_handler: node=%d\n", my_node);
+  #endif
+
+  if (my_node == 0) {
+    SeqType const& seq_id = theSeq->nextSeq();
+    theSeq->sequenced(seq_id, testSeqForFn);
+
+    for (int i = 0; i < 10; i++) {
+      theMsg->sendMsg<TestMsg, testSeqForHan>(
+        my_node, makeSharedMessage<TestMsg>()
+      );
+    }
+
+    theTerm->attachGlobalTermAction([=]{
+      testSeqForFn(-1);
+    });
+  }
+}
+
+}}} // end namespace vt::tests::unit
