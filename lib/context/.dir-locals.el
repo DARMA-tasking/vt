f649b1da9ba7e33b1cf90d34d534f8517042359c
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-03 04:37:08 -0700

    ULTs: #24: improve build process

---
+++ lib/context/.dir-locals.el
@@ -0,0 +1,11 @@
+((nil
+  (cmake-ide-project-dir . "/Users/jliffla/codes/vt/virtual-transport/lib/context")
+  (cmake-ide-build-dir . "/Users/jliffla/codes/vt/virtual-transport/lib/context-build")
+  (cmake-ide-cmake-opts . " -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER=clang-mp-3.9 -DCMAKE_CXX_COMPILER=clang++-mp-3.9")
+))
+
+;; (concat " -DCMAKE_BUILD_TYPE=debug "
+;;         " -DGTEST_DIR=/Users/jliffla/codes/gtest/gtest-build"
+;;         " -DCMAKE_C_COMPILER=mpicc-mpich-clang39"
+;;         " -DCMAKE_CXX_COMPILER=mpic++-mpich-clang39"
+;; )
