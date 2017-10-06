c07663593a45e503fa2a0bbdeaa47a9c5a820e2f
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-06 16:47:17 -0700

    ULTs: #24: add in general header for context library

---
+++ lib/context/src/context.h
@@ -0,0 +1,11 @@
+
+#if !defined __RUNTIME_TRANSPORT_CONTEXT__
+#define __RUNTIME_TRANSPORT_CONTEXT__
+
+#include "context_wrapper.h"
+#include "context_functor.h"
+#include "fcontext.h"
+#include "stack.h"
+#include "page.h"
+
+#endif /*__RUNTIME_TRANSPORT_CONTEXT__*/
