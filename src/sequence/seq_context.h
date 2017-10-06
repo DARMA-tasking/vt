f0054713a03975b9118b6e3b74b63bbd5cb4ebc6
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-06 16:09:44 -0700

    ULTs: #24: start adding suspendability to sequencer contexts

--- src/sequence/seq_context.h
+++ src/sequence/seq_context.h
@@ -12,15 +12,21 @@
 namespace vt { namespace seq {
 
 struct SeqContext {
-  SeqContext(SeqType const& in_seq_id, SeqNodePtrType in_node);
+  SeqContext(
+    SeqType const& in_seq_id, SeqNodePtrType in_node, bool is_suspendable = false
+  );
   SeqContext(SeqType const& in_seq_id);
   SeqContext(SeqContext const&) = delete;
 
   SeqType getSeq() const;
   SeqNodePtrType getNode() const;
   void setNode(SeqNodePtrType node);
+  bool isSuspendable() const;
+  void setSuspendable(bool const is_suspendable);
 
 private:
+  bool suspendable_ = false;
+
   SeqNodePtrType node_ = nullptr;
 
   SeqType seq_id = no_seq;
