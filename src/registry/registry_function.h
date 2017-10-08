01c732dcf47006ecbb248befd6a34a8d96e3628c
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-07 23:52:57 -0700

    active functions: add in comment explaining new naming scheme

--- src/registry/registry_function.h
+++ src/registry/registry_function.h
@@ -7,6 +7,42 @@
 
 namespace vt {
 
+/*******************************************************************************
+ *
+ *                          Active Function Naming Scheme
+ *
+ *******************************************************************************
+ *
+ *    - All registerable active function handlers start with "Active" and end
+ *      with "Type", since they are a type alias.
+ *
+ *    - If the active function is a pointer, it should end with `FnPtr' rather
+ *      than just `Fn'
+ *
+ *      -- Non-function pointer
+ *
+ *            using ActiveFnType = void(BaseMessage*);
+ *
+ *      -- Function pointer
+ *
+ *            using ActiveFnPtrType = void(*)(BaseMessage*);
+ *
+ *     - If the active function is typed, it should have `Typed' before the
+ *       Fn/FnPtr:
+ *
+ *      -- Typed function pointer
+ *
+ *            template <typename MessageT>
+ *            using ActiveTypedFnPtrType = void(*)(MessageT*);
+ *
+ *    - If the active function is a closure (can have state associated with
+ *      it beyond just a pointer), it should be post-fixed with `Closure'
+ *
+ *            using ActiveClosureFnType = std::function<void(BaseMessage*)>;
+ *
+ *******************************************************************************
+ */
+
 using ActiveClosureFnType = std::function<void(vt::BaseMessage*)>;
 using ActiveFnType = void(vt::BaseMessage *);
 using ActiveFnPtrType = void(*)(vt::BaseMessage *);
