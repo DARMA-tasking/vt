1d7fa90c3091c1182e6cd49a53cf1e3ae8e0d2fb
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-14 01:41:28 -0700

    serialization: #23: add unit test for serialized virtual active handlers

--- tests/unit/serialization/test_serialize_messenger_virtual.cc
+++ tests/unit/serialization/test_serialize_messenger_virtual.cc
@@ -16,31 +16,35 @@ static constexpr int const val1 = 20;
 static constexpr int const val2 = 21;
 static constexpr int const val3 = 22;
 
+struct TestCtx : ::vt::vrt::VirtualContext {
+  TestCtx() = default;
+};
+
 template <typename Tuple>
 struct DataMsg : ShortMessage {
   Tuple* tup = nullptr;
   DataMsg(Tuple* in_tup) : ShortMessage(), tup(in_tup) { }
 };
 
-struct TestSerialMessenger : TestParallelHarness {
+struct TestSerialMessengerVirtual : TestParallelHarness {
   using TestMsg = TestStaticBytesShortMsg<4>;
 
-  template <typename Tuple>
-  static void testHandler(DataMsg<Tuple>* msg) {
+  template <typename Tuple, typename VirtualContextT>
+  static void testHandler(DataMsg<Tuple>* msg, VirtualContextT* ctx) {
     EXPECT_EQ(std::get<0>(*msg->tup), val1);
     EXPECT_EQ(std::get<1>(*msg->tup)[0], val2);
     EXPECT_EQ(std::get<1>(*msg->tup)[1], val3);
   }
 };
 
-TEST_F(TestSerialMessenger, test_serial_messenger_1) {
+TEST_F(TestSerialMessengerVirtual, test_serial_messenger_1) {
   auto const& my_node = theContext->getNode();
 
   if (my_node == 0) {
     using TupleType = std::tuple<int, std::vector<int>>;
 
-    SerializedMessenger::sendSerialMsg<
-      DataMsg<TupleType>, testHandler<TupleType>
+    SerializedMessenger::sendSerialVirtualMsg<
+      TestCtx, DataMsg<TupleType>, testHandler<TupleType, TestCtx>
     >(1, TupleType{val1,std::vector<int>{val2,val3}});
   }
 }
