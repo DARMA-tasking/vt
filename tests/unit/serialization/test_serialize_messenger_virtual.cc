e62f0a28357752c312a2a7b32f4dade52ca3b2a1
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-14 11:22:39 -0700

    serialization: #23: improve other test

--- tests/unit/serialization/test_serialize_messenger_virtual.cc
+++ tests/unit/serialization/test_serialize_messenger_virtual.cc
@@ -14,7 +14,6 @@ using namespace vt::tests::unit;
 
 static constexpr int const val1 = 20;
 static constexpr int const val2 = 21;
-static constexpr int const val3 = 22;
 
 struct TestCtx : ::vt::vrt::VirtualContext {
   TestCtx() = default;
@@ -32,8 +31,7 @@ struct TestSerialMessengerVirtual : TestParallelHarness {
   template <typename Tuple, typename VirtualContextT>
   static void testHandler(DataMsg<Tuple>* msg, VirtualContextT* ctx) {
     EXPECT_EQ(std::get<0>(*msg->tup), val1);
-    EXPECT_EQ(std::get<1>(*msg->tup)[0], val2);
-    EXPECT_EQ(std::get<1>(*msg->tup)[1], val3);
+    EXPECT_EQ(std::get<1>(*msg->tup), val2);
   }
 };
 
@@ -41,11 +39,11 @@ TEST_F(TestSerialMessengerVirtual, test_serial_messenger_1) {
   auto const& my_node = theContext->getNode();
 
   if (my_node == 0) {
-    using TupleType = std::tuple<int, std::vector<int>>;
+    using TupleType = std::tuple<int, int>;
 
     SerializedMessenger::sendSerialVirtualMsg<
       TestCtx, DataMsg<TupleType>, testHandler<TupleType, TestCtx>
-    >(1, TupleType{val1,std::vector<int>{val2,val3}});
+    >(1, TupleType{val1,val2});
   }
 }
 
