72ad18f0df76acd3f416246cdc493a53ec6027f3
Jonathan Lifflander <jliffla@sandia.gov>
2018-01-24 15:40:59 -0800

    put: implement simple message put ptr

---
+++ src/messaging/put_message.h
@@ -0,0 +1,26 @@
+
+#if !defined INCLUDED_MESSAGING_PUT_MESSAGE_H
+#define INCLUDED_MESSAGING_PUT_MESSAGE_H
+
+#include "config.h"
+#include "messaging/envelope.h"
+#include "messaging/payload.h"
+#include "messaging/message.h"
+
+namespace vt {
+
+template <typename MessageT>
+struct PutMessageComponent : MessageT {
+  void setPut(void* const ptr, size_t const size) {
+    envelopeSetPutPtr(MessageT::env, ptr, size);
+  }
+  void* getPut() {
+    return envelopeGetPutPtr(MessageT::env);
+  }
+};
+
+using PutMessage = PutMessageComponent<ActiveMessage<PutShortEnvelope>>;
+
+} /* end namespace vt */
+
+#endif /*INCLUDED_MESSAGING_PUT_MESSAGE_H*/
