128c24f4433729767f9e62976a218bad17fbaef8
Jonathan Lifflander <jliffla@sandia.gov>
2018-01-25 13:01:00 -0800

    group: builder for creating bit-pattern IDs

---
+++ src/group/group_headers.h
@@ -0,0 +1,9 @@
+
+#if !defined INCLUDED_GROUP_GROUP_HEADERS_H
+#define INCLUDED_GROUP_GROUP_HEADERS_H
+
+#include "config.h"
+#include "group/group_types.h"
+#include "group/group_id.h"
+
+#endif /*INCLUDED_GROUP_GROUP_HEADERS_H*/
---
+++ src/group/group_id.cc
@@ -0,0 +1,68 @@
+
+#include "config.h"
+#include "group/group_id.h"
+#include "utils/bits/bits_common.h"
+
+namespace vt { namespace group {
+
+/*static*/ GroupType GroupIDBuilder::createGroupID(
+  GroupIDType const& id, NodeType const& node, bool const& is_collective,
+  bool const& is_static
+) {
+  GroupType new_group = 0;
+
+  setIsCollective(new_group, is_collective);
+  setIsStatic(new_group, is_static);
+  setNode(new_group, node);
+  setID(new_group, id);
+
+  return new_group;
+}
+
+/*static*/ void GroupIDBuilder::setIsCollective(
+  GroupType& group, bool const& is_coll
+) {
+  BitPackerType::boolSetField<eGroupIDBits::Collective>(group, is_coll);
+}
+
+/*static*/ void GroupIDBuilder::setIsStatic(
+  GroupType& group, bool const& is_static
+) {
+  BitPackerType::boolSetField<eGroupIDBits::Static>(group, is_static);
+}
+
+/*static*/ void GroupIDBuilder::setNode(
+  GroupType& group, NodeType const& node
+) {
+  BitPackerType::setField<eGroupIDBits::Node, group_node_num_bits>(group, node);
+}
+
+/*static*/ void GroupIDBuilder::setID(
+  GroupType& group, GroupIDType const& id
+) {
+  BitPackerType::setField<eGroupIDBits::ID, group_id_num_bits>(group, id);
+}
+
+/*static*/ bool GroupIDBuilder::isCollective(GroupType const& group) {
+  return BitPackerType::boolGetField<eGroupIDBits::Collective>(group);
+}
+
+/*static*/ bool GroupIDBuilder::isStatic(GroupType const& group) {
+  return BitPackerType::boolGetField<eGroupIDBits::Static>(group);
+}
+
+/*static*/ NodeType GroupIDBuilder::getNode(GroupType const& group) {
+  return BitPackerType::getField<
+    eGroupIDBits::Node, group_node_num_bits, NodeType
+  >(group);
+}
+
+/*static*/ GroupIDType GroupIDBuilder::getGroupID(GroupType const& group) {
+  return BitPackerType::getField<
+    eGroupIDBits::ID, group_id_num_bits, GroupIDType
+  >(group);
+}
+
+
+
+}} /* end namespace vt::group */
