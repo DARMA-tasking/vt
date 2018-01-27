ad143b14a97adc923db0389b5a002b54ca32187f
Jonathan Lifflander <jliffla@sandia.gov>
2018-01-26 22:58:38 -0800

    test: improve put testing

--- tests/unit/active/test_active_send.cc
+++ tests/unit/active/test_active_send.cc
@@ -36,7 +36,26 @@ struct TestActiveSend : TestParallelHarness {
   }
 
   static void test_handler_2(PutTestMessage* msg) {
+    auto const& this_node = theContext()->getNode();
     auto ptr = static_cast<int*>(msg->getPut());
+    auto size = msg->getPutSize();
+    #if DEBUG_TEST_HARNESS_PRINT
+      printf("%d: test_handler_2: size=%lu, ptr=%p\n", this_node, size, ptr);
+    #endif
+    EXPECT_EQ(2 * sizeof(int), size);
+    for (int i = 0; i < 2; i++) {
+      EXPECT_EQ(ptr[i], i);
+    }
+  }
+
+  static void test_handler_3(PutTestMessage* msg) {
+    auto const& this_node = theContext()->getNode();
+    auto ptr = static_cast<int*>(msg->getPut());
+    auto size = msg->getPutSize();
+    #if DEBUG_TEST_HARNESS_PRINT
+      printf("%d: test_handler_3: size=%lu, ptr=%p\n", this_node, size, ptr);
+    #endif
+    EXPECT_EQ(10 * sizeof(int), size);
     for (int i = 0; i < 10; i++) {
       EXPECT_EQ(ptr[i], i);
     }
@@ -67,19 +86,13 @@ TEST_F(TestActiveSend, test_type_safe_active_fn_send) {
     printf("test_type_safe_active_fn_send: node=%d\n", my_node);
   #endif
 
-  std::vector<int> test_vec{0,1,2,3,4,5,6,7,8,9};
-
   if (my_node == from_node) {
     for (int i = 0; i < num_msg_sent; i++) {
+      #if DEBUG_TEST_HARNESS_PRINT
+        printf("%d: sendMsg: i=%d\n", my_node, i);
+      #endif
       auto msg = new TestMsg();
       theMsg()->sendMsg<TestMsg, test_handler>(1, msg, [=]{ delete msg; });
-    }
-    for (int i = 0; i < num_msg_sent; i++) {
-      auto msg = new PutTestMessage();
-      msg->setPut(&test_vec[0], sizeof(int)*test_vec.size());
-      theMsg()->sendMsg<PutTestMessage, test_handler_2>(
-        1, msg, [=]{ delete msg; }
-      );
     }
   } else if (my_node == to_node) {
     theTerm()->attachGlobalTermAction([=]{
@@ -87,5 +100,52 @@ TEST_F(TestActiveSend, test_type_safe_active_fn_send) {
     });
   }
 }
+
+TEST_F(TestActiveSend, test_type_safe_active_fn_send_small_put) {
+  auto const& my_node = theContext()->getNode();
+
+  #if DEBUG_TEST_HARNESS_PRINT
+    printf("test_type_safe_active_fn_send_small_put: node=%d\n", my_node);
+  #endif
+
+  std::vector<int> test_vec{0,1};
+
+  if (my_node == from_node) {
+    for (int i = 0; i < num_msg_sent; i++) {
+      auto msg = new PutTestMessage();
+      msg->setPut(&test_vec[0], sizeof(int)*test_vec.size());
+      #if DEBUG_TEST_HARNESS_PRINT
+        printf("%d: sendMsg: (put) i=%d\n", my_node, i);
+      #endif
+      theMsg()->sendMsg<PutTestMessage, test_handler_2>(
+        1, msg, [=]{ delete msg; }
+      );
+    }
+  }
+}
+
+TEST_F(TestActiveSend, test_type_safe_active_fn_send_large_put) {
+  auto const& my_node = theContext()->getNode();
+
+  #if DEBUG_TEST_HARNESS_PRINT
+    printf("test_type_safe_active_fn_send_large_put: node=%d\n", my_node);
+  #endif
+
+  std::vector<int> test_vec_2{0,1,2,3,4,5,6,7,8,9};
+
+  if (my_node == from_node) {
+    for (int i = 0; i < num_msg_sent; i++) {
+      auto msg = new PutTestMessage();
+      msg->setPut(&test_vec_2[0], sizeof(int)*test_vec_2.size());
+      #if DEBUG_TEST_HARNESS_PRINT
+        printf("%d: sendMsg: (put) i=%d\n", my_node, i);
+      #endif
+      theMsg()->sendMsg<PutTestMessage, test_handler_3>(
+        1, msg, [=]{ delete msg; }
+      );
+    }
+  }
+}
+
 
 }}} // end namespace vt::tests::unit
