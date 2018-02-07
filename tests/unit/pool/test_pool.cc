0d2d063b13d196bc2f36641237b62822cc290f76
Jonathan Lifflander <jliffla@sandia.gov>
2018-02-07 00:41:11 -0800

    pool: improve unit test

--- tests/unit/pool/test_pool.cc
+++ tests/unit/pool/test_pool.cc
@@ -13,30 +13,6 @@ namespace vt { namespace tests { namespace unit {
 
 using namespace vt::tests::unit;
 
-struct TestPoolSimple : TestHarness { };
-
-TEST_F(TestPoolSimple, pool_alloc) {
-  using namespace vt;
-
-  using CharType = unsigned char;
-
-  static constexpr size_t const max_bytes = 16384;
-  static constexpr CharType const init_val = 'z';
-
-  std::unique_ptr<pool::Pool> testPool = std::make_unique<pool::Pool>();
-
-  for (size_t cur_bytes = 1; cur_bytes < max_bytes; cur_bytes *= 2) {
-    void* ptr = testPool->alloc(cur_bytes);
-    std::memset(ptr, init_val, cur_bytes);
-    //printf("alloc %ld bytes, ptr=%p\n", cur_bytes, ptr);
-    EXPECT_NE(ptr, nullptr);
-    for (size_t i = 0; i < cur_bytes; i++) {
-      EXPECT_EQ(static_cast<CharType*>(ptr)[i], init_val);
-    }
-    testPool->dealloc(ptr);
-  }
-}
-
 static constexpr int64_t const min_bytes = 1;
 static constexpr int64_t const max_bytes = 16384;
 
@@ -47,6 +23,9 @@ struct TestPool : TestParallelHarness {
   template <int64_t num_bytes>
   static void testPoolFun(TestMsg<num_bytes>* prev_msg);
 
+  virtual void SetUp() {
+    TestParallelHarness::SetUp();
+  }
 };
 
 template <int64_t num_bytes>
@@ -72,6 +51,29 @@ TEST_F(TestPool, pool_message_alloc) {
     auto msg = new TestMsg<min_bytes>();
     testPoolFun<min_bytes>(msg);
     delete msg;
+  }
+}
+
+
+TEST_F(TestPool, pool_alloc) {
+  using namespace vt;
+
+  using CharType = unsigned char;
+
+  static constexpr size_t const max_bytes = 16384;
+  static constexpr CharType const init_val = 'z';
+
+  std::unique_ptr<pool::Pool> testPool = std::make_unique<pool::Pool>();
+
+  for (size_t cur_bytes = 1; cur_bytes < max_bytes; cur_bytes *= 2) {
+    void* ptr = testPool->alloc(cur_bytes);
+    std::memset(ptr, init_val, cur_bytes);
+    //printf("alloc %ld bytes, ptr=%p\n", cur_bytes, ptr);
+    EXPECT_NE(ptr, nullptr);
+    for (size_t i = 0; i < cur_bytes; i++) {
+      EXPECT_EQ(static_cast<CharType*>(ptr)[i], init_val);
+    }
+    testPool->dealloc(ptr);
   }
 }
 
