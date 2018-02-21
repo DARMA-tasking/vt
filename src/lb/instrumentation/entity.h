e0810fdc9042f5e5a5731924940b63bb63faebd9
Jonathan Lifflander <jliffla@sandia.gov>
2018-02-21 15:56:44 -0800

    lb #98: start entity for LB abstraction

---
+++ src/lb/instrumentation/entity.h
@@ -0,0 +1,13 @@
+
+#if !defined INCLUDED_LB_INSTRUMENTATION_ENTITY_H
+#define INCLUDED_LB_INSTRUMENTATION_ENTITY_H
+
+#include "config.h"
+
+namespace vt { namespace lb { namespace instrumentation {
+
+struct Entity { };
+
+}}} /* end namespace vt::lb::instrumentation */
+
+#endif /*INCLUDED_LB_INSTRUMENTATION_ENTITY_H*/
