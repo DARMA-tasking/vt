a444ce7bdce9840fa40e1f5d3f3e483fbc369b09
Jonathan Lifflander <jliffla@sandia.gov>
2018-04-08 05:06:59 -0700

    #183: build: add in stripped down header agglomerations

---
+++ src/transport_basic.h
@@ -0,0 +1,12 @@
+
+#if !defined INCLUDED_TRANSPORT_BASIC_H
+#define INCLUDED_TRANSPORT_BASIC_H
+
+#include "config.h"
+#include "transport_core.h"
+#include "messaging/envelope.h"
+#include "messaging/message.h"
+#include "collective/collective_ops.h"
+#include "termination/termination.h"
+
+#endif /*INCLUDED_TRANSPORT_BASIC_H*/
