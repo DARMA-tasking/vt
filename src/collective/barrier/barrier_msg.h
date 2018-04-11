00d7ca91d82b52d432244201e08f4bbf863a4ea7
Jonathan Lifflander <jliffla@sandia.gov>
2018-04-11 10:20:22 -0700

    #180: barrier: make barrier message contain an epoch

--- src/collective/barrier/barrier_msg.h
+++ src/collective/barrier/barrier_msg.h
@@ -7,14 +7,14 @@
 
 namespace vt { namespace collective { namespace barrier {
 
-struct BarrierMsg : vt::ShortMessage {
+struct BarrierMsg : vt::EpochMessage {
   bool is_named, is_wait, skip_term = false;
   BarrierType barrier;
 
   BarrierMsg(
-    bool const& in_is_named, BarrierType const& in_barrier, bool const& in_is_wait
-  )
-    : ShortMessage(), is_named(in_is_named), is_wait(in_is_wait),
+    bool const& in_is_named, BarrierType const& in_barrier,
+    bool const& in_is_wait
+  ) : EpochMessage(), is_named(in_is_named), is_wait(in_is_wait),
       skip_term(false), barrier(in_barrier)
   { }
 };
