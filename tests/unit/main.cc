81120a40aac7df25414ba5fb573f895c4cc4ec9e
Jonathan Lifflander <jliffla@sandia.gov>
2018-04-23 16:38:54 -0700

    #183: build: remove spurious reference to gmock which is not used

--- tests/unit/main.cc
+++ tests/unit/main.cc
@@ -2,13 +2,11 @@
 #include <vector>
 
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 
 #include "test_harness.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  ::testing::InitGoogleMock(&argc, argv);
 
   vt::tests::unit::TestHarness::store_cmdline_args(argc, argv);
 
