b0b6f0edd51f9f8e3728a9d9b13d9ebe35420d16
Jonathan Lifflander <jliffla@sandia.gov>
2018-04-30 14:56:22 -0400

    #183: build: add linking to atomic library

--- cmake/link_vt.cmake
+++ cmake/link_vt.cmake
@@ -52,6 +52,9 @@ function(link_target_with_vt)
     target_link_libraries(
       ${ARG_TARGET} PUBLIC ${ARG_BUILD_TYPE} ${VIRTUAL_TRANSPORT_LIBRARY}
     )
+    target_link_libraries(
+      ${ARG_TARGET} PUBLIC ${ARG_BUILD_TYPE} atomic
+    )
   endif()
 
   if (NOT DEFINED ARG_LINK_MPI AND ${ARG_DEFAULT_LINK_SET} OR ARG_LINK_MPI)
