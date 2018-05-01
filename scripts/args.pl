1c6727a51b81fabe4ed5c7c393b6f63267ded643
Jonathan Lifflander <jliffla@sandia.gov>
2018-05-01 16:04:52 -0700

    #183: build: modify script to all more flexible argument passing

--- scripts/args.pl
+++ scripts/args.pl
@@ -11,7 +11,9 @@ use warnings;
         my $self = {};
         bless($self, $class);
         $self->{input_params} = {};
+        $self->{input_params_pos} = {};
         $self->{verbose} = 0;
+        $self->{cur_position} = 0;
         return $self;
     }
 
@@ -34,6 +36,11 @@ use warnings;
         my $self = shift;
         my ($name,$required,$ref,$default,$hasfn,$fn) = @_;
         $self->{input_params}{$name} = [$required,0,$ref,$default,$hasfn,$fn];
+        my $cur_pos = $self->{cur_position};
+        my $verbose = $self->{verbose};
+        $self->{input_params_pos}{$cur_pos} = $name;
+        $self->{cur_position} += 1;
+        print "adding arg: name=$name, position=$cur_pos\n" if $verbose == 1;
     }
 
     sub get_argument_value {
@@ -66,12 +73,14 @@ use warnings;
     sub print_arguments {
         my $self = shift;
         my %input_params = %{$self->{input_params}};
+        my %input_params_pos = %{$self->{input_params_pos}};
         print "usage $0 <arguments>... \n";
         print "Format each argument to script as: <argname>=<argval>\n";
         print "Example: \"$0 mode=fast\"\n";
         print "\t<argument-name> <required>\n";
-        for (sort {$a cmp $b } (keys %input_params)) {
-            my $key = $_;
+        for (my $i = 0; $i < $self->{cur_position}; $i++) {
+            my $name = $input_params_pos{$i};
+            my $key = $name;
             my $required = ${$input_params{$key}}[0];
             my $req_str = "";
             $req_str = "true" if ($required == 1);
@@ -85,6 +94,7 @@ use warnings;
         my $self = shift;
         my @args = @_;
         my %input_params = %{$self->{input_params}};
+        my %input_params_pos = %{$self->{input_params_pos}};
         my $verbose = $self->{verbose};
         for (@args) {
             if ($_ eq "-h" || $_ eq "--help" || $_ eq "-help") {
@@ -92,36 +102,45 @@ use warnings;
                 exit 2;
             }
         }
+        my $arg_position = 0;
         for (@args) {
             my @spl = split /=/,$_;
 
             print "\t arg=\"$_\", spl=@spl\n" if $verbose == 1;
 
             if (@spl != 2) {
-                die "error parsing: argument without equal: $_\n";
+                my $pos_key = $input_params_pos{$arg_position};
+                if (${$input_params{$pos_key}}[1] != 1) {
+                    ${${$input_params{$pos_key}}[2]} = $spl[0];
+                    ${$input_params{$pos_key}}[1] = 1;
+                } else {
+                    die "error parsing: argument without equal: $_\n";
+                }
+            } else {
+              my $exact_key = $spl[0];
+              if (exists $input_params{$exact_key} && @{$input_params{$exact_key}}[1] == 0) {
+                  print "found exact matching: $exact_key\n" if $verbose == 1;
+                  ${${$input_params{$exact_key}}[2]} = $spl[1];
+                  ${$input_params{$exact_key}}[1] = 1;
+              } else {
+                  for my $key (keys %input_params) {
+                      print "\t\t key=\"$key\" \t arg=\"$_\"\n" if $verbose == 1;
+                      if ($_ =~ /$key=([^\s]*)/) {
+                          print "matching: $key, $1\n" if $verbose == 1;
+                          #${$input_params{$key}} = $1;
+                          my @state = @{$input_params{$key}};
+                          if ($state[1] == 0) {
+                              #print "@state\n";
+                              ${${$input_params{$key}}[2]} = $1;
+                              ${$input_params{$key}}[1] = 1;
+                          }
+                          last;
+                      }
+                  }
+              }
             }
 
-            my $exact_key = $spl[0];
-            if (exists $input_params{$exact_key} && @{$input_params{$exact_key}}[1] == 0) {
-                print "found exact matching: $exact_key\n" if $verbose == 1;
-                ${${$input_params{$exact_key}}[2]} = $spl[1];
-                ${$input_params{$exact_key}}[1] = 1;
-            } else {
-                for my $key (keys %input_params) {
-                    print "\t\t key=\"$key\" \t arg=\"$_\"\n" if $verbose == 1;
-                    if ($_ =~ /$key=([^\s]*)/) {
-                        print "matching: $key, $1\n" if $verbose == 1;
-                        #${$input_params{$key}} = $1;
-                        my @state = @{$input_params{$key}};
-                        if ($state[1] == 0) {
-                            #print "@state\n";
-                            ${${$input_params{$key}}[2]} = $1;
-                            ${$input_params{$key}}[1] = 1;
-                        }
-                        last;
-                    }
-                }
-            }
+            $arg_position += 1;
         }
         for (sort {$a cmp $b } (keys %input_params)) {
             if (${$input_params{$_}}[1] == 0 && ${$input_params{$_}}[0] == 1) {
