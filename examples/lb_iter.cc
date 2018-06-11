7e650800cb98a5ce2387c8f895aba68d27c8115a
Jonathan Lifflander <jliffla@sandia.gov>
2018-06-11 11:05:56 -0700

    #191: lb-lite: add parameters for lb_iter example

--- examples/lb_iter.cc
+++ examples/lb_iter.cc
@@ -11,7 +11,7 @@ using namespace ::vt::index;
 using namespace ::vt::mapping;
 
 static constexpr int32_t const default_num_elms = 64;
-static constexpr int32_t const num_iter = 8;
+static int32_t num_iter = 8;
 
 struct IterCol : Collection<IterCol,Index1D> {
   IterCol() = default;
@@ -57,15 +57,17 @@ struct FinishedIter {
   }
 };
 
+static double weight = 1.0f;
+
 static void iterWork(IterMsg* msg, IterCol* col) {
   double val = 0.1f;
   double val2 = 0.4f * msg->work_amt_;
   auto const idx = col->getIndex().x();
   auto const iter = msg->iter_;
   //::fmt::print("proc={}, idx={}, iter={}\n", theContext()->getNode(),idx,iter);
-  int64_t const max_work = 10000;
-  int64_t const mid_work = 1000;
-  int64_t const min_work = 10;
+  int64_t const max_work = 1000 * weight;
+  int64_t const mid_work = 100 * weight;
+  int64_t const min_work = 1 * weight;
   int const x = idx < 8 ? max_work : (idx > 40 ? mid_work : min_work);
   for (int i = 0; i < 10000 * x; i++) {
     val *= val2 + i*29.4;
@@ -99,8 +101,19 @@ int main(int argc, char** argv) {
   if (argc > 1) {
     num_elms = atoi(argv[1]);
   }
+  if (argc > 2) {
+    weight = atoi(argv[2]);
+  }
+  if (argc > 3) {
+    num_iter = atoi(argv[3]);
+  }
 
   if (this_node == 0) {
+    ::fmt::print(
+      "lb_iter: elms={}, weight={}, num_iter={}\n",
+      num_elms, weight, num_iter
+    );
+
     auto const& range = Index1D(num_elms);
     proxy = theCollection()->construct<IterCol>(range);
     cur_time = ::vt::timing::Timing::getCurrentTime();
