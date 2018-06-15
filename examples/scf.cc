a59f1a54701b26438b37884e509d0581f73b836d
Cannada Lewis <canlewi@.sandia.gov>
2018-06-15 11:53:38 -0700

    Outlining the SCF procedure:
    At the moment the code is just serial, but with an eye for
    parallelization.
    
    Next week:
    1. Bring in integrals
    2. Add Cmake detection for Eigen
    3. Allow for user input
    4. Compute energies
    5. Then we will be ready to parallelize

--- examples/scf.cc
+++ examples/scf.cc
@@ -28,16 +28,65 @@ int main(int argc, char** argv) {
     theMsg()->broadcastMsg<TestMsg, testHandler>(msg);
   }
 
-  std::vector<Water> waters(10);
+  std::vector<scf::Water> waters(10);
   const auto num_waters = waters.size();
 
-  // All the matrices we will need, trim down later. 
-  SparseMartix S(num_waters, num_waters);
-  SparseMartix T(num_waters, num_waters);
-  SparseMartix V(num_waters, num_waters);
-  SparseMartix H(num_waters, num_waters);
-  SparseMartix D(num_waters, num_waters);
-  SparseMartix F(num_waters, num_waters);
+  // All the matrices we will need, trim down later.
+  scf::SparseMatrix S(num_waters, num_waters);
+  scf::SparseMatrix H(num_waters, num_waters);
+  scf::SparseMatrix F(num_waters, num_waters);
+
+  // Populate the matrices with information
+  for (auto i = 0; i < num_waters; ++i) {
+    auto const& wi = waters[i];
+    for (auto j = 0; j < num_waters; ++j) {
+      if (S.is_local(i, j)) { // Assume that H has the same distribution as S
+        auto const& wj = waters[j];
+        S.block(i, j) = S_integral(wi, wj);
+        H.block(i, j) = V_integral(wi, wj) + T_integral(wi, wj);
+      }
+    }
+  }
+
+  auto truncate_thresh = 1e-8;
+  S.truncate(truncate_thresh);
+  H.truncate(truncate_thresh);
+
+  // Initialize guess assuming that D = 0;
+  F = H;
+
+  // Bootstrap SCF
+  auto Srep = S.replicate();
+  auto Frep = F.replicate();
+
+  const auto water_occupation = 5;
+  const auto nocc = water_occupation * num_waters;
+
+  auto make_new_density = [num_waters, nocc](auto F, auto S, double thresh) {
+    // TODO Compute on one node and bcast, luckily Eigen will work this way
+    // since there are no phase issues
+    Eigen::GeneralizedSelfAdjointEigenSolver<scf::MatrixBlock> gevd(F, S);
+    scf::MatrixBlock const& C = gevd.eigenvectors().leftCols(nocc);
+    scf::MatrixBlock Drep = C * C.transpose();
+
+    return scf::SparseMatrix(num_waters, num_waters, Drep, thresh);
+  };
+
+  auto D = make_new_density(Frep, Srep, truncate_thresh);
+  scf::MatrixBlock Q;
+  std::vector<std::vector<scf::IndexType>> sparse_pair_list;
+  std::tie(Q, sparse_pair_list) = scf::Schwarz(waters);
+
+  auto iter = 0;
+  while (3 != iter++) {
+    ::fmt::print("Starting iteration {}\n", iter);
+
+    // TODO Make a new fock matrix given the density
+    // four_center_update(F, D, waters);
+
+    D = make_new_density(Frep, Srep, truncate_thresh);
+  }
+
 
   while (!rt->isTerminated()) {
     runScheduler();
@@ -51,17 +100,46 @@ int main(int argc, char** argv) {
 // Until we have integral kernels just fill with dummy data
 scf::MatrixBlock scf::two_e_integral(
   Water const& i, Water const& j, Water const& k, Water const& l) {
-  return scf::MatrixBlock(i.size() * j.size(), k.size() * l.size());
+  return scf::MatrixBlock::Random(
+    i.nbasis() * j.nbasis(), k.nbasis() * l.nbasis());
 }
 
 scf::MatrixBlock scf::S_integral(Water const& i, Water const& j) {
-  return scf::MatrixBlock(i.size(), j.size());
+  return scf::MatrixBlock::Random(i.nbasis(), j.nbasis());
 }
 
 scf::MatrixBlock scf::V_integral(Water const& i, Water const& j) {
-  return scf::MatrixBlock(i.size(), j.size());
+  return scf::MatrixBlock::Random(i.nbasis(), j.nbasis());
 }
 
 scf::MatrixBlock scf::T_integral(Water const& i, Water const& j) {
-  return scf::MatrixBlock(i.size(), j.size());
+  return scf::MatrixBlock::Random(i.nbasis(), j.nbasis());
+}
+
+std::pair<scf::MatrixBlock, std::vector<std::vector<scf::IndexType>>>
+scf::Schwarz(std::vector<Water> const& waters) {
+  const auto num_waters = waters.size();
+  // To start off let's just do screening on individual waters, we can make it
+  // do shell level screening later.
+  scf::MatrixBlock Q(num_waters, num_waters);
+  Q.setZero();
+
+  std::vector<std::vector<scf::IndexType>> sparse_pair_list(num_waters);
+
+  // TODO should distribute and reduce the matrix and the pair list
+  for (auto i = 0; i < num_waters; ++i) {
+    auto const& wi = waters[i];
+
+    for (auto j = 0; j < num_waters; ++j) {
+      auto const& wj = waters[j];
+
+      Q(i, j) = scf::two_e_integral(wi, wj, wi, wj).norm();
+
+      if (Q(i, j) > 1e-12) { // Screen the sparse pairs
+        sparse_pair_list[i].push_back(j);
+      }
+    }
+  }
+
+  return std::make_pair(std::move(Q), std::move(sparse_pair_list));
 }
--- examples/scf.h
+++ examples/scf.h
@@ -8,26 +8,62 @@ namespace scf {
 
 using IndexType = int32_t;
 
-static constexpr IndexType const i_block = 24;
-static constexpr IndexType const j_block = 24;
 static constexpr IndexType const nbasis_per_water = 24;
+static constexpr IndexType const nshell_per_water = 3;
+
+static constexpr IndexType const i_block = nbasis_per_water;
+static constexpr IndexType const j_block = nbasis_per_water;
 
 // struct MatrixBlock : public Eigen::MatrixXd {};
 using MatrixBlock = Eigen::MatrixXd;
 using Vector3d = Eigen::Vector3d;
 
-struct SparseMartix {
+//
+// TODO if and when we switch to a truly sparse data structure the replication
+// and initialization from a dense structure will need to be reimplemented.
+//
+struct SparseMatrix {
   using BlockType = MatrixBlock;
   using BlockPtrType = MatrixBlock*;
 
-  SparseMartix(IndexType r, IndexType c)
+  SparseMatrix(IndexType r, IndexType c)
     : blocks_(r, std::vector<BlockType>(c)), rows_(r), cols_(c) {}
+
+  SparseMatrix(IndexType r, IndexType c, MatrixBlock const& mat)
+    : SparseMatrix(r, c) {
+    // Check that the input matrix is correctly sized
+    assert(mat.rows() == rows_ * nbasis_per_water);
+    assert(mat.cols() == cols_ * nbasis_per_water);
+
+    for (auto i = 0; i < rows_; ++i) {
+      for (auto j = 0; j < cols_; ++j) {
+        if (is_local(i, j)) {
+          blocks_[i][j] = mat.block(
+            i * nbasis_per_water, j * nbasis_per_water, nbasis_per_water,
+            nbasis_per_water);
+        }
+      }
+    }
+  }
+
+  // TODO Do this like the above constructor, with the truncate in the loop, if
+  // space is an issue.
+  SparseMatrix(IndexType r, IndexType c, MatrixBlock const& mat, double thresh)
+    : SparseMatrix(r, c, mat) {
+    truncate(thresh);
+  }
+
+  SparseMatrix(SparseMatrix const&) = default;
+  SparseMatrix& operator=(SparseMatrix const&) = default;
 
   BlockType& block(IndexType i, IndexType j) {
     assert(blocks_.size() >= i && "i must exist");
     assert(blocks_[i].size() >= j && "j must exist");
     return blocks_[i][j];
   }
+
+  // Return True for now until we decide on the distribution
+  bool is_local(IndexType i, IndexType j) const { return true; }
 
   double& globalEl(IndexType i, IndexType j) {
     const auto bi = i / i_block;
@@ -40,7 +76,40 @@ struct SparseMartix {
 
   // We need a way to get a copy of the matrix that we can call an Eigensolver
   // on.
-  MatrixBlock replicate() const {}
+  MatrixBlock replicate() const {
+    const auto num_waters = blocks_.size();
+    const auto total_functions = num_waters * nbasis_per_water;
+    MatrixBlock replicated(total_functions, total_functions);
+    replicated.setZero();
+
+    for (auto i = 0; i < rows_; ++i) {
+      for (auto j = 0; j < cols_; ++j) {
+        if (is_local(i, j) && blocks_[i][j].size() > 0) {
+          replicated.block(
+            i * nbasis_per_water, j * nbasis_per_water, nbasis_per_water,
+            nbasis_per_water) = blocks_[i][j];
+        }
+      }
+    }
+
+    // TODO once the Sparse matrix is distributed we will need a way to
+    // reduce the replicated matrix since each node only writes the blocks
+    // that it owns.
+
+    return replicated;
+  }
+
+  // Remove blocks with fnorm smaller than threshold
+  void truncate(double thresh) {
+    const auto thresh2 = thresh * thresh;
+    for (auto& row : blocks_) {
+      for (auto& block : row) {
+        if (thresh < block.squaredNorm()) {
+          block.resize(0, 0);
+        }
+      }
+    }
+  }
 
   protected:
   std::vector<std::vector<BlockType>> blocks_;
@@ -48,18 +117,38 @@ struct SparseMartix {
   IndexType cols_;
 };
 
-// Assume we hard coded the basis.
-static constexpr IndexType const nfunc_per_water = 24;
 
-// Water will need to be expanded to have shells and we will select a canonical
-// ordering for the shells
-struct Water {
-  Water(Vector3d v) : pos_(std::move(v)) {}
+// Later change this to Libint Shell
+struct Shell {
+  Shell(Vector3d v) : pos_(std::move(v)) {}
 
-  IndexType size() const { return nfunc_per_water; }
+  Shell() = default;
+  Shell(Shell const&) = default;
+  Shell(Shell&&) = default;
+  ~Shell() = default;
+
+  Vector3d& center() { return pos_; }
+  Vector3d const& center() const { return pos_; }
 
   private:
   Vector3d pos_;
+};
+
+// Water will need to be expanded to have shells and we will select a
+// canonical ordering for the shells
+struct Water {
+  Water(Vector3d O, Vector3d H1, Vector3d H2) : shells_(nshell_per_water) {
+    shells_.emplace_back(Shell(std::move(O)));
+    shells_.emplace_back(Shell(std::move(H1)));
+    shells_.emplace_back(Shell(std::move(H2)));
+  }
+
+  Water() = default;
+
+  IndexType nbasis() const { return nbasis_per_water; }
+
+  private:
+  std::vector<Shell> shells_;
 };
 
 // 4 center integrals for 4 different waters
@@ -74,6 +163,10 @@ MatrixBlock V_integral(Water const&, Water const&);
 
 // Kinetic energy integrals
 MatrixBlock T_integral(Water const&, Water const&);
+
+// Compute the Schwarz bounds and the sparse pair list
+std::pair<MatrixBlock, std::vector<std::vector<IndexType>>>
+Schwarz(std::vector<Water> const&);
 
 } /*end namespace scf*/
 
