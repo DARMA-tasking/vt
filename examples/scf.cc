1369aaf30a4508c38e3b7b49baf685e8f648ead5
Cannada Lewis <canlewi@.sandia.gov>
2018-06-14 17:24:32 -0700

    Started to outline what we will need to fake SCF.

--- examples/scf.cc
+++ examples/scf.cc
@@ -1,4 +1,3 @@
-
 #include "scf.h"
 
 #include <fmt/format.h>
@@ -13,7 +12,7 @@ static NodeType num_nodes = uninitialized_destination;
 struct TestMsg : ::vt::Message {};
 
 static void testHandler(TestMsg* msg) {
-  ::fmt::print("{}: testHandler\n",this_node);
+  ::fmt::print("{}: testHandler\n", theContext()->getNode());
 }
 
 int main(int argc, char** argv) {
@@ -22,12 +21,23 @@ int main(int argc, char** argv) {
   this_node = theContext()->getNode();
   num_nodes = theContext()->getNumNodes();
 
-  ::fmt::print("{}: started\n",this_node);
+  ::fmt::print("{}: started\n", this_node);
 
   if (this_node == 0) {
     auto msg = makeSharedMessage<TestMsg>();
-    theMsg()->broadcastMsg<TestMsg,testHandler>(msg);
+    theMsg()->broadcastMsg<TestMsg, testHandler>(msg);
   }
+
+  std::vector<Water> waters(10);
+  const auto num_waters = waters.size();
+
+  // All the matrices we will need, trim down later. 
+  SparseMartix S(num_waters, num_waters);
+  SparseMartix T(num_waters, num_waters);
+  SparseMartix V(num_waters, num_waters);
+  SparseMartix H(num_waters, num_waters);
+  SparseMartix D(num_waters, num_waters);
+  SparseMartix F(num_waters, num_waters);
 
   while (!rt->isTerminated()) {
     runScheduler();
@@ -36,4 +46,22 @@ int main(int argc, char** argv) {
   CollectiveOps::finalize();
 
   return 0;
+}
+
+// Until we have integral kernels just fill with dummy data
+scf::MatrixBlock scf::two_e_integral(
+  Water const& i, Water const& j, Water const& k, Water const& l) {
+  return scf::MatrixBlock(i.size() * j.size(), k.size() * l.size());
+}
+
+scf::MatrixBlock scf::S_integral(Water const& i, Water const& j) {
+  return scf::MatrixBlock(i.size(), j.size());
+}
+
+scf::MatrixBlock scf::V_integral(Water const& i, Water const& j) {
+  return scf::MatrixBlock(i.size(), j.size());
+}
+
+scf::MatrixBlock scf::T_integral(Water const& i, Water const& j) {
+  return scf::MatrixBlock(i.size(), j.size());
 }
--- examples/scf.h
+++ examples/scf.h
@@ -1,38 +1,79 @@
-
 #include "transport.h"
 
 #include <cstdlib>
+#include <eigen3/Eigen/Dense>
 #include <vector>
 
 namespace scf {
 
 using IndexType = int32_t;
 
-static constexpr IndexType const i_block = 32;
-static constexpr IndexType const j_block = 32;
+static constexpr IndexType const i_block = 24;
+static constexpr IndexType const j_block = 24;
+static constexpr IndexType const nbasis_per_water = 24;
 
-struct MatrixBlock : public eigen::MatrixXd { };
+// struct MatrixBlock : public Eigen::MatrixXd {};
+using MatrixBlock = Eigen::MatrixXd;
+using Vector3d = Eigen::Vector3d;
 
 struct SparseMartix {
   using BlockType = MatrixBlock;
   using BlockPtrType = MatrixBlock*;
 
-  BlockPtrType& getBlock(IndexType i, IndexType j) {
+  SparseMartix(IndexType r, IndexType c)
+    : blocks_(r, std::vector<BlockType>(c)), rows_(r), cols_(c) {}
+
+  BlockType& block(IndexType i, IndexType j) {
     assert(blocks_.size() >= i && "i must exist");
     assert(blocks_[i].size() >= j && "j must exist");
     return blocks_[i][j];
   }
 
-  BlockPtrType getBlockGlobal(IndexType i, IndexType j) {
-    auto bi = i / i_block;
-    auto bj = j / j_block;
-    auto ei = i % i_block;
-    auto ej = j % j_block;
+  double& globalEl(IndexType i, IndexType j) {
+    const auto bi = i / i_block;
+    const auto bj = j / j_block;
+    const auto ei = i % i_block;
+    const auto ej = j % j_block;
+
+    return block(bi, bj)(ei, ej);
   }
 
-protected:
+  // We need a way to get a copy of the matrix that we can call an Eigensolver
+  // on.
+  MatrixBlock replicate() const {}
+
+  protected:
   std::vector<std::vector<BlockType>> blocks_;
+  IndexType rows_;
+  IndexType cols_;
 };
+
+// Assume we hard coded the basis.
+static constexpr IndexType const nfunc_per_water = 24;
+
+// Water will need to be expanded to have shells and we will select a canonical
+// ordering for the shells
+struct Water {
+  Water(Vector3d v) : pos_(std::move(v)) {}
+
+  IndexType size() const { return nfunc_per_water; }
+
+  private:
+  Vector3d pos_;
+};
+
+// 4 center integrals for 4 different waters
+MatrixBlock
+two_e_integral(Water const&, Water const&, Water const&, Water const&);
+
+// Overlap integrals
+MatrixBlock S_integral(Water const&, Water const&);
+
+// Nuclear attraction integrals
+MatrixBlock V_integral(Water const&, Water const&);
+
+// Kinetic energy integrals
+MatrixBlock T_integral(Water const&, Water const&);
 
 } /*end namespace scf*/
 
