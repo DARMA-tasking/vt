53e37cf4350d990f08997f08562b969e1f953aef
Jonathan Lifflander <jliffla@sandia.gov>
2018-06-18 12:29:25 -0700

    vrt collection: reduce: add test for non-default operator

--- tests/unit/collectives/test_reduce_collection.cc
+++ tests/unit/collectives/test_reduce_collection.cc
@@ -22,6 +22,45 @@ struct MyReduceMsg : ReduceMsg {
   int num = 0;
 };
 
+struct VectorPayload {
+  VectorPayload() = default;
+
+  friend VectorPayload operator+(VectorPayload v1, VectorPayload const& v2) {
+    for (auto&& elm : v2.vec) {
+      v1.vec.push_back(elm);
+    }
+    return v1;
+  }
+
+  template <typename SerializerT>
+  void serialize(SerializerT& s) {
+    s | vec;
+  }
+
+  std::vector<double> vec;
+};
+
+struct SysMsgVec : ReduceTMsg<VectorPayload> {
+  SysMsgVec() = default;
+  explicit SysMsgVec(double in_num)
+    : ReduceTMsg<VectorPayload>()
+  {
+    getVal().vec.push_back(in_num);
+    getVal().vec.push_back(in_num + 1);
+  }
+
+  template <typename SerializerT>
+  void serialize(SerializerT& s) {
+    ReduceTMsg<VectorPayload>::invokeSerialize(s);
+  }
+};
+
+struct PrintVec {
+  void operator()(SysMsgVec* msg) {
+    fmt::print("final size={}\n", msg->getConstVal().vec.size());
+  }
+};
+
 struct MyCol : Collection<MyCol, Index1D> {
   MyCol()
     : Collection<MyCol, Index1D>()
@@ -67,6 +106,26 @@ struct TestReduceCollection : TestParallelHarness {
     theCollection()->reduceMsg<MyCol,MyReduceMsg,reducePlus>(proxy, reduce_msg);
   }
 
+  static void colHanVec(ColMsg* msg, MyCol* col) {
+    auto const& node = theContext()->getNode();
+    auto const& idx = col->getIndex();
+    fmt::print(
+      "{}: colHanVec received: ptr={}, idx={}, getIndex={}\n",
+      node, print_ptr(col), idx.x(), col->getIndex().x()
+    );
+
+    auto reduce_msg = makeSharedMessage<SysMsgVec>(static_cast<double>(idx.x()));
+    auto proxy = col->getProxy();
+    fmt::print(
+      "reduce_msg->vec.size()={}\n", reduce_msg->getConstVal().vec.size()
+    );
+    theCollection()->reduceMsg<
+      MyCol,
+      SysMsgVec,
+      SysMsgVec::msgHandler<SysMsgVec,PlusOp<VectorPayload>,PrintVec>
+    >(proxy, reduce_msg);
+  }
+
   static void reducePlus(MyReduceMsg* msg) {
     fmt::print(
       "{}: cur={}: is_root={}, count={}, next={}, num={}\n",
@@ -105,6 +164,19 @@ TEST_F(TestReduceCollection, test_reduce_op) {
     auto proxy = theCollection()->construct<MyCol>(range);
     auto msg = new ColMsg(this_node);
     proxy.broadcast<ColMsg,colHan>(msg);
+  }
+}
+
+TEST_F(TestReduceCollection, test_reduce_vec_op) {
+  auto const& my_node = theContext()->getNode();
+  auto const& root = 0;
+
+  auto const& this_node = theContext()->getNode();
+  if (this_node == 0) {
+    auto const& range = Index1D(32);
+    auto proxy = theCollection()->construct<MyCol>(range);
+    auto msg = new ColMsg(this_node);
+    proxy.broadcast<ColMsg,colHanVec>(msg);
   }
 }
 
