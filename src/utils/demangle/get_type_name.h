c8be1e40c6de9e9d104dd06b806139fb68ac8f1e
Jonathan Lifflander <jliffla@sandia.gov>
2018-06-21 13:11:16 -0700

    demangle: implement new method for extracting type

---
+++ src/utils/demangle/get_type_name.h
@@ -0,0 +1,35 @@
+
+#if !defined INCLUDED_UTILS_DEMANGLE_GET_TYPE_NAME_H
+#define INCLUDED_UTILS_DEMANGLE_GET_TYPE_NAME_H
+
+#include "config.h"
+#include "utils/string/static.h"
+
+namespace vt { namespace util { namespace demangle {
+
+using StaticString = util::string::StatStr;
+
+template <class T>
+constexpr StaticString type_name() {
+#if defined(__clang__)
+  StaticString p = __PRETTY_FUNCTION__;
+  return StaticString(p.data() + 31, p.size() - 31 - 1);
+
+# elif defined(__GNUC__)
+  StaticString p = __PRETTY_FUNCTION__;
+
+#   if __cplusplus < 201402
+    return StaticString(p.data() + 36, p.size() - 36 - 1);
+#   else
+    return StaticString(p.data() + 46, p.size() - 46 - 1);
+#   endif
+
+# elif defined(_MSC_VER)
+  StaticString p = __FUNCSIG__;
+  return StaticString(p.data() + 38, p.size() - 38 - 7);
+#endif
+}
+
+}}} /* end namespace vt::util::demangle */
+
+#endif /*INCLUDED_UTILS_DEMANGLE_GET_TYPE_NAME_H*/
