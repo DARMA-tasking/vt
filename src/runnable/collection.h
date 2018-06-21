55792edd7460be95f69f485b071c62db4837d217
Jonathan Lifflander <jliffla@sandia.gov>
2018-06-20 22:37:05 -0700

    runnable: new collection interface for dispatch

---
+++ src/runnable/collection.h
@@ -0,0 +1,21 @@
+
+#if !defined INCLUDED_RUNNABLE_COLLECTION_H
+#define INCLUDED_RUNNABLE_COLLECTION_H
+
+#include "config.h"
+
+namespace vt { namespace runnable {
+
+template <typename MsgT, typename ElementT>
+struct RunnableCollection {
+  static void run(
+    HandlerType handler, MsgT* msg, ElementT* elm, NodeType from_node,
+    uint64_t idx
+  );
+};
+
+}} /* end namespace vt::runnable */
+
+#include "runnable/collection.impl.h"
+
+#endif /*INCLUDED_RUNNABLE_COLLECTION_H*/
---
+++ src/runnable/collection.impl.h
@@ -0,0 +1,47 @@
+
+#if !defined INCLUDED_RUNNABLE_COLLECTION_IMPL_H
+#define INCLUDED_RUNNABLE_COLLECTION_IMPL_H
+
+#include "config.h"
+#include "runnable/collection.h"
+#include "registry/auto/auto_registry_common.h"
+#include "registry/auto/auto_registry_interface.h"
+#include "registry/auto/collection/auto_registry_collection.h"
+#include "trace/trace_common.h"
+#include "messaging/envelope.h"
+
+namespace vt { namespace runnable {
+
+template <typename MsgT, typename ElementT>
+/*static*/ void RunnableCollection<MsgT,ElementT>::run(
+  HandlerType handler, MsgT* msg, ElementT* elm, NodeType from_node,
+  uint64_t idx
+) {
+  #if backend_check_enabled(trace_enabled)
+    trace::TraceEntryIDType trace_id = auto_registry::theTraceID(
+      handler, auto_registry::RegistryTypeEnum::RegVrtCollection
+    );
+    trace::TraceEventIDType trace_event = envelopeGetTraceEvent(msg->env);
+  #endif
+
+  #if backend_check_enabled(trace_enabled)
+    theTrace()->beginProcessing(
+      trace_id, sizeof(*msg), trace_event, from_node,
+      trace::Trace::getCurrentTime(), idx
+    );
+  #endif
+
+  auto const func = auto_registry::getAutoHandlerCollection(handler);
+  func(msg, elm);
+
+  #if backend_check_enabled(trace_enabled)
+    theTrace()->endProcessing(
+      trace_id, sizeof(*msg), trace_event, from_node,
+      trace::Trace::getCurrentTime(), idx
+    );
+  #endif
+}
+
+}} /* end namespace vt::runnable */
+
+#endif /*INCLUDED_RUNNABLE_COLLECTION_IMPL_H*/
