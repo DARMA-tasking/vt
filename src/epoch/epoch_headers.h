241c6c8b195b2e557cda8f9bffbf92bc40be4c38
Jonathan Lifflander <jliffla@sandia.gov>
2018-07-04 10:15:38 -0700

    #199: epoch: header file that combines all epoch-related headers

---
+++ src/epoch/epoch_headers.h
@@ -0,0 +1,9 @@
+
+#if !defined INCLUDED_EPOCH_EPOCH_HEADERS_H
+#define INCLUDED_EPOCH_EPOCH_HEADERS_H
+
+#include "config.h"
+#include "epoch/epoch.h"
+#include "epoch/epoch_manip.h"
+
+#endif /*INCLUDED_EPOCH_EPOCH_HEADERS_H*/
