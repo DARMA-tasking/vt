8fbd60faf15531c6aff41ed5d09b1e1bfbc98da8
Jonathan Lifflander <jliffla@sandia.gov>
2018-07-04 10:01:58 -0700

    #199: utils: bit packer: fix bugs in bit packing related to field size

--- src/utils/bits/bits_packer.h
+++ src/utils/bits/bits_packer.h
@@ -3,45 +3,53 @@
 #define INCLUDED_BITS_PACKER
 
 #include <cassert>
+#include <cstdlib>
 
 namespace vt { namespace utils {
 
 struct BitPacker {
+  using FieldType         = int64_t;
+  using FieldUnsignedType = uint64_t;
+
+  #define gen_bit_mask(len) ((static_cast<uint64_t>(1) << (len)) - 1)
 
   template <typename BitField>
-  static inline uint8_t getMsbBit(BitField const& field) {
+  static inline FieldUnsignedType getMsbBit(BitField const& field) {
     uint64_t field_copy = static_cast<uint64_t>(field);
-    uint8_t r = 0;
+    FieldUnsignedType r = 0;
     while (field_copy >>= 1) {
       r++;
     }
     return r;
   }
 
-  template <int8_t start, int8_t len, typename BitSegment, typename BitField>
+  template <FieldType start, FieldType len, typename BitSegment, typename BitField>
   static inline void bitorSetField(BitField& field, BitSegment const& segment) {
     field |= segment << start;
   }
 
-  #define gen_bit_mask(len) ((static_cast<uint64_t>(1) << (len)) - 1)
-
-  template <int8_t start, int8_t len, typename BitSegment, typename BitField>
+  template <FieldType start, FieldType len, typename BitSegment, typename BitField>
   static inline BitSegment getField(BitField const& field) {
     return getFieldDynamic<BitSegment, BitField>(start, len, field);
   }
 
   template <typename BitSegment, typename BitField>
   static inline BitSegment getFieldDynamic(
-    int8_t start, int8_t len, BitField const& field
+    FieldType start, FieldType len, BitField const& field
   ) {
     auto const& comp = field >> start;
-    return
-      len < sizeof(BitField) ?
+    ::fmt::print(
+      "start={}, len={}, field={}, comp={}, sizeof(BitField)={}, masked={},"
+      "mask={}\n",
+      start, len, field, comp, sizeof(BitField), comp & gen_bit_mask(len),
+      gen_bit_mask(len)
+    );
+    return len < sizeof(BitField)*8 ?
       static_cast<BitSegment>(comp & gen_bit_mask(len)) :
       static_cast<BitSegment>(comp);
   }
 
-  template <int8_t start, int8_t len, typename BitSegment, typename BitField>
+  template <FieldType start, FieldType len, typename BitSegment, typename BitField>
   static inline void setField(BitField& field, BitSegment const& segment) {
     #if backend_check_enabled(bit_check_overflow)
     auto const& seg_msb_bit = get_msb_bit(segment);
@@ -59,24 +67,27 @@ struct BitPacker {
 
   template <typename BitSegment, typename BitField>
   static inline void setFieldDynamic(
-    int8_t start, int8_t len, BitField& field, BitSegment const& segment
+    FieldType start, FieldType len, BitField& field, BitSegment const& segment
   ) {
     field = field & ~(gen_bit_mask(len) << start);
     field = field | (static_cast<BitField>(segment) << start);
   }
 
-  template <int8_t start, int8_t len = 1, typename BitField>
+  template <FieldType start, FieldType len = 1, typename BitField>
   static inline void boolSetField(BitField& field, bool const& set_value) {
     if (set_value) {
-      field |= 1 << start;
+      field |= 1UL << start;
     } else {
-      field &= ~(1 << start);
+      field &= ~(1UL << start);
     }
   }
 
-  template <int8_t start, int8_t len = 1, typename BitField>
-  static inline bool boolGetField(BitField& field) {
-    return field & (1 << start);
+  template <FieldType start, FieldType len = 1, typename BitField>
+  static inline bool boolGetField(BitField const& field) {
+    // return field & (1 << start);
+    BitField field_copy = field;
+    bool const is_set = field_copy & (1UL << start);
+    return is_set;
   }
 
 };
