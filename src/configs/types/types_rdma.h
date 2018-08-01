81adc23510d868e82e377904a475b47806d70887
Jonathan Lifflander <jliffla@sandia.gov>
2018-07-31 19:05:22 -0700

    #205: pipe: reformat RDMA types to conform to new style

--- src/configs/types/types_rdma.h
+++ src/configs/types/types_rdma.h
@@ -11,14 +11,14 @@
 
 namespace vt {
 
-using RDMA_PtrType = void *;
-using RDMA_ElmType = int64_t;
-using RDMA_BlockType = int64_t;
-using RDMA_HandleType = int64_t;
-using RDMA_HandlerType = int64_t;
-using RDMA_GetType = std::tuple<RDMA_PtrType, ByteType>;
-using RDMA_PutRetType = RDMA_GetType;
-using RDMA_ContinuationType = std::function<void(RDMA_GetType)>;
+using RDMA_PtrType                = void *;
+using RDMA_ElmType                = int64_t;
+using RDMA_BlockType              = int64_t;
+using RDMA_HandleType             = int64_t;
+using RDMA_HandlerType            = int64_t;
+using RDMA_GetType                = std::tuple<RDMA_PtrType, ByteType>;
+using RDMA_PutRetType             = RDMA_GetType;
+using RDMA_ContinuationType       = std::function<void(RDMA_GetType)>;
 using RDMA_ContinuationDeleteType = std::function<
   void(RDMA_GetType, ActionType)
 >;
