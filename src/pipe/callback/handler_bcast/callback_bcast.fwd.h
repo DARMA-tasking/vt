946bad3bda2d75fe0fc9eaae8705202fabbaab94
Jonathan Lifflander <jliffla@sandia.gov>
2018-08-02 11:47:11 -0700

    #205: pipe: add forward files

---
+++ src/pipe/callback/handler_bcast/callback_bcast.fwd.h
@@ -0,0 +1,14 @@
+
+#if !defined INCLUDED_PIPE_CALLBACK_HANDLER_BCAST_CALLBACK_BCAST_FWD_H
+#define INCLUDED_PIPE_CALLBACK_HANDLER_BCAST_CALLBACK_BCAST_FWD_H
+
+#include "config.h"
+
+namespace vt { namespace pipe { namespace callback {
+
+template <typename MsgT>
+struct CallbackBcast;
+
+}}} /* end namespace vt::pipe::callback */
+
+#endif /*INCLUDED_PIPE_CALLBACK_HANDLER_BCAST_CALLBACK_BCAST_FWD_H*/
---
+++ src/pipe/callback/handler_send/callback_send.fwd.h
@@ -0,0 +1,14 @@
+
+#if !defined INCLUDED_PIPE_CALLBACK_HANDLER_SEND_CALLBACK_SEND_FWD_H
+#define INCLUDED_PIPE_CALLBACK_HANDLER_SEND_CALLBACK_SEND_FWD_H
+
+#include "config.h"
+
+namespace vt { namespace pipe { namespace callback {
+
+template <typename MsgT>
+struct CallbackSend;
+
+}}} /* end namespace vt::pipe::callback */
+
+#endif /*INCLUDED_PIPE_CALLBACK_HANDLER_SEND_CALLBACK_SEND_FWD_H*/
