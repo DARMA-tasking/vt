90b71082250896690b4c9f91cde8488c4e162062
Jonathan Lifflander <jliffla@sandia.gov>
2018-08-02 11:49:03 -0700

    #205: active: traits: add meta code for detecting message type from functor

---
+++ src/utils/static_checks/functor.h
@@ -0,0 +1,32 @@
+
+#if !defined INCLUDED_UTILS_STATIC_CHECKS_FUNCTOR_H
+#define INCLUDED_UTILS_STATIC_CHECKS_FUNCTOR_H
+
+#include "config.h"
+
+namespace vt { namespace util {
+
+template <typename FunctorT>
+struct FunctorTraits;
+
+template <typename FunctorT, typename ReturnT, typename MsgT>
+struct FunctorTraits<ReturnT(FunctorT::*)(MsgT*)> {
+  using FunctorType = FunctorT;
+  using MessageType = MsgT;
+  using ReturnType  = ReturnT;
+};
+
+template <
+  typename FunctorT,
+  typename FunctorFnT = decltype(&FunctorT::operator()),
+  typename Traits = FunctorTraits<FunctorFnT>,
+  typename MsgT = typename Traits::MessageType
+>
+struct FunctorExtractor {
+  using FunctorType = FunctorT;
+  using MessageType = MsgT;
+};
+
+}} /* end namespace vt::util */
+
+#endif /*INCLUDED_UTILS_STATIC_CHECKS_FUNCTOR_H*/
