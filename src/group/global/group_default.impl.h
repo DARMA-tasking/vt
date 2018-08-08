12f84b13a99a88d2e6f1b807a1b64dee7a3b1b66
Jonathan Lifflander <jliffla@sandia.gov>
2018-08-08 16:16:50 -0700

    #205: group: check destination==this_node before sending

--- src/group/global/group_default.impl.h
+++ src/group/global/group_default.impl.h
@@ -7,6 +7,7 @@
 #include "group/global/group_default.h"
 #include "messaging/active.h"
 #include "activefn/activefn.h"
+#include "context/context.h"
 
 namespace vt { namespace group { namespace global {
 
@@ -14,9 +15,18 @@ template <typename MsgT, ActiveTypedFnType<MsgT>* handler>
 /*static*/ void DefaultGroup::sendPhaseMsg(
   PhaseType const& phase, NodeType const& node
 ) {
-  auto msg = makeSharedMessage<MsgT>();
-  envelopeSetTag(msg->env, phase);
-  theMsg()->sendMsg<MsgT, handler>(node, msg);
+  auto const& this_node = theContext()->getNode();
+  if (this_node == node) {
+    auto msg = makeSharedMessage<MsgT>();
+    envelopeSetTag(msg->env, phase);
+    messageRef(msg);
+    handler(msg);
+    messageDeref(msg);
+  } else {
+    auto msg = makeSharedMessage<MsgT>();
+    envelopeSetTag(msg->env, phase);
+    theMsg()->sendMsg<MsgT, handler>(node, msg);
+  }
 }
 
 }}} /* end namespace vt::group::global */
