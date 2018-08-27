6d6c7f124a78d4e06e0a748d7a7ea6ae773640ed
Jonathan Lifflander <jliffla@sandia.gov>
2018-08-27 13:03:22 -0700

    #206: config: add in code class for error codes

---
+++ src/configs/error/code_class.h
@@ -0,0 +1,41 @@
+
+#if !defined INCLUDED_CONFIGS_ERROR_CODE_H
+#define INCLUDED_CONFIGS_ERROR_CODE_H
+
+#include <cstdlib>
+
+namespace vt { namespace error {
+
+enum struct ModuleErrorCode : int8_t {
+  VrtCollection    = 0,
+  VrtContext       = 1,
+  RDMA             = 2,
+  Barrier          = 3,
+  Reduce           = 4,
+  Scatter          = 5,
+  Epoch            = 6,
+  Event            = 7,
+  Group            = 8,
+  Handler          = 9,
+  LB               = 10,
+  Active           = 11,
+  Pipe             = 12,
+  Pool             = 13,
+  Runtime          = 14,
+  Timing           = 15,
+  Termination      = 16,
+  Trace            = 17,
+  Serialization    = 18,
+  Util             = 19,
+  Sequence         = 20,
+  Registry         = 21,
+  Location         = 22,
+  Index            = 23,
+  Mapping          = 24,
+  Worker           = 25,
+  Parameterization = 26
+};
+
+}} /* end namespace vt::error */
+
+#endif /*INCLUDED_CONFIGS_ERROR_CODE_H*/
