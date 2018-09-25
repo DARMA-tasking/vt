49c2cb05d1a339da822c8aac66236b4ace10be39
Jonathan Lifflander <jliffla@sandia.gov>
2018-09-24 20:51:19 -0700

    #210: termination: new msgs inquire/reply rooted epoch term status

--- src/termination/term_msgs.h
+++ src/termination/term_msgs.h
@@ -17,6 +17,35 @@ struct TermMsg : vt::ShortMessage {
   { }
 };
 
+struct TermFinishedMsg : vt::Message {
+
+  TermFinishedMsg() = default;
+  TermFinishedMsg(EpochType const& in_epoch, NodeType const& in_from_node)
+    : epoch_(in_epoch), from_node_(in_from_node)
+  { }
+
+  EpochType getEpoch() const { return epoch_; }
+  NodeType getFromNode() const { return from_node_; }
+
+private:
+  EpochType epoch_    = no_epoch;
+  NodeType from_node_ = uninitialized_destination;
+};
+
+struct TermFinishedReplyMsg : vt::Message {
+  TermFinishedReplyMsg() = default;
+  TermFinishedReplyMsg(EpochType const& in_epoch, bool const& in_finished)
+    : epoch_(in_epoch), finished_(in_finished)
+  { }
+
+  EpochType getEpoch() const { return epoch_; }
+  bool isFinished() const { return finished_; }
+
+private:
+  EpochType epoch_ = no_epoch;
+  bool finished_   = false;
+};
+
 struct TermCounterMsg : vt::ShortMessage {
   EpochType epoch = no_epoch;
   TermCounterType prod = 0, cons = 0;
