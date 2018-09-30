0ec344dab1b9ab1e005248622986268dce485ab1
Jonathan Lifflander <jliffla@sandia.gov>
2018-09-29 18:04:09 -0700

    #210: lblite: realign and indent the lblite constancts

--- src/vrt/collection/balance/greedylb/greedylb_constants.h
+++ src/vrt/collection/balance/greedylb/greedylb_constants.h
@@ -6,12 +6,12 @@
 
 namespace vt { namespace vrt { namespace collection { namespace lb {
 
-static constexpr NodeType const greedy_root = 0;
-static constexpr int32_t const greedy_bin_size = 10;
-static constexpr bool const greedy_auto_threshold_p = true;
-static constexpr double const greedy_tolerance = 5.0f;
-static constexpr double const greedy_threshold_p = 0.3f;
-static constexpr double const greedy_max_threshold_p = 1.004f;
+static constexpr NodeType const greedy_root             = 0;
+static constexpr int32_t  const greedy_bin_size         = 10;
+static constexpr bool     const greedy_auto_threshold_p = true;
+static constexpr double   const greedy_tolerance        = 5.0f;
+static constexpr double   const greedy_threshold_p      = 0.3f;
+static constexpr double   const greedy_max_threshold_p  = 1.004f;
 
 #if backend_check_enabled(parserdes)
   // This is not a mistake, currently default off, even if parserdes is on
