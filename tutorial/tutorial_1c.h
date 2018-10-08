af2537e2861429723d28fe4e646fc38a86d7d3aa
Jonathan Lifflander <jliffla@sandia.gov>
2018-10-07 21:22:20 -0700

    #223: tutorial: implement tutorial examples (merged from `tutorial` branch)

---
+++ tutorial/tutorial_1c.h
@@ -0,0 +1,105 @@
+
+#include "transport.h"
+
+#include <vector>
+
+namespace vt { namespace tutorial {
+
+/*
+ * Some user defined data structures
+ */
+struct Particle {
+  Particle() = default;
+  Particle(double in_x, double in_y, double in_z)
+    : x(in_x), y(in_y), z(in_z)
+  { }
+
+  template <typename SerializerT>
+  void serialize(SerializerT& s) {
+    s | x | y | z;
+  }
+
+  double x, y, z;
+};
+
+struct Data {
+  int a;
+
+  template <typename SerializerT>
+  void serialize(SerializerT& s) {
+    s | a;
+  }
+};
+
+//                  VT Base Message
+//                 \----------------/
+//                  \              /
+struct ParticleMsg : ::vt::Message {
+  ParticleMsg() = default;
+
+  ParticleMsg(int in_x, int in_y, int in_z)
+    : x(in_x), y(in_y), z(in_z)
+  { }
+
+  /*
+   * Implement a serialize method so the std::vector and pointer are properly
+   * serialization on the send
+   */
+  template <typename SerializerT>
+  void serialize(SerializerT& s) {
+    s | particles;
+    s | x | y | z;
+
+    // Serialize a pointer by first serializing whether it is non-null and then
+    // the actual value if it's not non-null
+    bool has_a = a != nullptr;
+    s | has_a;
+    if (has_a) {
+      s | *a;
+    }
+  }
+
+public:
+  std::vector<Particle> particles;
+  int x = 0, y = 0, z = 0;
+  Data* a = nullptr;
+};
+
+// Forward declaration for the active message handler
+static void msgSerialA(ParticleMsg* msg);
+
+// Tutorial code to demonstrate serialization in active message sends
+static inline void activeMessageSerialization() {
+  NodeType const this_node = ::vt::theContext()->getNode();
+  NodeType const num_nodes = ::vt::theContext()->getNumNodes();
+
+  /*
+   * The theMsg()->sendMsgAuto(..) will serialize the message sent to the
+   * destination node if it has a serialize method. If not, it will send the
+   * message as if it is sent directly the sendMsg.
+   */
+
+  if (this_node == 0) {
+    NodeType const to_node = 1;
+    auto msg = ::vt::makeSharedMessage<ParticleMsg>(1,2,3);
+    msg->particles.push_back(Particle{10,11,12});
+    msg->particles.push_back(Particle{13,14,15});
+    ::vt::theMsg()->sendMsgAuto<ParticleMsg,msgSerialA>(to_node, msg);
+  }
+}
+
+// Message handler
+static void msgSerialA(ParticleMsg* msg) {
+  vtAssert(msg->particles.size() == 2, "Should be two particles");
+  vtAssert(msg->x == 1 && msg->y == 2 && msg->z == 3, "Values x,y,z incorrect");
+
+  auto const cur_node = ::vt::theContext()->getNode();
+
+  /* Node 0 sends MyMsg to node 1 in the above code so this should execute on
+   * node 1 */
+  vtAssert(cur_node == 1, "This handler should execute on node 1");
+
+  ::fmt::print("msgSerialA: triggered on node={}\n", cur_node);
+}
+
+}} /* end namespace vt::tutorial */
---
+++ tutorial/tutorial_2a.h
@@ -0,0 +1,60 @@
+
+#include "transport.h"
+
+namespace vt { namespace tutorial {
+
+// Forward declaration for message
+struct MyCollMsg;
+
+//               VT Base Class for a collection     Index (built-in)
+//            \----------------------------------/   \---------/
+//             \                                /     \       /
+struct MyCol : ::vt::vrt::collection::Collection<MyCol,Index1D> {
+
+  void msgHandler(MyCollMsg* msg);
+
+};
+
+//                            VT Base Message for Collections
+//               \-------------------------------------------/
+//                \                                         /
+struct MyCollMsg : ::vt::vrt::collection::CollectionMessage<MyCol> { };
+
+void MyCol::msgHandler(MyCollMsg* msg) {
+  auto cur_node = theContext()->getNode();
+  auto idx = this->getIndex();
+  ::fmt::print("MyCol::msgHandler index={}, node={}\n", idx.x(), cur_node);
+}
+
+// Tutorial code to demonstrate broadcasting a message to the entire system
+static inline void collection() {
+  NodeType const this_node = ::vt::theContext()->getNode();
+  NodeType const num_nodes = ::vt::theContext()->getNumNodes();
+
+  /*
+   * This is an example of the rooted group creation and broadcast to that
+   * group. A group allows the user to create a subset of nodes. A broadcast by
+   * default sends the message to every node in the default group (which
+   * includes all nodes). If a explicit group is set in the envelope, the
+   * broadcast will only arrive on the nodes in that group.
+   */
+
+  if (this_node == 0) {
+    // Range of 32 elements for the collection
+    auto range = ::vt::index::Index1D(32);
+    // Construct the collection: invoked by one node. By default, the elements
+    // will be block mapped to the nodes
+    auto proxy = theCollection()->construct<MyCol>(range);
+
+    // Broadcast a message to the entire collection. The msgHandler will be
+    // invoked on every element to the collection
+    auto msg = ::vt::makeSharedMessage<MyCollMsg>();
+    proxy.broadcast<MyCollMsg,&MyCol::msgHandler>(msg);
+
+    // Send a message to the 5th element of the collection
+    auto msg2 = ::vt::makeSharedMessage<MyCollMsg>();
+    proxy[5].send<MyCollMsg,&MyCol::msgHandler>(msg2);
+  }
+}
+
+}} /* end namespace vt::tutorial */
