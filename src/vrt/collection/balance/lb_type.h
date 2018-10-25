39e3c9c3fe5186b4c50be7951ae1577b44e745a4
Jonathan Lifflander <jliffla@sandia.gov>
2018-10-25 10:17:30 -0700

    vrt coll: add missing hash for vrt::collection::LBType

--- src/vrt/collection/balance/lb_type.h
+++ src/vrt/collection/balance/lb_type.h
@@ -6,6 +6,7 @@
 
 #include <unordered_map>
 #include <string>
+#include <type_traits>
 
 namespace vt { namespace vrt { namespace collection { namespace balance {
 
@@ -15,6 +16,25 @@ enum struct LBType : int8_t {
   HierarchicalLB   = 2,
   RotateLB         = 3
 };
+
+}}}} /* end namespace vt::vrt::collection::balance */
+
+namespace std {
+
+using LBTypeType = vt::vrt::collection::balance::LBType;
+
+template <>
+struct hash<LBTypeType> {
+  size_t operator()(LBTypeType const& in) const {
+    using LBUnderType = typename std::underlying_type<LBTypeType>::type;
+    auto const val = static_cast<LBUnderType>(in);
+    return std::hash<LBUnderType>()(val);
+  }
+};
+
+} /* end namespace std */
+
+namespace vt { namespace vrt { namespace collection { namespace balance {
 
 template <typename=void> std::unordered_map<LBType,std::string> lb_names_ = {
   {LBType::NoLB,           std::string{"NoLB"          }},
