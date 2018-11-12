27a7d3bc49d5c18b81998c132100a95e11096e5c
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-12 14:48:41 -0800

    #220: examples: confrom to index changes (stop using vt::index::*)

--- examples/index_test.cc
+++ examples/index_test.cc
@@ -11,12 +11,12 @@ int main(int argc, char** argv) {
   auto const& my_node = theContext()->getNode();
   auto const& num_nodes = theContext()->getNumNodes();
 
-  index::Index2D idx(2, 3);
-  index::Index2D idx2(5, 10);
-  index::Index2D idx3 = idx2 - idx;
+  Index2D idx(2, 3);
+  Index2D idx2(5, 10);
+  Index2D idx3 = idx2 - idx;
 
-  index::Index1D idx_1d(10);
-  index::Index1D idx_1d_2(20);
+  Index1D idx_1d(10);
+  Index1D idx_1d_2(20);
 
   auto node = mapping::dense1DBlockMap(&idx_1d, &idx_1d_2, num_nodes);
 
@@ -27,15 +27,15 @@ int main(int argc, char** argv) {
   );
 
   int const dim1 = 4, dim2 = 5;
-  index::Index2D idx_a(2, 2);
-  index::Index2D idx_a_max(dim1, dim2);
+  Index2D idx_a(2, 2);
+  Index2D idx_a_max(dim1, dim2);
 
   auto node_a = mapping::dense2DBlockMap(&idx_a, &idx_a_max, num_nodes);
 
   int cur_val = 0;
   for (int i = 0; i < dim1; i++) {
     for (int j = 0; j < dim2; j++) {
-      auto cur_idx = index::Index2D(i,j);
+      auto cur_idx = Index2D(i,j);
       auto lin_idx = mapping::linearizeDenseIndexColMajor(&cur_idx, &idx_a_max);
       fmt::print(
         "idx={}, max={}, lin={}\n",
