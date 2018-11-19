a52808e870c2beca83e03100fd31e62259b34746
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-19 12:15:33 -0800

    #217: tests: add collective, distib construct test for vrt coll

--- tests/unit/collection/test_collection_construct_common.h
+++ tests/unit/collection/test_collection_construct_common.h
@@ -46,18 +46,28 @@ template <typename ColT, typename Tuple>
 struct ConstructParams {
   using IndexType = typename ColT::IndexType;
   using ProxyType = CollectionIndexProxy<ColT,IndexType>;
-  static ProxyType constructTup(IndexType idx,Tuple args) {
-    return construct(idx,args);
+  static ProxyType constructTup(
+    IndexType idx,Tuple args,bool collective = false
+  ) {
+    return construct(collective,idx,args);
   }
   template <typename... Args>
-  static ProxyType construct(IndexType idx, std::tuple<Args...> args) {
-    return unpack(idx,args,std::index_sequence_for<Args...>{});
+  static ProxyType construct(
+    bool collective, IndexType idx, std::tuple<Args...> args
+  ) {
+    return unpack(collective,idx,args,std::index_sequence_for<Args...>{});
   }
   template <std::size_t ...I>
   static ProxyType unpack(
-    IndexType idx, Tuple args, std::index_sequence<I...>
+    bool collective, IndexType idx, Tuple args, std::index_sequence<I...>
   ) {
-    return theCollection()->construct<ColT>(idx,std::get<I>(args)...);
+    if (collective) {
+      return theCollection()->constructCollective<ColT>(
+        idx,std::get<I>(args)...
+      );
+    } else {
+      return theCollection()->construct<ColT>(idx,std::get<I>(args)...);
+    }
   }
 };
 
@@ -81,9 +91,9 @@ TYPED_TEST_P(TestConstruct, test_construct_1) {
   auto const& this_node = theContext()->getNode();
   if (this_node == 0) {
     auto const& col_size = 32;
-    auto range = TestIndex(col_size);
+    auto rng = TestIndex(col_size);
     ParamType args = ConstructTuple<ParamType>::construct();
-    auto proxy = ConstructParams<ColType,ParamType>::constructTup(range,args);
+    auto proxy = ConstructParams<ColType,ParamType>::constructTup(rng,args);
     auto msg = makeSharedMessage<MsgType>();
     proxy.template broadcast<
       MsgType,
@@ -92,7 +102,24 @@ TYPED_TEST_P(TestConstruct, test_construct_1) {
   }
 }
 
+TYPED_TEST_P(TestConstruct, test_construct_distributed_1) {
+  using ColType = TypeParam;
+  using MsgType = typename ColType::MsgType;
+  using ParamType = typename ColType::ParamType;
+
+  auto const& col_size = 32;
+  auto rng = TestIndex(col_size);
+  ParamType args = ConstructTuple<ParamType>::construct();
+  auto proxy = ConstructParams<ColType,ParamType>::constructTup(rng,args,true);
+  auto msg = makeSharedMessage<MsgType>();
+  proxy.template broadcast<
+    MsgType,
+    ConstructHandlers::handler<ColType,MsgType>
+  >(msg);
+}
+
 REGISTER_TYPED_TEST_CASE_P(TestConstruct, test_construct_1);
+REGISTER_TYPED_TEST_CASE_P(TestConstruct, test_construct_distributed_1);
 
 }}} // end namespace vt::tests::unit
 
