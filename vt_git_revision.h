573b7b7db47922afc51d3a932605e050d6cc70c6
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-30 22:56:45 -0800

    #229: build: generate file with git src config

---
+++ vt_git_revision.h
@@ -0,0 +1,13 @@
+
+#if !defined VT_CONFIG_GIT_REVISION_H
+#define VT_CONFIG_GIT_REVISION_H
+
+#include <string>
+
+extern std::string const vt_git_sha1;
+extern std::string const vt_git_exact_tag;
+extern std::string const vt_git_refspec;
+extern std::string const vt_git_description;
+extern std::string const vt_git_clean_status;
+
+#endif /*VT_CONFIG_GIT_REVISION_H*/
