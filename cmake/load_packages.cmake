5a15f63235a579f96b41ae402c2fc8611dba8217
Jonathan Lifflander <jliffla@sandia.gov>
2018-12-01 00:35:13 -0800

    #231: build: add the CLI11 library for arg parsing to cmake

--- cmake/load_packages.cmake
+++ cmake/load_packages.cmake
@@ -9,6 +9,8 @@ include(cmake/load_mpi_package.cmake)
 include(cmake/load_zlib_package.cmake)
 # FMT package
 include(cmake/load_fmt_package.cmake)
+# CLI11 package
+include(cmake/load_cli11_package.cmake)
 # Google test package
 include(cmake/load_gtest_package.cmake)
 
