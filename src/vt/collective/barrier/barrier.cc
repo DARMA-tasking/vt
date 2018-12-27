8f3abc984cfd1c5c8a3e70a012e74f85ad9cc564
Jonathan Lifflander <jliffla@sandia.gov>
2018-12-27 02:12:00 -0800

    barrier: fix not priming the entire scheduler in barrier!

--- src/vt/collective/barrier/barrier.cc
+++ src/vt/collective/barrier/barrier.cc
@@ -133,7 +133,7 @@ void Barrier::waitBarrier(
   barrierUp(is_named, is_wait, barrier, skip_term);
 
   while (not barrier_state.released) {
-    theMsg()->scheduler();
+    vt::runScheduler();
     if (poll_action) {
       poll_action();
     }
