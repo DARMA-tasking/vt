f639f36f07ed62d2fd31df3f894fee780af9bdef
hobywan <hobywan@users.noreply.github.com>
2019-01-31 19:01:41 +0100

    #248: termination: enable additional debug infos on epoch propagation

--- src/vt/termination/termination.cc
+++ src/vt/termination/termination.cc
@@ -492,12 +492,20 @@ bool TerminationDetector::propagateEpoch(TermStateType& state) {
               auto bred            = ::vt::debug::bred();
               auto magenta         = ::vt::debug::magenta();
 
+              // debug additional infos
+              auto const& current  = state.getEpoch();
+              bool const is_rooted = epoch::EpochManip::isRooted(current);
+              bool const has_categ = epoch::EpochManip::hasCategory(current);
+              bool const useDS = has_categ
+                and epoch::EpochManip::category(current) ==
+                    epoch::eEpochCategory::DijkstraScholtenEpoch;
+
               auto f1 = fmt::format(
                 "{}Termination hang detected:{} {}traversals={} epoch={:x} "
-                "produced={}{} {}consumed={}{}\n",
+                "produced={}{} {}consumed={}{} rooted={}, ds={}\n",
                 bred, reset,
                 magenta, state.constant_count, state.getEpoch(), state.g_prod1,
-                reset, magenta, state.g_cons1, reset
+                reset, magenta, state.g_cons1, reset, is_rooted, useDS
               );
               vt_print(term, "{}", f1);
               state.num_print_constant++;
