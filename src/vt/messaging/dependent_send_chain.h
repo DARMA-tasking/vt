f7c52dfd432c243a318bde96ee020e4b8818720d
Phil Miller <pbmille@sandia.gov>
2019-02-05 14:15:29 -0800

    DependentSendChain: simulate an event for the pending send on a user-provided epoch

--- src/vt/messaging/dependent_send_chain.h
+++ src/vt/messaging/dependent_send_chain.h
@@ -79,10 +79,16 @@ class DependentSendChain final {
   void add(EpochType new_epoch, PendingSend&& link) {
     checkInit();
 
+    // Produce an event here, to be consumed when the send gets
+    // released, so that new_epoch doesn't appear to have completed in
+    // the interim
+    theTerm()->produce(new_epoch);
+
     theTerm()->addAction(last_epoch_, [new_epoch, ps = std::move(link)] () mutable {
         theMsg()->pushEpoch(new_epoch);
         ps.release();
         theMsg()->popEpoch(new_epoch);
+        theTerm()->consume(new_epoch);
         // Note no call to finishedEpoch(new_epoch) here, since the caller may be reusing it
         });
     last_epoch_ = new_epoch;
