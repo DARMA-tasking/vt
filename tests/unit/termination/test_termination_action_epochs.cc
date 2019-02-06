a6105dc93f54db7c620cc521cecf1e2826aa07f8
hobywan <hoby.rakotoarivelo@gmail.com>
2019-02-06 11:09:39 +0100

    #248: termination: split test cases to multiple targets

---
+++ tests/unit/termination/test_termination_action_epochs.cc
@@ -0,0 +1,99 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                   test_termination_action_epochs.h
+//                     vt (Virtual Transport)
+//                  Copyright (C) 2018 NTESS, LLC
+//
+// Under the terms of Contract DE-NA-0003525 with NTESS, LLC,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include "test_termination_action_common.h"
+
+namespace vt { namespace tests { namespace unit {
+
+struct TestTermActionCollectEpoch : action::BaseFixture {};
+struct TestTermActionRootedEpoch : action::BaseFixture {};
+
+// collective epochs
+TEST_P(TestTermActionCollectEpoch, test_term_detect_collect_epoch){
+  auto&& sequence = action::newEpochSeq(5);
+
+  for(auto&& ep : sequence){
+    if(channel::me == channel::root){
+      action::compute(ep);
+      channel::trigger(ep);
+    }
+    action::finalize(ep,order_);
+  }
+}
+
+// rooted epochs
+TEST_P(TestTermActionRootedEpoch, test_term_detect_rooted_epoch){
+  if(channel::me == channel::root){
+    auto&& sequence = action::newEpochSeq(5,true,useDS_);
+
+    for(auto&& ep : sequence){
+      action::compute(ep);
+      action::finalize(ep,order_);
+    }
+  }
+}
+
+INSTANTIATE_TEST_CASE_P(
+  InstantiationName, TestTermActionCollectEpoch,
+  ::testing::Combine(
+    ::testing::Values(
+      action::Order::before, action::Order::after, action::Order::misc
+    ),
+    ::testing::Values(false),
+    ::testing::Values(1)
+  )
+);
+
+INSTANTIATE_TEST_CASE_P(
+  InstantiationName, TestTermActionRootedEpoch,
+  ::testing::Combine(
+    ::testing::Values(
+      action::Order::before, action::Order::after, action::Order::misc
+    ),
+    ::testing::Bool(),
+    ::testing::Values(1)
+  )
+);
+
+}}} // end namespace vt::tests::unit
---
+++ tests/unit/termination/test_termination_action_global.cc
@@ -0,0 +1,82 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                  test_termination_action_global.h
+//                     vt (Virtual Transport)
+//                  Copyright (C) 2018 NTESS, LLC
+//
+// Under the terms of Contract DE-NA-0003525 with NTESS, LLC,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include "test_termination_action_common.h"
+
+namespace vt { namespace tests { namespace unit {
+
+struct TestTermActionGlobal : action::BaseFixture {};
+
+TEST_P(TestTermActionGlobal, test_term_detect_broadcast){
+  if(channel::me == channel::root){
+    // start computation
+    channel::broadcast<channel::basicHandler>(1,vt::no_epoch);
+    // trigger detection and check status
+    action::finalize(vt::no_epoch,order_);
+  }
+}
+
+// routed messages
+TEST_P(TestTermActionGlobal, test_term_detect_routed){
+  // there should be at least 3 nodes for this case
+  if(channel::all > 2 and channel::me == channel::root){
+    //start computation
+    action::compute(vt::no_epoch);
+    // trigger detection and check status
+    action::finalize(vt::no_epoch,order_);
+  }
+}
+
+INSTANTIATE_TEST_CASE_P(
+  InstantiationName, TestTermActionGlobal,
+  ::testing::Combine(
+    ::testing::Values(
+      action::Order::before, action::Order::after, action::Order::misc
+    ),
+    ::testing::Values(false),
+    ::testing::Values(1)
+  )
+);
+
+}}} // end namespace vt::tests::unit
