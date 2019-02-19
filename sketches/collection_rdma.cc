b306209fcad5c1f930a2f8dba0c606ab1f276ae5
Jonathan Lifflander <jliffla@sandia.gov>
2019-02-19 14:12:42 -0800

    #277: rdma collection: modify example with new interface

--- sketches/collection_rdma.cc
+++ sketches/collection_rdma.cc
@@ -6,8 +6,8 @@ struct MyMsg : vt::Message { };
 struct MyCol : vt::Collection<MyCol,vt::Index1D> {
   void method(MyMsg* msg) { }
 
-  vt::Fetch<std::vector<int>> getMyData(MyMsg* msg) {
-    return vt::Fetch<std::vector<int>>(my_data_);
+  void getMyData(vt::Fetch<std::vector<int>>* msg) {
+    msg->set(std::move(my_data_));
   }
 
 private:
@@ -20,12 +20,11 @@ int main(int argc, char** argv) {
   if (vt::theContext()->getNode() == 0) {
     auto proxy = vt::theCollection()->construct<MyCol>(vt::Index1D(10));
     auto msg = vt::makeMessage<MyMsg>();
-    proxy.broadcast<MyMsg,&MyCol::method>(msg.get());
+    proxy.broadcast<MyMsg,&MyCol::methond>(msg.get());
 
-    auto msg2 = vt::makeMessage<MyMsg>();
-    proxy[i].fetch<MyMsg,&MyCol::getMyData>(
-      msg2.get(),[](vt::Fetch<std::vector<int>> data) {
-        for (auto&& elm : data) {
+    proxy[i].fetch<vt::Fetch<std::vector<int>>,&MyCol::getMyData>(
+      [](vt::Fetch<std::vector<int>> data) {
+        for (auto&& elm : *data) {
           fmt::print("printing fetched data elm={}\n",elm);
         }
       }
