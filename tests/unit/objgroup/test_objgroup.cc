21dd44db4e03a44555d45673d167c446866acce1
Hoby Rakotoarivelo <hoby.rakotoarivelo@gmail.com>
2019-03-12 16:22:37 +0100

    #290: objgroup: update proxy send test to use scheduler

--- tests/unit/objgroup/test_objgroup.cc
+++ tests/unit/objgroup/test_objgroup.cc
@@ -118,12 +118,11 @@ TEST_F(TestObjGroup, test_proxy_schedule) {
 TEST_F(TestObjGroup, test_proxy_construct_send) {
 
   auto const my_node = vt::theContext()->getNode();
-  auto const epoch = vt::theTerm()->makeEpochCollective();
 
   // create object group using object constructor or a raw pointer.
   // EDIT: cannot create group using a smart pointer (unique, shared)
   // due to overload resolution issue related to variadic constructor.
-  MyObjB obj(0xFFFFFFFF);
+  MyObjB obj(1);
   auto proxy1 = vt::theObjGroup()->makeCollective<MyObjA>();
   auto proxy2 = vt::theObjGroup()->makeCollective<MyObjB>(&obj);
   auto proxy3 = vt::theObjGroup()->makeCollective<MyObjA>();
@@ -137,23 +136,23 @@ TEST_F(TestObjGroup, test_proxy_construct_send) {
     proxy3[0].send<MyMsg, &MyObjA::handler>();
   }
 
-  // check received messages for each group
-  vt::theTerm()->addAction(epoch, [=]{
-    auto obj1 = proxy1.get();
-    auto obj2 = proxy2.get();
-    auto obj3 = proxy3.get();
-
-    switch (my_node) {
-      case 0:  EXPECT_EQ(obj1->recv_, 2); break;
-      case 1:  EXPECT_EQ(obj1->recv_, 1); break;
-      default: EXPECT_EQ(obj1->recv_, 0); break;
-    }
-    EXPECT_EQ(obj2->recv_, 1);
-    EXPECT_EQ(obj3->recv_, my_node == 0 ? 1 : 0);
-  });
-
   vt::theCollective()->barrier();
-  vt::theTerm()->finishedEpoch(epoch);
+
+  // wait for all events to be processed (for all proxies)
+  while (vt::theObjGroup()->scheduler()) {}
+
+  // check received messages for each group
+  auto obj1 = proxy1.get();
+  auto obj2 = proxy2.get();
+  auto obj3 = proxy3.get();
+
+  switch (my_node) {
+    case 0:  EXPECT_EQ(obj1->recv_, 2); break;
+    case 1:  EXPECT_EQ(obj1->recv_, 1); break;
+    default: EXPECT_EQ(obj1->recv_, 0); break;
+  }
+  EXPECT_EQ(obj2->recv_, 1);
+  EXPECT_EQ(obj3->recv_, my_node == 0 ? 1 : 0);
 }
 
 TEST_F(TestObjGroup, test_proxy_reduce) {
