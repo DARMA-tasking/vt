aeccc5ea32d8e19a165cc83afcc9f24afa8e7e25
Phil Miller <pbmille@sandia.gov>
2019-04-15 09:37:12 -0700

    #246: Add dependent send chaining code

---
+++ src/vt/messaging/dependent_send_chain.h
@@ -0,0 +1,127 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                     dependent_send_chain.h
+//                     vt (Virtual Transport)
+//                  Copyright (C) 2018 NTESS, LLC
+//
+// Under the terms of Contract DE-NA-0003525 with NTESS, LLC,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#if !defined INCLUDED_VT_MESSAGING_DEPENDENT_SEND_CHAIN_H
+#define INCLUDED_VT_MESSAGING_DEPENDENT_SEND_CHAIN_H
+
+#include "vt/config.h"
+#include "vt/messaging/pending_send.h"
+#include "vt/epoch/epoch.h"
+
+#include <functional>
+
+namespace vt { namespace messaging {
+
+struct PendingClosure {
+  PendingClosure(PendingSend&& in_pending, EpochType in_epoch)
+    : pending_(std::move(in_pending)), epoch_(in_epoch)
+  { }
+  PendingClosure(PendingClosure const&) = delete;
+  PendingClosure(PendingClosure&& in) = default;
+
+  void operator()() {
+    pending_.release();
+    theTerm()->consume(epoch_);
+  }
+
+private:
+  PendingSend pending_;
+  EpochType epoch_ = no_epoch;
+};
+
+class DependentSendChain final {
+ public:
+  DependentSendChain() { }
+
+  // Add a task to the chain of work to be run in the specified epoch,
+  // with subsequent tasks dependent on all work occuring in the
+  // specified epoch
+  void add(EpochType new_epoch, PendingSend&& link) {
+    checkInit();
+
+    // Produce an event here, to be consumed when the send gets
+    // released, so that new_epoch doesn't appear to have completed in
+    // the interim
+    theTerm()->produce(new_epoch);
+
+    theTerm()->addActionUnique(last_epoch_,
+                               PendingClosure(std::move(link),
+                                              new_epoch));
+    last_epoch_ = new_epoch;
+  }
+
+  void done() {
+    reset();
+  }
+
+  DependentSendChain            (const DependentSendChain&) = delete;
+  DependentSendChain            (DependentSendChain&&)      = default;
+  DependentSendChain& operator= (const DependentSendChain&) = delete;
+  DependentSendChain& operator= (DependentSendChain&&)      = default;
+
+  ~DependentSendChain() = default;
+
+ private:
+  void checkInit() {
+    // Done here rather than the constructor to make static
+    // initialization of an instance safe
+    if (last_epoch_ == no_epoch) {
+      reset();
+    }
+  }
+
+  void reset() {
+    // Set up a 'closed' epoch so that we keep an invariant of always
+    // having an epoch to call addAction on, rather than edge cases of
+    // whether we've made one yet or not
+    last_epoch_ = theTerm()->makeEpochRooted();
+    theTerm()->finishedEpoch(last_epoch_);
+  }
+
+  EpochType last_epoch_ = no_epoch;
+};
+
+}} /* end namespace vt::messaging */
+
+#endif /*INCLUDED_VT_MESSAGING_DEPENDENT_SEND_CHAIN_H*/
---
+++ tests/unit/termination/test_term_chaining.cc
@@ -0,0 +1,165 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                      test_term_chaining.cc
+//                     vt (Virtual Transport)
+//                  Copyright (C) 2018 NTESS, LLC
+//
+// Under the terms of Contract DE-NA-0003525 with NTESS, LLC,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "test_parallel_harness.h"
+#include "data_message.h"
+
+#include "vt/transport.h"
+#include "vt/messaging/dependent_send_chain.h"
+
+namespace vt { namespace tests { namespace unit {
+
+using namespace vt;
+using namespace vt::tests::unit;
+
+struct TestTermChaining : TestParallelHarness {
+  using TestMsg = EpochMessage;
+
+  static int32_t handler_count;
+
+  static vt::messaging::DependentSendChain chain;
+  static vt::EpochType epoch;
+
+  static void test_handler_reflector(TestMsg* msg) {
+    fmt::print("reflector run\n");
+
+    handler_count = 12;
+
+    EXPECT_EQ(theContext()->getNode(), 1);
+    auto msg2 = makeSharedMessage<TestMsg>();
+    theMsg()->sendMsg<TestMsg, test_handler_response>(0, msg2);
+  }
+
+  static void test_handler_response(TestMsg* msg) {
+    fmt::print("response run\n");
+
+    EXPECT_EQ(theContext()->getNode(), 0);
+    EXPECT_EQ(handler_count, 0);
+    handler_count++;
+  }
+
+  static void test_handler_chainer(TestMsg* msg) {
+    fmt::print("chainer run\n");
+
+    EXPECT_EQ(handler_count, 12);
+    handler_count++;
+    auto msg2 = makeSharedMessage<TestMsg>();
+    theMsg()->sendMsg<TestMsg, test_handler_chained>(0, msg2);
+  }
+
+  static void test_handler_chained(TestMsg* msg) {
+    fmt::print("chained run\n");
+
+    EXPECT_EQ(theContext()->getNode(), 0);
+    EXPECT_EQ(handler_count, 1);
+    handler_count = 4;
+  }
+
+  static void start_chain() {
+    EpochType epoch1 = theTerm()->makeEpochRooted();
+    vt::theMsg()->pushEpoch(epoch1);
+    auto msg = makeSharedMessage<TestMsg>();
+    chain.add(epoch1, theMsg()->sendMsg<TestMsg, test_handler_reflector>(1, msg));
+    vt::theMsg()->popEpoch(epoch1);
+    vt::theTerm()->finishedEpoch(epoch1);
+
+    EpochType epoch2 = theTerm()->makeEpochRooted();
+    vt::theMsg()->pushEpoch(epoch1);
+    auto msg2 = makeSharedMessage<TestMsg>();
+    chain.add(epoch2, theMsg()->sendMsg<TestMsg, test_handler_chainer>(1, msg2));
+    vt::theMsg()->popEpoch(epoch2);
+    vt::theTerm()->finishedEpoch(epoch2);
+
+    chain.done();
+  }
+
+  static void run_to_term() {
+    bool finished = false;
+
+    theTerm()->addAction(epoch, [&finished]{ finished = true; });
+
+    while (!finished) {
+      runScheduler();
+    }
+  }
+};
+
+/*static*/ int32_t TestTermChaining::handler_count = 0;
+/*static*/ vt::messaging::DependentSendChain TestTermChaining::chain;
+/*static*/ vt::EpochType TestTermChaining::epoch;
+
+TEST_F(TestTermChaining, test_termination_chaining_1) {
+  auto const& this_node = theContext()->getNode();
+  auto const& num_nodes = theContext()->getNumNodes();
+
+  if (num_nodes != 2)
+    return;
+
+  epoch = theTerm()->makeEpochCollective();
+
+  fmt::print("global collective epoch {:x}\n", epoch);
+
+  if (this_node == 0) {
+    theMsg()->pushEpoch(epoch);
+    start_chain();
+    theTerm()->finishedEpoch(epoch);
+    theMsg()->popEpoch(epoch);
+    fmt::print("before run 1\n");
+    run_to_term();
+    fmt::print("after run 1\n");
+
+    EXPECT_EQ(handler_count, 4);
+  } else {
+    theMsg()->pushEpoch(epoch);
+    theTerm()->finishedEpoch(epoch);
+    theMsg()->popEpoch(epoch);
+    run_to_term();
+    EXPECT_EQ(handler_count, 13);
+  }
+}
+
+}}} // end namespace vt::tests::unit
