16611d184c5e8b7fcaf080345383cccfea137544
Jonathan Lifflander <jliffla@sandia.gov>
2019-04-16 11:39:11 -0700

    #315: reduce: fix minor whitespace alignment

--- src/vt/collective/reduce/reduce_state.h
+++ src/vt/collective/reduce/reduce_state.h
@@ -65,7 +65,7 @@ struct ReduceState {
 
   ReduceVecType msgs               = {};
   TagType tag_                     = no_tag;
-  SequentialIDType seq_id_          = no_seq_id;
+  SequentialIDType seq_id_         = no_seq_id;
   ReduceNumType num_contrib_       = 1;
   ReduceNumType num_local_contrib_ = 0;
   HandlerType combine_handler_     = uninitialized_handler;
