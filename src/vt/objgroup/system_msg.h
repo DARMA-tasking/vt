22430a85196748fe0f4531e0e4267be91892e4e2
Jonathan Lifflander <jliffla@sandia.gov>
2019-07-08 16:08:45 -0700

    #410: objgroup: add missing sytem msg header

---
+++ src/vt/objgroup/system_msg.h
@@ -0,0 +1,68 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                          system_msg.h
+//                     vt (Virtual Transport)
+//                  Copyright (C) 2018 NTESS, LLC
+//
+// Under the terms of Contract DE-NA-0003525 with NTESS, LLC,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#if !defined INCLUDED_VT_OBJGROUP_SYSTEM_MSG_H
+#define INCLUDED_VT_OBJGROUP_SYSTEM_MSG_H
+
+#include "vt/config.h"
+#include "vt/messaging/message/message.h"
+#include "vt/objgroup/proxy/proxy_objgroup_elm.h"
+
+namespace vt { namespace objgroup {
+
+template <typename ObjT>
+struct ReleaseMsg : vt::Message {
+
+  ReleaseMsg() = default;
+  ReleaseMsg(proxy::ProxyElm<ObjT> in_proxy, EpochType in_epoch)
+    : proxy_(in_proxy), epoch_(in_epoch)
+  { }
+
+  proxy::ProxyElm<ObjT> proxy_ = {};
+  EpochType epoch_ = no_epoch;
+};
+
+}} /* end namespace vt::objgroup */
+
+#endif /*INCLUDED_VT_OBJGROUP_SYSTEM_MSG_H*/
