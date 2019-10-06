c1c9d64e534a9f43e87569646d4765cdc4451996
Ulrich Hetmaniuk <ulrich.hetmaniuk@gmail.com>
2019-10-05 22:00:51 -0700

    Fix printing of directory and file names for tracing.

--- src/vt/trace/trace.h
+++ src/vt/trace/trace.h
@@ -181,7 +181,6 @@ private:
   TraceContainerType traces_;
   TraceStackType open_events_;
   TraceEventIDType cur_event_   = 1;
-  std::string dir_name_         = "";
   std::string prog_name_        = "";
   std::string trace_name_       = "";
   bool enabled_                 = true;
