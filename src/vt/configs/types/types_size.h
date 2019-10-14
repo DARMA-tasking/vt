8cbaad281b9081a36d9fb9bdd033edee3ab47e25
Jonathan Lifflander <jliffla@sandia.gov>
2019-10-14 14:29:42 -0700

    #474: config: add PriorityLevelType

--- src/vt/configs/types/types_size.h
+++ src/vt/configs/types/types_size.h
@@ -63,6 +63,8 @@ static constexpr BitCountType const
     group_num_bits = utils::BitCounter<GroupType>::value;
 static constexpr BitCountType const
     priority_num_bits = BitCounterType<PriorityType>::value;
+static constexpr BitCountType const
+    priority_level_num_bits = BitCounterType<PriorityLevelType>::value;
 
 }  // end namespace vt
 
