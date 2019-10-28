c52fb04c41238c22aef142b5921a599cf6c09af9
Paul Nathan Stickney <pstickne@gmail.com>
2019-10-27 19:12:19 -0700

    #521: eliminate lambda-closure (and VirtualMsgPtr entirely)
    - Folded VirtualMsgPtr into MsgSharedPtr making
      'to' and 'toVirtual' synonyms.
    
      If the wrapped message type has a non-trivial destructor
      then a proxy 'deref' object is is used so that all derefs
      will apply to the original T-type (and thus invoke the
      delete-expression appropriately).
    
      This type-erasure ensures that 'messageDeref' is always
      invoked upon the correct T for expeted delete-expr behavior.
    
    - Remove unused code and unified methods
    
    - Ensure move contructor and added move-assignment operator
      (steal resources!!!!)
    
    - Remove messageDeref<Msg<T>> templates; if having a MsgSharedPtr, use it..

--- src/vt/messaging/message/refs.h
+++ src/vt/messaging/message/refs.h
@@ -51,14 +51,8 @@
 
 namespace vt {
 
-template <template <typename> class MsgPtrT, typename MsgT>
-void messageRef(MsgPtrT<MsgT> msg);
-
 template <typename MessageT>
 void messageRef(MessageT* msg);
-
-template <template <typename> class MsgPtrT, typename MsgT>
-void messageDeref(MsgPtrT<MsgT> msg);
 
 template <typename MessageT>
 void messageDeref(MessageT* msg);
