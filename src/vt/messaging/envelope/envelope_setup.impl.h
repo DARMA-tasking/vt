2ec8de5facf558d1cb8a6f88350359509d6569a6
Jonathan Lifflander <jliffla@sandia.gov>
2019-10-29 21:26:55 -0700

    #474: msg: start with default priority

--- src/vt/messaging/envelope/envelope_setup.impl.h
+++ src/vt/messaging/envelope/envelope_setup.impl.h
@@ -66,6 +66,10 @@ inline void envelopeInit(Env& env) {
   envelopeSetHandler(env, uninitialized_handler);
   envelopeSetRef(env, not_shared_message);
   envelopeSetGroup(env);
+# if backend_check_enabled(priorities)
+  envelopeSetPriority(env, min_priority);
+  envelopeSetPriorityLevel(env, 0);
+# endif
 }
 
 inline void envelopeInitEmpty(Envelope& env) {
