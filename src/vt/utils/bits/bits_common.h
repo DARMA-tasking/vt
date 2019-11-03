50acc3dec3eae32f5cce9c5cb58d75ddccc98ac8
Paul Nathan Stickney <pstickne@gmail.com>
2019-11-02 18:53:21 -0700

    #541: config: move 'pretty print message' to cc
    - Code does not have benefit being in header so..
      moved to compile-once-CC-file.
    
      And then FIXED a number of headers/code that broke
      due to having the transitive dependency spider web
      removed.
    
    - Removed last gobal usage of unistd.h and cstdio
      from headers - should add wiki note about the 'allowed headers'
      that may be included in .h files.. so much global pollution.
    
    - As added bonus, uses cached std::string hostname.
    
    - Fix broken header includes exposed when headers stopped
      being included from 'pretty_print_message.h'
    
      Now 'types_headers.h' should be resolveable at the TOP of the header graph;
      updated bits_counter to accomodate.

--- src/vt/utils/bits/bits_common.h
+++ src/vt/utils/bits/bits_common.h
@@ -45,7 +45,6 @@
 #if !defined INCLUDED_BITS_COMMON
 #define INCLUDED_BITS_COMMON
 
-#include "vt/config.h"
 #include "vt/utils/bits/bits_counter.h"
 #include "vt/utils/bits/bits_packer.h"
 
@@ -53,6 +52,7 @@ namespace vt {
 
 template <typename T>
 using BitCounterType = utils::BitCounter<T>;
+
 using BitPackerType = utils::BitPacker;
 
 }  // end namespace vt
