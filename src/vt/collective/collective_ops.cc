78a518c89d1d4377bc58e39a5d49062fb72112cf
Jonathan Lifflander <jliffla@sandia.gov>
2019-12-16 10:40:23 -0800

    NOMERGE: add glibc hooks for memory tracking

--- src/vt/collective/collective_ops.cc
+++ src/vt/collective/collective_ops.cc
@@ -51,6 +51,7 @@
 #include <memory>
 #include <cstdlib>
 #include <mpi.h>
+#include <malloc.h>
 
 namespace vt {
 
@@ -168,3 +169,49 @@ HandlerType CollectiveAnyOps<instance>::registerHandler(ActiveClosureFnType fn) 
 template struct CollectiveAnyOps<collective_default_inst>;
 
 } /* end namespace vt */
+
+
+static void vtTrackMemoryInit();
+static void *my_malloc_hook(size_t, const void *);
+static void my_free_hook(void*, const void *);
+
+static void vtTrackMemoryInit() {
+  old_malloc_hook = __malloc_hook;
+  old_free_hook = __free_hook;
+  __malloc_hook = my_malloc_hook;
+  __free_hook = my_free_hook;
+}
+
+static void* my_malloc_hook (size_t size, const void *caller) {
+  void *result;
+  /* Restore all old hooks */
+  __malloc_hook = old_malloc_hook;
+  __free_hook = old_free_hook;
+  /* Call recursively */
+  result = malloc (size);
+  /* Save underlying hooks */
+  old_malloc_hook = __malloc_hook;
+  old_free_hook = __free_hook;
+  /* printf might call malloc, so protect it too. */
+  printf ("malloc (%u) returns %p\n", (unsigned int) size, result);
+  /* Restore our own hooks */
+  __malloc_hook = my_malloc_hook;
+  __free_hook = my_free_hook;
+  return result;
+}
+
+static void my_free_hook(void *ptr, const void *caller) {
+  /* Restore all old hooks */
+  __malloc_hook = old_malloc_hook;
+  __free_hook = old_free_hook;
+  /* Call recursively */
+  free (ptr);
+  /* Save underlying hooks */
+  old_malloc_hook = __malloc_hook;
+  old_free_hook = __free_hook;
+  /* printf might call free, so protect it too. */
+  printf ("freed pointer %p\n", ptr);
+  /* Restore our own hooks */
+  __malloc_hook = my_malloc_hook;
+  __free_hook = my_free_hook;
+}
