34bdafe2ceb34833d7e983016d6344bfc5b33338
Jonathan Lifflander <jliffla@sandia.gov>
2019-12-17 16:51:30 -0800

    #621: test: new TD resource test with DS+Wave+Coll altogether

--- tests/unit/termination/test_term_cleanup.cc
+++ tests/unit/termination/test_term_cleanup.cc
@@ -91,6 +91,7 @@ TEST_F(TestTermCleanup, test_termination_cleanup_1) {
 
     EXPECT_LT(theTerm()->getEpochState().size(), 2);
     EXPECT_EQ(theTerm()->getEpochWaitSet().size(), 0);
+    EXPECT_EQ(theTerm()->getDSTermMap().size(), 0);
     EXPECT_LT(theTerm()->getEpochReadySet().size(), 2);
   }
 
@@ -100,7 +101,65 @@ TEST_F(TestTermCleanup, test_termination_cleanup_1) {
 
   EXPECT_LT(theTerm()->getEpochState().size(), 2);
   EXPECT_EQ(theTerm()->getEpochWaitSet().size(), 0);
+  EXPECT_EQ(theTerm()->getDSTermMap().size(), 0);
   EXPECT_LT(theTerm()->getEpochReadySet().size(), 2);
 }
+
+TEST_F(TestTermCleanup, test_termination_cleanup_2) {
+  auto const this_node = theContext()->getNode();
+  auto const num_nodes = theContext()->getNumNodes();
+
+  if (num_nodes < 2) {
+    return;
+  }
+
+  int const num_epochs = 100;
+
+  for (int i = 0; i < num_epochs; i++) {
+    EpochType const coll_epoch = theTerm()->makeEpochCollective();
+    EpochType const root_epoch = theTerm()->makeEpochRootedDS(false,no_epoch);
+    EpochType const wave_epoch = theTerm()->makeEpochRootedWave(false,no_epoch);
+    bool coll_done = false;
+    bool root_done = false;
+    bool wave_done = false;
+    //fmt::print("global collective epoch {:x}\n", epoch);
+
+    NodeType const next = this_node + 1 < num_nodes ? this_node + 1 : 0;
+
+    for (int j = 0; j < 5; j++) {
+      auto msg = makeMessage<TestMsgType>();
+      envelopeSetEpoch(msg->env, coll_epoch);
+      theMsg()->sendMsg<TestMsgType, handler>(next, msg.get());
+    }
+    for (int j = 0; j < 5; j++) {
+      auto msg = makeMessage<TestMsgType>();
+      envelopeSetEpoch(msg->env, root_epoch);
+      theMsg()->sendMsg<TestMsgType, handler>(next, msg.get());
+    }
+    for (int j = 0; j < 5; j++) {
+      auto msg = makeMessage<TestMsgType>();
+      envelopeSetEpoch(msg->env, wave_epoch);
+      theMsg()->sendMsg<TestMsgType, handler>(next, msg.get());
+    }
+
+    theTerm()->finishedEpoch(coll_epoch);
+    theTerm()->finishedEpoch(root_epoch);
+    theTerm()->finishedEpoch(wave_epoch);
+    theTerm()->addAction(coll_epoch, [&]{ coll_done = true; });
+    theTerm()->addAction(root_epoch, [&]{ root_done = true; });
+    theTerm()->addAction(wave_epoch, [&]{ wave_done = true; });
+    do vt::runScheduler(); while (not coll_done or not root_done or not wave_done);
+  }
+
+  while (not vt::rt->isTerminated() or not vt::theSched()->isIdle()) {
+    vt::runScheduler();
+  }
+
+  EXPECT_LT(theTerm()->getEpochState().size(), 2);
+  EXPECT_EQ(theTerm()->getEpochWaitSet().size(), 0);
+  EXPECT_EQ(theTerm()->getDSTermMap().size(), 0);
+  EXPECT_LT(theTerm()->getEpochReadySet().size(), 2);
+}
+
 
 }}} // end namespace vt::tests::unit
