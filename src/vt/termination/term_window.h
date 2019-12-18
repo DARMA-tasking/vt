098431ab40ce640ad9d5e0b36aa34d0e5a3c4a77
Jonathan Lifflander <jliffla@sandia.gov>
2019-12-17 19:23:33 -0800

    #621: termination: fix duplicated and typo in comment

--- src/vt/termination/term_window.h
+++ src/vt/termination/term_window.h
@@ -70,10 +70,14 @@ public:
   bool inWindow(EpochType const& epoch) const;
   bool isTerminated(EpochType const& epoch) const;
   void addEpoch(EpochType const& epoch);
+
+  /*
+   * Track terminated, previously active epoch by adding it to the set
+   */
   void closeEpoch(EpochType const& epoch);
 
   /*
-   * Terminated an active epoch by adding it to the sett
+   * Get the size of the current terminated epochs set
    */
   std::size_t getSize() const { return terminated_epochs_.size(); }
 
