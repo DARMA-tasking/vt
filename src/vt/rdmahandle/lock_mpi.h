e2f0e77effd5e9f26f858812b8868cf8b7eaa291
Jonathan Lifflander <jliffla@sandia.gov>
2020-01-28 12:27:58 -0800

    #653: rdma: move MPI lock out of holder

--- src/vt/rdmahandle/lock_mpi.h
+++ src/vt/rdmahandle/lock_mpi.h
@@ -2,7 +2,7 @@
 //@HEADER
 // *****************************************************************************
 //
-//                                   holder.h
+//                                  lock_mpi.h
 //                           DARMA Toolkit v. 1.0.0
 //                       DARMA/vt => Virtual Transport
 //
@@ -42,86 +42,35 @@
 //@HEADER
 */
 
-#if !defined INCLUDED_VT_RDMAHANDLE_HOLDER_H
-#define INCLUDED_VT_RDMAHANDLE_HOLDER_H
+#if !defined INCLUDED_VT_RDMAHANDLE_LOCK_MPI_H
+#define INCLUDED_VT_RDMAHANDLE_LOCK_MPI_H
 
 #include "vt/config.h"
-#include "vt/rdmahandle/common.h"
-#include "vt/rdmahandle/request_holder.h"
-#include "vt/rdmahandle/lock_mpi.h"
-
-#include <unordered_map>
-#include <vector>
 
 namespace vt { namespace rdma {
 
-struct Manager;
-
-template <typename T, HandleEnum E>
-struct Holder {
-  Holder() = default;
-
-  bool ready() const { return ready_; }
-
-  friend struct Manager;
-
-private:
-  template <typename ProxyT>
-  void addHandle(HandleKey key, ElemType lin, Handle<T,E> han, std::size_t in_size) {
-    handles_[lin] = han;
-    key_ = key;
-    size_ += in_size;
+struct LockMPI {
+  LockMPI(bool in_exclusive, vt::NodeType in_rank, MPI_Win in_window)
+    : exclusive_(in_exclusive),
+      rank_(in_rank),
+      window_(in_window)
+  {
+    auto lock_type = exclusive_ ? MPI_LOCK_EXCLUSIVE : MPI_LOCK_SHARED;
+    MPI_Win_lock(lock_type, rank_, 0, window_);
   }
+  LockMPI(LockMPI const&) = delete;
+  LockMPI(LockMPI&&) = default;
 
-  void allocateDataWindow(std::size_t const in_len = 0) {
-    std::size_t len = in_len == 0 ? size_ : in_len;
-    fmt::print("allocate: len={}\n", len);
-    MPI_Alloc_mem(len * sizeof(T), MPI_INFO_NULL, &data_base_);
-    MPI_Win_create(data_base_, len * sizeof(T), sizeof(T), MPI_INFO_NULL, MPI_COMM_WORLD, &data_window_);
-    ready_ = true;
-  }
-
-public:
-  template <typename Callable>
-  void access(bool exclusive, Callable fn) {
-    auto this_node = theContext()->getNode();
-    auto lock_type = exclusive ? MPI_LOCK_EXCLUSIVE : MPI_LOCK_SHARED;
-    MPI_Win_lock(lock_type, this_node, 0, data_window_);
-    fn(data_base_);
-    MPI_Win_unlock(this_node, data_window_);
-  }
-
-  RequestHolder rget(
-    vt::NodeType node, bool exclusive, T* ptr, std::size_t len, int offset
-  ) {
-    auto mpi_type = TypeMPI<T>::getType();
-    RequestHolder r;
-    {
-      LockMPI _scope_lock(exclusive, node, data_window_);
-      MPI_Rget(ptr, len, mpi_type, node, offset, len, mpi_type, data_window_, r.add());
-    }
-    return r;
-  }
-
-  void get(vt::NodeType node, bool exclusive, T* ptr, std::size_t len, int offset) {
-    rget(node, exclusive, ptr, len, offset);
+  ~LockMPI() {
+    MPI_Win_unlock(rank_, window_);
   }
 
 private:
-  HandleKey key_;
-  MPI_Win data_window_;
-  MPI_Win idx_window_;
-  MPI_Win control_window_;
-  T* data_base_ = nullptr;
-  T* idx_base_ = nullptr;
-  T* control_base_ = nullptr;
-  std::size_t size_ = 0;
-  bool ready_ = false;
-
-private:
-  std::unordered_map<ElemType, Handle<T,E>> handles_;
+  bool exclusive_ = false;
+  vt::NodeType rank_ = vt::uninitialized_destination;
+  MPI_Win window_;
 };
 
 }} /* end namespace vt::rdma */
 
-#endif /*INCLUDED_VT_RDMAHANDLE_HOLDER_H*/
+#endif /*INCLUDED_VT_RDMAHANDLE_LOCK_MPI_H*/
