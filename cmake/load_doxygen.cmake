a7ec146560d4cee4bd9e2bf7d52b44be199d3697
Jonathan Lifflander <jliffla@sandia.gov>
2020-02-03 12:52:26 -0800

    #645: docs: remove sphinx

--- cmake/load_doxygen.cmake
+++ cmake/load_doxygen.cmake
@@ -25,30 +25,6 @@ if (${vt_doxygen_enabled})
       VERBATIM
     )
 
-  #   find_package(Sphinx REQUIRED)
-
-  #   set(SPHINX_SOURCE ${CMAKE_CURRENT_SOURCE_DIR})
-  #   set(SPHINX_BUILD ${CMAKE_CURRENT_BINARY_DIR}/sphinx)
-  #   set(SPHINX_INDEX_FILE ${SPHINX_BUILD}/index.html)
-
-  #   add_custom_command(
-  #     OUTPUT ${SPHINX_INDEX_FILE}
-	#     COMMAND ${SPHINX_EXECUTABLE} -b html
-	# 	  # Tell Breathe where to find the Doxygen output
-	# 	  -Dbreathe_projects.vt=${CMAKE_CURRENT_BINARY_DIR}/docs/xml
-	# 	  ${SPHINX_SOURCE} ${SPHINX_BUILD}
-	#     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-	#     DEPENDS
-	# 	  # Other docs files you want to track should go here (or in some variable)
-	# 	  ${CMAKE_CURRENT_SOURCE_DIR}/index.rst
-	# 	  ${DOXYGEN_INDEX_FILE}
-	#     MAIN_DEPENDENCY ${SPHINX_SOURCE}/conf.py
-	#     COMMENT "Generating documentation with Sphinx"
-  #   )
-
-  # # Nice named target so we can run the job easily
-  # add_custom_target(sphinx ALL DEPENDS ${SPHINX_INDEX_FILE})
-
   else()
     message(FATAL_ERROR "Doxygen could not be found even though it was enabled")
   endif()
