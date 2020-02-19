bb3a0e159ad38fafc8aeba9a6959065d1fc4a403
Jonathan Lifflander <jliffla@sandia.gov>
2020-02-19 12:42:01 -0800

    #653: rdma: add types back in collection test

--- tests/unit/rdma/test_rdma_collection_handle.cc
+++ tests/unit/rdma/test_rdma_collection_handle.cc
@@ -202,24 +202,20 @@ TYPED_TEST_P(TestRDMAHandleCollection, test_rdma_handle_collection_1) {
 }
 
 using RDMACollectionTestTypes = testing::Types<
-  int// ,
-  // double,
-  // float,
-  // int32_t,
-  // int64_t,
-  // uint64_t,
-  // int64_t,
-  // int16_t,
-  // uint16_t
+  int,
+  double,
+  float,
+  int32_t,
+  int64_t,
+  uint64_t,
+  int64_t,
+  int16_t,
+  uint16_t
 >;
 
 REGISTER_TYPED_TEST_CASE_P(
   TestRDMAHandleCollection,
   test_rdma_handle_collection_1
-  // test_rdma_handle_set_2,
-  // test_rdma_handle_set_3,
-  // test_rdma_handle_set_4,
-  // test_rdma_handle_set_5
 );
 
 INSTANTIATE_TYPED_TEST_CASE_P(
