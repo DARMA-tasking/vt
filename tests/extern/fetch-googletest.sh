fcf5ddaded95ae01012be29383034638153ba581
Paul Nathan Stickney <pstickne@gmail.com>
2020-03-08 12:31:47 -0700

    #595 add extern + googletest fetch
    - Little script to fetch + cleanup the googletest probject
      to distribute with the VT project.

---
+++ tests/extern/fetch-googletest.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+GTEST_BRANCHTAG=release-1.10.0
+
+# Fetch googletest from Guthub.
+# GitHub does not support git-archive - however this endpoint works (MAR 2020)
+mkdir -p googletest && rm -fr googletest/*
+curl -L https://github.com/google/googletest/tarball/$GTEST_BRANCHTAG \
+   | tar xf - -C googletest --strip-components 1
+
+# Remove googletest artifacts - set cmake BUILD_GMOCK=0, don't install, don't build test.
+pushd googletest
+rm -f .[!.]* BUILD.bazel WORKSPACE appveyor.yml library.json platformio.ini
+rm -fr ci/
+rm -fr googlemock/
+rm -fr googletest/test/ googletest/samples/ googletest/docs/ googletest/scripts/
+popd
