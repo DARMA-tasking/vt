4b383c10c9ec1e23e9fe39be0e3c764cd5d84af5
Jonathan Lifflander <jliffla@sandia.gov>
2020-03-09 16:28:53 -0700

    Build: fix local paths

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ((nil
-  (cmake-ide-project-dir . (expand-file-name "~/codes/vt/virtual-transport"))
-  (cmake-ide-build-dir . (expand-file-name "~/codes/vt/build-virtual-transport"))
-  (cmake-ide-cmake-opts . " -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER=clang-mp-3.9 -DCMAKE_CXX_COMPILER=clang++-mp-3.9")
+  (cmake-ide-project-dir . (expand-file-name "/Users/jliffla/codes/vt/virtual-transport"))
+  (cmake-ide-build-dir . (expand-file-name "/Users/jliffla/codes/vt/build-virtual-transport"))
+  (cmake-ide-cmake-opts . " -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++")
   ))
 
 ;; (concat " -DCMAKE_BUILD_TYPE=debug "
