a9b1c133bcb73c2d751f95157226c291432ef545
Jonathan Lifflander <jliffla@sandia.gov>
2020-03-09 16:44:08 -0700

    Build: simply dir locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,3 @@
 ((nil
-  (cmake-ide-project-dir . (expand-file-name "/Users/jliffla/codes/vt/virtual-transport"))
-  (cmake-ide-build-dir . (expand-file-name "/Users/jliffla/codes/vt/build-virtual-transport"))
-  (cmake-ide-cmake-opts . " -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++")
-  ))
-
-;; (concat " -DCMAKE_BUILD_TYPE=debug "
-;;         " -DCMAKE_C_COMPILER=mpicc-mpich-clang39"
-;;         " -DCMAKE_CXX_COMPILER=mpic++-mpich-clang39"
-;; )
+  (setq cmake-ide-build-dir "/Users/jliffla/codes/vt/build-virtual-transport"))
+)
