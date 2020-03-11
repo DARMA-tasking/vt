ee5dc2254ee2b00864f463d2add586126a86eb21
Jonathan Lifflander <jliffla@sandia.gov>
2020-03-11 15:37:14 -0700

    #738: util: add parser for units to byte conversion

--- src/vt/utils/memory/memory_usage.h
+++ src/vt/utils/memory/memory_usage.h
@@ -121,6 +121,8 @@ struct MemoryUsage {
 
   std::vector<std::string> getWorkingReporters();
 
+  std::size_t convertBytesFromString(std::string const& in);
+
   static MemoryUsage* get();
 
   static void initialize();
