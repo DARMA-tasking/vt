a6f406846a2e9a4cc74c97f817f3f2a11f9f5afc
Jonathan Lifflander <jliffla@sandia.gov>
2020-04-20 17:13:51 -0700

    #764: runtime: add doxygen for components

--- src/vt/runtime/component/component.h
+++ src/vt/runtime/component/component.h
@@ -55,56 +55,127 @@
 
 namespace vt { namespace runtime { namespace component {
 
+/**
+ * \struct Component component.h vt/runtime/component/component.h
+ *
+ * \brief \c Component class for a generic VT runtime module, CRTP'ed over the
+ * component's actual type
+ */
 template <typename T>
 struct Component : BaseComponent {
 
   Component() = default;
 
+  /**
+   * \brief Constructor for a new component with the appropriate typed
+   * dependencies
+   *
+   * \param[in] DepsPack a pack of typed dependencies for the given component
+   * that it depends on
+   *
+   * \return the new component
+   */
   template <typename... Deps>
   Component(DepsPack<Deps...>) {
     ComponentRegistry::dependsOn<T, Deps...>();
   }
 
-  /// Traits for objgroup components which have a specialized static construct
+  /// Trait for components that have a specialized static construct method (used
+  /// for objgroups)
   template <typename U>
   using hasCons = typename std::enable_if<ComponentTraits<U>::hasConstruct, T>::type;
+
+  /// Trait for components that do not have a specialized static construct method
   template <typename U>
   using hasNoCons = typename std::enable_if<not ComponentTraits<U>::hasConstruct, T>::type;
 
+  /**
+   * \brief Construct the component with the specialized construct method
+   *
+   * \param[in] args the arguments to forward to the component's constructor
+   *
+   * \return unique pointer to the the component
+   */
   template <typename... Args, typename U = T>
   static std::unique_ptr<T> staticInit(Args&&... args, hasCons<U>* = nullptr) {
     return T::construct(std::forward<Args>(args)...);
   }
 
+  /**
+   * \brief Construct the component with normal component constructor
+   *
+   * \param[in] args the arguments to forward to the component's constructor
+   *
+   * \return unique pointer to the the component
+   */
   template <typename... Args, typename U = T>
   static std::unique_ptr<T> staticInit(Args&&... args, hasNoCons<U>* = nullptr) {
     return std::make_unique<T>(std::forward<Args>(args)...);
   }
 
+  /**
+   * \brief Whether the component requires the scheduler to poll
+   *
+   * \return default component is not pollable
+   */
   bool pollable() override {
     return false;
   }
 
+  /**
+   * \brief Empty default overridden initialize method
+   */
   virtual void initialize() override { }
+
+  /**
+   * \brief Empty default overridden finalize method
+   */
   virtual void finalize() override { }
+
+  /**
+   * \brief Empty default overridden startup method
+   */
   virtual void startup() override { }
 
-  // Default empty progress function
+  /**
+   * \brief Empty default overridden progress method
+   *
+   * \return no units processed
+   */
   virtual int progress() override { return 0; }
 
-  void dumpState() override {
-    /* here to compile, should be implemented by each component*/
-  }
+  /**
+   * \brief Empty default diagnostic dump state
+   */
+  void dumpState() override { }
 };
 
+/**
+ * \struct PollableComponent component.h vt/runtime/component/component.h
+ *
+ * \brief \c Component class for a generic, pollable VT runtime module, CRTP'ed
+ * over the component's actual type. A pollable component will be registered
+ * with the VT scheduler to ensure it makes progress.
+ */
 template <typename T>
 struct PollableComponent : Component<T> {
 
+  /**
+   * \brief Whether the component requires the scheduler to poll
+   *
+   * \return pollable component returns true to indicate progress function to be
+   * invoked
+   */
   bool pollable() override {
     return true;
   }
 
-  // Fail if progress method not overridden by user
+  /**
+   * \brief Override progress function to force user to supply a real
+   * function. Abort if the user does not.
+   *
+   * \return number of units processed---zero
+   */
   virtual int progress() override {
     vtAbort("PollableComponent should have a progress function");
     return 0;
