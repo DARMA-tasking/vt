c94b5726d80aebe25135fef14c5361e5f9d681d3
Jonathan Lifflander <jliffla@sandia.gov>
2020-04-22 15:15:08 -0700

    #526: param: fix gcc bugs in parameterization so example compiles/runs

--- src/vt/parameterization/param_meta.h
+++ src/vt/parameterization/param_meta.h
@@ -60,11 +60,11 @@ using MultiParamType = void(*)(Args...);
 template<typename T, T value>
 struct NonType {};
 
-#define PARAM_FUNCTION_RHS(value) vt::param::NonType<decltype(value),(value)>()
-#define PARAM_FUNCTION(value) decltype(value),(value)
+#define PARAM_FUNCTION_RHS(value) vt::param::NonType<decltype(&value),(&value)>()
+#define PARAM_FUNCTION(value) decltype(&value),(&value)
 
 template <typename Function, typename Tuple, size_t... I>
-auto callFnTuple(Function f, Tuple&& t, std::index_sequence<I...>) {
+auto callFnTuple(Function f, Tuple t, std::index_sequence<I...>) {
   return f(
     std::forward<typename std::tuple_element<I,Tuple>::type>(
       std::get<I>(t)
@@ -73,21 +73,21 @@ auto callFnTuple(Function f, Tuple&& t, std::index_sequence<I...>) {
 }
 
 template <size_t size, typename TypedFnT, typename... Args>
-void invokeFnTuple(TypedFnT f, std::tuple<Args...>&& t) {
+void invokeFnTuple(TypedFnT f, std::tuple<Args...> t) {
   using TupleType = std::tuple<Args...>;
   callFnTuple(f, std::forward<TupleType>(t), std::make_index_sequence<size>{});
 }
 
 template <typename FnT, typename... Args>
-void invokeCallableTuple(std::tuple<Args...>&& tup, FnT fn, bool const& is_functor) {
+void invokeCallableTuple(std::tuple<Args...>& tup, FnT fn, bool const& is_functor) {
   using TupleType = typename std::decay<decltype(tup)>::type;
   static constexpr auto size = std::tuple_size<TupleType>::value;
   if (is_functor) {
-    auto typed_fn = reinterpret_cast<MultiParamType<Args&&...>>(fn);
-    return invokeFnTuple<size>(typed_fn, std::forward<TupleType>(tup));
+    auto typed_fn = reinterpret_cast<MultiParamType<Args...>>(fn);
+    return invokeFnTuple<size>(typed_fn, tup);
   } else {
     auto typed_fn = reinterpret_cast<MultiParamType<Args...>>(fn);
-    return invokeFnTuple<size>(typed_fn, std::forward<TupleType>(tup));
+    return invokeFnTuple<size>(typed_fn, tup);
   }
 }
 
