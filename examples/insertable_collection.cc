5b3fa1ad832527a117e2ef21ee0ee0f8f41e7ea3
Jonathan Lifflander <jliffla@sandia.gov>
2020-04-21 19:11:42 -0700

    #526: example: rewrite collection_insert

--- examples/insertable_collection.cc
+++ examples/insertable_collection.cc
@@ -2,7 +2,7 @@
 //@HEADER
 // *****************************************************************************
 //
-//                             collection_insert.cc
+//                          insertable_collection.cc
 //                           DARMA Toolkit v. 1.0.0
 //                       DARMA/vt => Virtual Transport
 //
@@ -42,82 +42,50 @@
 //@HEADER
 */
 
-#include "vt/transport.h"
-
-#include <cstdlib>
-
-using namespace ::vt;
-using namespace ::vt::collective;
-using namespace ::vt::mapping;
+#include <vt/transport.h>
 
 static constexpr int32_t const default_num_elms = 64;
 
-struct InsertCol : InsertableCollection<InsertCol,Index1D> {
-  InsertCol()
-    : InsertableCollection<InsertCol,Index1D>()
-  {
-    auto const& this_node = theContext()->getNode();
+struct InsertCol : vt::InsertableCollection<InsertCol, vt::Index1D> {
+  InsertCol() {
+    vt::NodeType this_node = vt::theContext()->getNode();
     ::fmt::print("{}: constructing: idx={}\n", this_node, getIndex().x());
- }
+  }
 };
 
-struct TestMsg : CollectionMessage<InsertCol> { };
-
-static void work(TestMsg* msg, InsertCol* col) {
-  auto const& this_node = theContext()->getNode();
-  ::fmt::print("work: node={}, idx={}\n", this_node, col->getIndex().x());
-}
-
 int main(int argc, char** argv) {
-  CollectiveOps::initialize(argc, argv);
+  vt::initialize(argc, argv);
 
-  auto const& this_node = theContext()->getNode();
+  vt::NodeType this_node = vt::theContext()->getNode();
 
   int32_t num_elms = default_num_elms;
-
   if (argc > 1) {
     num_elms = atoi(argv[1]);
   }
 
-  auto const epoch = theTerm()->makeEpochCollective();
-  theMsg()->setGlobalEpoch(epoch);
-
   if (this_node == 0) {
-    auto const& range = Index1D(num_elms);
-    auto proxy = theCollection()->construct<InsertCol>(range);
+    auto range = vt::Index1D(num_elms);
+    auto proxy = vt::theCollection()->construct<InsertCol>(range);
 
-    theTerm()->addActionEpoch(epoch,[=]{
-      ::fmt::print("broadcasting\n");
-      auto msg = makeSharedMessage<TestMsg>();
-      proxy.broadcast<TestMsg,work>(msg);
-    });
     for (int i = 0; i < range.x() / 2; i++) {
       proxy[i].insert(i % 2);
-      /*
-       * Alternative syntax:
-       *   theCollection()->insert<InsertCol>(proxy, Index1D(i));
-       */
     }
-    theTerm()->finishedEpoch(epoch);
-    ::fmt::print("calling finished insert\n");
+
+    fmt::print("calling finished insert\n");
+
     proxy.finishedInserting([=]{
-      ::fmt::print("insertions are finished\n");
+      fmt::print("insertions are finished (first phase)\n");
       for (int i = range.x()/2; i < range.x(); i++) {
         proxy[i].insert(i % 2);
       }
+
       proxy.finishedInserting([]{
-        ::fmt::print("insertions are finished2\n");
+        fmt::print("insertions are finished (second phase)\n");
       });
     });
-  } else {
-    theTerm()->finishedEpoch(epoch);
   }
 
-  while (!rt->isTerminated()) {
-    runScheduler();
-  }
-
-  CollectiveOps::finalize();
+  vt::finalize();
 
   return 0;
 }
