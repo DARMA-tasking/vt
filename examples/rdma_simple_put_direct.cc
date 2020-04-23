4963926d796a2ae88d2383bfdf2b842a794aa95a
Jonathan Lifflander <jliffla@sandia.gov>
2020-04-23 11:02:43 -0700

    #526: example: rewrite rdma_simple_put_direct

--- examples/rdma_simple_put_direct.cc
+++ examples/rdma_simple_put_direct.cc
@@ -42,94 +42,87 @@
 //@HEADER
 */
 
-#include "vt/transport.h"
-#include <cstdlib>
+#include <vt/transport.h>
 
-using namespace vt;
-
-static NodeType my_node = uninitialized_destination;
-static NodeType num_nodes = uninitialized_destination;
-
-static RDMA_HandleType my_handle_1 = no_rdma_handle;
+#include <memory>
 
 static int const put_len = 2;
 static int const my_data_len = 8;
-static double* my_data = nullptr;
+static std::unique_ptr<double[]> my_data = nullptr;
 
-struct TestMsg : vt::Message {
-  RDMA_HandleType han;
-  TestMsg(RDMA_HandleType const& in_han) : Message(), han(in_han) { }
+struct HandleMsg : vt::Message {
+  vt::RDMA_HandleType han;
+  explicit HandleMsg(vt::RDMA_HandleType const& in_han) : han(in_han) { }
 };
 
-static void readDataFn(TestMsg* msg) {
-  fmt::print("{}: readDataFn: handle={}\n", my_node, msg->han);
+static void readDataFn(HandleMsg* msg) {
+  vt::NodeType this_node = vt::theContext()->getNode();
+
+  fmt::print("{}: readDataFn: handle={}\n", this_node, msg->han);
 
   for (auto i = 0; i < put_len*2; i++) {
     fmt::print(
-      "{}: han={} \t: my_data[{}] = {}\n", my_node, msg->han, i, my_data[i]
+      "{}: han={} \t: my_data[{}] = {}\n", this_node, msg->han, i, my_data[i]
     );
   }
 }
 
-static void putDataFn(TestMsg* msg) {
-  if (my_node == 1 or my_node == 2) {
+static void putDataFn(HandleMsg* msg) {
+  vt::NodeType this_node = vt::theContext()->getNode();
+
+  if (this_node == 1 or this_node == 2) {
     fmt::print(
       "{}: putting data, handle={}, my_data={}\n",
-      my_node, msg->han, print_ptr(my_data)
+      this_node, msg->han, print_ptr(&my_data[0])
     );
 
     int const num_elm = 2;
-    int const offset = num_elm*(my_node-1);
+    int const offset = num_elm*(this_node-1);
     auto han = msg->han;
-    theRDMA()->putTypedData(msg->han, my_data, num_elm, offset, [=]{
+    vt::theRDMA()->putTypedData(msg->han, &my_data[0], num_elm, offset, [=]{
       fmt::print(
-        "{}: after put: sending msg back to 0: offset={}\n", my_node, offset
+        "{}: after put: sending msg back to 0: offset={}\n", this_node, offset
       );
 
-      auto back = makeMessage<TestMsg>(han);
-      theMsg()->sendMsg<TestMsg, readDataFn>(0, back.get());
+      auto back = vt::makeMessage<HandleMsg>(han);
+      vt::theMsg()->sendMsg<HandleMsg, readDataFn>(0, back.get());
     });
   }
 }
 
 int main(int argc, char** argv) {
-  CollectiveOps::initialize(argc, argv);
+  vt::initialize(argc, argv);
 
-  my_node = theContext()->getNode();
-  num_nodes = theContext()->getNumNodes();
+  vt::NodeType this_node = vt::theContext()->getNode();
+  vt::NodeType num_nodes = vt::theContext()->getNumNodes();
 
   if (num_nodes < 4) {
-    fmt::print("requires exactly 4 nodes\n");
-    CollectiveOps::finalize();
-    return 0;
+    return vt::rerror("requires exactly 4 nodes");
   }
 
-  my_data = new double[my_data_len];
+  my_data = std::make_unique<double[]>(my_data_len);
 
   // initialize my_data buffer, all but node 0 get -1.0
   for (auto i = 0; i < my_data_len; i++) {
-    my_data[i] = my_node != 0 ? (my_node+1)*i+1 : -1.0;
+    my_data[i] = this_node != 0 ? (this_node+1)*i+1 : -1.0;
   }
 
-  if (my_node == 0) {
-    my_handle_1 = theRDMA()->registerNewTypedRdmaHandler(my_data, put_len);
+  if (this_node == 0) {
+    vt::RDMA_HandleType my_handle =
+      vt::theRDMA()->registerNewTypedRdmaHandler(&my_data[0], put_len);
 
     fmt::print(
-      "{}: initializing my_handle_1={}\n",
-      my_node, my_handle_1
+      "{}: initializing my_handle={}\n",
+      this_node, my_handle
     );
 
-    auto msg1 = makeMessage<TestMsg>(my_handle_1);
-    auto msg2 = makeMessage<TestMsg>(my_handle_1);
-    theMsg()->sendMsg<TestMsg, putDataFn>(1, msg1.get());
-    theMsg()->sendMsg<TestMsg, putDataFn>(2, msg2.get());
+    auto msg1 = vt::makeMessage<HandleMsg>(my_handle);
+    auto msg2 = vt::makeMessage<HandleMsg>(my_handle);
+    vt::theMsg()->sendMsg<HandleMsg, putDataFn>(1, msg1.get());
+    vt::theMsg()->sendMsg<HandleMsg, putDataFn>(2, msg2.get());
   }
 
-  while (!rt->isTerminated()) {
-    runScheduler();
-  }
-
-  CollectiveOps::finalize();
+  vt::finalize();
 
   return 0;
 }
