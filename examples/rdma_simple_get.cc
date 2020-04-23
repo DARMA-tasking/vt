fb01f03807c6b1abd996abeee12ef94100aba6b0
Jonathan Lifflander <jliffla@sandia.gov>
2020-04-23 11:02:33 -0700

    #526: example: rewrite rdma_simple_get

--- examples/rdma_simple_get.cc
+++ examples/rdma_simple_get.cc
@@ -42,34 +42,30 @@
 //@HEADER
 */
 
-#include "vt/transport.h"
-#include <cstdlib>
+#include <vt/transport.h>
 
-using namespace vt;
+#include <memory>
 
-static NodeType my_node = uninitialized_destination;
-static NodeType num_nodes = uninitialized_destination;
-
-static RDMA_HandleType my_handle = no_rdma_handle;
-
-struct TestMsg : vt::Message {
-  RDMA_HandleType han;
-  TestMsg(RDMA_HandleType const& in_han) : Message(), han(in_han) { }
+struct HandleMsg : vt::Message {
+  vt::RDMA_HandleType han;
+  explicit HandleMsg(vt::RDMA_HandleType const& in_han) : han(in_han) { }
 };
 
-static void tell_handle(TestMsg* msg) {
-  fmt::print("{}: handle={}\n", my_node, msg->han);
+static void tell_handle(HandleMsg* msg) {
+  vt::NodeType this_node = vt::theContext()->getNode();
 
-  if (my_node == 1 || my_node == 2) {
-    fmt::print("{}: requesting data\n", my_node);
-    theRDMA()->getData(
-      msg->han, my_node, sizeof(double)*3, no_byte,
-      [](void* data, size_t num_bytes){
+  fmt::print("{}: handle={}\n", this_node, msg->han);
+
+  if (this_node == 1 || this_node == 2) {
+    fmt::print("{}: requesting data\n", this_node);
+    vt::theRDMA()->getData(
+      msg->han, this_node, sizeof(double)*3, vt::no_byte,
+      [=](void* data, size_t num_bytes){
         double* const ptr = static_cast<double*>(data);
         size_t const num_elems = num_bytes / sizeof(double);
         fmt::print(
           "{}: data arrived: data={}, num_bytes={}\n",
-          my_node, print_ptr(data), num_bytes
+          this_node, print_ptr(data), num_bytes
         );
         for (size_t i = 0; i < num_elems; i++) {
           fmt::print("\t: my_data[{}] = {}\n", i, ptr[i]);
@@ -79,51 +75,48 @@ static void tell_handle(TestMsg* msg) {
   }
 }
 
-static double* my_data = nullptr;
-static TestMsg* test_msg = nullptr;
+static std::unique_ptr<double[]> my_data = nullptr;
 
-static RDMA_GetType
-test_get_fn(TestMsg* msg, ByteType num_bytes, ByteType offset, TagType tag, bool) {
+static vt::RDMA_GetType test_get_fn(
+  vt::BaseMessage*, vt::ByteType num_bytes, vt::ByteType offset, vt::TagType tag,
+  bool
+) {
+  vt::NodeType this_node = vt::theContext()->getNode();
   fmt::print(
-    "{}: running test_get_fn: msg={}, num_bytes={}, tag={}\n",
-    my_node, print_ptr(msg), num_bytes, tag
+    "{}: running test_get_fn: num_bytes={}, tag={}\n",
+    this_node, num_bytes, tag
   );
-  return RDMA_GetType{
-    my_data+tag, num_bytes == no_byte ? sizeof(double)*10 : num_bytes
+  return vt::RDMA_GetType{
+    &my_data[0] + tag, num_bytes == vt::no_byte ? sizeof(double)*10 : num_bytes
   };
 }
 
 int main(int argc, char** argv) {
-  CollectiveOps::initialize(argc, argv);
+  vt::initialize(argc, argv);
 
-  my_node = theContext()->getNode();
-  num_nodes = theContext()->getNumNodes();
+  vt::NodeType this_node = vt::theContext()->getNode();
 
-  if (my_node == 0) {
+  if (this_node == 0) {
     auto const len = 64;
-    my_data = new double[len];
+    my_data = std::make_unique<double[]>(len);
 
     for (auto i = 0; i < len; i++) {
       my_data[i] = i+1;
     }
 
-    my_handle = theRDMA()->registerNewRdmaHandler();
-    theRDMA()->associateGetFunction<TestMsg,test_get_fn>(
-      test_msg, my_handle, test_get_fn, true
+    vt::RDMA_HandleType my_handle = vt::theRDMA()->registerNewRdmaHandler();
+    vt::theRDMA()->associateGetFunction<vt::BaseMessage,test_get_fn>(
+      nullptr, my_handle, test_get_fn, true
     );
 
     fmt::print("initializing my_handle={}\n", my_handle);
 
-    auto msg = makeSharedMessage<TestMsg>(my_node);
+    auto msg = vt::makeMessage<HandleMsg>(this_node);
     msg->han = my_handle;
-    theMsg()->broadcastMsg<TestMsg, tell_handle>(msg);
+    vt::theMsg()->broadcastMsg<HandleMsg, tell_handle>(msg.get());
   }
 
-  while (!rt->isTerminated()) {
-    runScheduler();
-  }
-
-  CollectiveOps::finalize();
+  vt::finalize();
 
   return 0;
 }
