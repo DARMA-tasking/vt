a90ee52e8a3fd29305fe9c44724187fabd7032c1
Jonathan Lifflander <jliffla@sandia.gov>
2020-04-25 10:24:11 -0700

    #584: build: fix capitalization on Zoltan package

--- cmake/load_zoltan_package.cmake
+++ cmake/load_zoltan_package.cmake
@@ -6,7 +6,7 @@ set(vt_zoltan_enabled "0")
 optional_pkg_directory(zoltan "Zoltan library" 1)
 # find the optional packages locally if identified
 if (${zoltan_DIR_FOUND})
-  find_package_local(zoltan "${zoltan_DIR}" zoltan)
+  find_package_local(zoltan "${zoltan_DIR}" Zoltan)
   if(NOT projHasParent)
     if (NOT ${zoltan_FOUND})
       message(FATAL_ERROR "Zoltan library not found")
