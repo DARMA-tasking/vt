7ed38aca92f9890306735cb2c952e3bed39fe8a3
Jonathan Lifflander <jliffla@sandia.gov>
2020-04-27 20:23:00 -0700

    #584: lb: finish fixing comm graph issues

--- src/vt/vrt/collection/balance/zoltanlb/zoltanlb.cc
+++ src/vt/vrt/collection/balance/zoltanlb/zoltanlb.cc
@@ -58,16 +58,202 @@ void ZoltanLB::init(objgroup::proxy::Proxy<ZoltanLB> in_proxy) {
   proxy = in_proxy;
 }
 
-void ZoltanLB::inputParams(balance::SpecEntry* spec) { }
+void ZoltanLB::inputParams(balance::SpecEntry* spec) {
+  zoltan_config_ = {
+    {"LB_APPROACH",                   "REPARTITION" },
+    {"DEBUG_LEVEL",                   "0"           },
+    {"FINAL_OUTPUT",                  "0"           },
+    {"DEBUG_PROCESSOR",               "0"           },
+    {"CHECK_HYPERGRAPH",              "1"           },
+    {"PHG_EDGE_WEIGHT_OPERATION",     "max"         },
+    {"IMBALANCE_TOL",                 "1.00"        },
+    {"PHG_REPART_MULTIPLIER",         "100"         },
+    {"PHG_EDGE_WEIGHT_OPERATION",     "max"         },
+    {"PHG_CUT_OBJECTIVE",             "connectivity"},
+    {"PHG_OUTPUT_LEVEL",              "0"           },
+    {"PHG_COARSENING_METHOD",         "agg"         },
+    {"PHG_COARSEPARTITION_METHOD",    "auto"        },
+    {"PHG_REFINEMENT_QUALITY",        "1"           },
+    {"PHG_RANDOMIZE_INPUT",           "0"           },
+    {"PHG_EDGE_SIZE_THRESHOLD",       "0.25"        },
+    {"PHG_PROCESSOR_REDUCTION_LIMIT", "0.5"         },
+    {"PHG_REFINEMENT_METHOD",         ""            },
+    {"PHG_MULTILEVEL",                ""            },
+    {"SEED",                          ""            }
+  };
+
+  std::vector<std::string> allowed{"edges", "use_shared_edges"};
+  for (auto& c : zoltan_config_) {
+    allowed.push_back(c.first);
+  }
+  spec->checkAllowedKeys(allowed);
+
+  do_edges_ = spec->getOrDefault<bool>("edges", do_edges_);
+  use_shared_edges_ = spec->getOrDefault<bool>(
+    "use_shared_edges", use_shared_edges_
+  );
+
+  for (auto& c : zoltan_config_) {
+    c.second = spec->getOrDefault<std::string>(c.first, c.second);
+  }
+}
+
+template <typename Callable>
+void executeInEpoch(Callable&& fn) {
+  auto ep = vt::theTerm()->makeEpochCollective();
+  vt::theMsg()->pushEpoch(ep);
+  fn();
+  vt::theMsg()->popEpoch(ep);
+  vt::theTerm()->finishedEpoch(ep);
+  bool done = false;
+  vt::theTerm()->addAction(ep, [&done]{ done = true; });
+  do vt::runScheduler(); while (!done);
+}
+
+void ZoltanLB::countEdges() {
+  debug_print(lb, node, "countEdges\n");
+
+  // Count the number of local and remote edges to allocation edge GIDs
+  int local_edge = 0;
+  int remote_owned_edge = 0;
+
+  auto const this_node = theContext()->getNode();
+  for (auto&& elm : load_comm_symm) {
+    if (
+      elm.first.cat_ == balance::CommCategory::SendRecv and
+      not elm.first.selfEdge()
+    ) {
+      auto from = elm.first.fromObjTemp();
+      auto to = elm.first.toObjTemp();
+
+      auto from_node = objGetNode(from);
+      auto to_node = objGetNode(to);
+
+      if (from_node == to_node and from_node == this_node) {
+        local_edge++;
+      } else {
+        // Break ties on non-local object edges based on obj ID
+        auto large_obj_id = from > to ? from : to;
+        if (objGetNode(large_obj_id) == this_node) {
+          remote_owned_edge++;
+        }
+      }
+    }
+  }
+
+  int const total_ids = local_edge + remote_owned_edge;
+
+  debug_print(lb, node, "ZoltanLB: total_ids_={}\n", total_ids);
+
+  auto cb = theCB()->makeBcast<ZoltanLB,ReduceMsg,&ZoltanLB::reduceCount>(proxy);
+  auto msg = makeMessage<ReduceMsg>(total_ids);
+  proxy.reduce<collective::MaxOp<int>>(msg.get(),cb);
+}
+
+void ZoltanLB::reduceCount(ReduceMsg* msg) {
+  max_edges_per_node_ = msg->getVal();
+
+  debug_print(
+    lb, node,
+    "ZoltanLB: max_edges_per_node_={}\n",
+    max_edges_per_node_
+  );
+}
+
+void ZoltanLB::allocateShareEdgeGIDs() {
+  std::unordered_map<NodeType, ElementCommType> shared_edges;
+
+  auto const this_node = theContext()->getNode();
+  for (auto&& elm : load_comm_symm) {
+    auto from = elm.first.fromObjTemp();
+    auto to = elm.first.toObjTemp();
+
+    auto from_node = objGetNode(from);
+    auto to_node = objGetNode(to);
+
+    if (from_node == to_node and from_node == this_node) {
+      auto offset = max_edges_per_node_ * this_node;
+      auto id = 1 + offset + edge_id_++;
+      auto key = elm.first;
+      key.edge_id_ = id;
+      load_comm_edge_id[key] = elm.second;
+
+      debug_print(
+        lb, node,
+        "allocate: local edge_id={:x}, from={:x}, to={:x}\n",
+        key.edge_id_,
+        key.fromObjTemp(),
+        key.toObjTemp()
+      );
+
+    } else {
+      auto large_obj_id = from > to ? from : to;
+      if (objGetNode(large_obj_id) == this_node) {
+        auto offset = max_edges_per_node_ * this_node;
+        auto id = 1 + offset + edge_id_++;
+        auto key = elm.first;
+        key.edge_id_ = id;
+        load_comm_edge_id[key] = elm.second;
+
+        debug_print(
+          lb, node,
+          "allocate: remote edge_id={:x}, from={:x}, to={:x}\n",
+          key.edge_id_,
+          key.fromObjTemp(),
+          key.toObjTemp()
+        );
+
+        if (use_shared_edges_) {
+          auto other_node = from_node == this_node ? to_node : from_node;
+          shared_edges[other_node][key] = elm.second;
+        }
+      } else {
+        // If use_shared_edges_, the other node will set the ID; wait to receive
+        // it in next phase. If not shared, the other node will provide the edge
+      }
+    }
+  }
+
+  if (use_shared_edges_) {
+    for (auto&& elm : shared_edges) {
+      auto msg = makeMessage<CommMsg>(elm.second);
+      proxy[elm.first].send<CommMsg, &ZoltanLB::recvEdgeGID>(msg.get());
+    }
+  }
+}
+
+void ZoltanLB::recvEdgeGID(CommMsg* msg) {
+  auto& comm = msg->comm_;
+  for (auto&& elm : comm) {
+    vtAssert(
+      load_comm_edge_id.find(elm.first) == load_comm_edge_id.end(),
+      "Must not exists in edge ID map"
+    );
+
+    debug_print(
+      lb, node,
+      "recvEdgeGID: edge_id={:x}, from={:x}, to={:x}\n",
+      elm.first.edge_id_,
+      elm.first.fromObjTemp(),
+      elm.first.toObjTemp()
+    );
+    load_comm_edge_id[elm.first] = elm.second;
+  }
+}
 
 void ZoltanLB::runLB() {
   auto const& this_node = theContext()->getNode();
-  // auto const& num_nodes = theContext()->getNumNodes();
-  // auto const next_node = this_node + 1 > num_nodes-1 ? 0 : this_node + 1;
 
   if (this_node == 0) {
-    vt_print(lb, "ZoltanLB: runLB\n");
+    vt_print(lb, "ZoltanLB: runLB: edges={}\n", do_edges_);
     fflush(stdout);
+  }
+
+  if (do_edges_) {
+    executeInEpoch([this]{ makeGraphSymmetric();    });
+    executeInEpoch([this]{ combineEdges();          });
+    executeInEpoch([this]{ countEdges();            });
+    executeInEpoch([this]{ allocateShareEdgeGIDs(); });
   }
 
   initZoltan();
@@ -100,15 +286,21 @@ void ZoltanLB::runLB() {
 
   startMigrationCollective();
 
-  // vt_print(
-  //   lb,
-  //   "ZoltanLB: num_export={}, num_import={}\n",
-  //   num_export, num_import
-  // );
+  debug_print(
+    lb, node,
+    "ZoltanLB: num_export={}, num_import={}\n",
+    num_export, num_import
+  );
 
   for (int i = 0; i < num_export; i++) {
     int to_node = export_procs[i];
-    // vt_print(lb, "migrateObjTo: to_node={} gid={:x}\n", to_node, export_global_ids[i]);
+
+    debug_print(
+      lb, node,
+      "migrateObjTo: to_node={} gid={:x}\n",
+      to_node, export_global_ids[i]
+    );
+
     migrateObjectTo(export_global_ids[i], static_cast<NodeType>(to_node));
   }
 
@@ -122,6 +314,89 @@ void ZoltanLB::runLB() {
   );
 
   destroyZoltan();
+}
+
+void ZoltanLB::makeGraphSymmetric() {
+  auto const this_node = theContext()->getNode();
+
+  // Go through the comm graph and extract out paired SendRecv edges that are
+  // not self-send and have a non-local edge
+  std::unordered_map<NodeType, ElementCommType> shared_edges;
+
+  for (auto&& elm : *comm_data) {
+    if (
+      elm.first.cat_ == balance::CommCategory::SendRecv and
+      not elm.first.selfEdge()
+    ) {
+      auto from = elm.first.fromObjTemp();
+      auto to = elm.first.toObjTemp();
+
+      auto from_node = objGetNode(from);
+      auto to_node = objGetNode(to);
+
+      vtAssert(
+        from_node == this_node or to_node == this_node,
+        "One node must involve this node"
+      );
+
+      debug_print(
+        lb, node,
+        "makeGraphSymmetric: from={:x}, to={:x}\n",
+        from, to
+      );
+
+      if (from_node != this_node) {
+        shared_edges[from_node][elm.first] = elm.second;
+      } else if (to_node != this_node) {
+        shared_edges[to_node][elm.first] = elm.second;
+      }
+
+      // Only add the edges wanted now (SendRecv, no self-edge)
+      load_comm_symm[elm.first] += elm.second;
+    }
+  }
+
+  for (auto&& elm : shared_edges) {
+    auto msg = makeMessage<CommMsg>(elm.second);
+    proxy[elm.first].send<CommMsg, &ZoltanLB::recvSharedEdges>(msg.get());
+  }
+
+}
+
+void ZoltanLB::recvSharedEdges(CommMsg* msg) {
+  auto& comm = msg->comm_;
+  for (auto&& elm : comm) {
+    debug_print(
+      lb, node,
+      "recv shared edge: from={:x}, to={:x}\n",
+      elm.first.fromObjTemp(), elm.first.toObjTemp()
+    );
+
+    load_comm_symm[elm.first] += elm.second;
+  }
+}
+
+void ZoltanLB::combineEdges() {
+  // Combine bi-directional, symmetric edges into a non-directional weight in
+  // bytes the sum of receive and send bytes
+  ElementCommType load_comm_combined;
+
+  for (auto&& e1 : load_comm_symm) {
+    auto from = e1.first.fromObjTemp() > e1.first.toObjTemp() ?
+      e1.first.fromObjTemp() :
+      e1.first.toObjTemp();
+    auto to = e1.first.fromObjTemp() > e1.first.toObjTemp() ?
+      e1.first.toObjTemp() :
+      e1.first.fromObjTemp();
+
+    auto key = balance::LBCommKey{
+      balance::LBCommKey::CollectionTag{},
+      from, from, to, to, false
+    };
+    load_comm_combined[key] += e1.second;
+  }
+
+  load_comm_symm = std::move(load_comm_combined);
 }
 
 Zoltan_Struct* ZoltanLB::initZoltan() {
@@ -139,8 +414,13 @@ void ZoltanLB::destroyZoltan() {
 
 void ZoltanLB::setParams() {
   auto gparts = fmt::format("{}", theContext()->getNumNodes());
-  Zoltan_Set_Param(zoltan_, "DEBUG_LEVEL",               "0"           );
-  Zoltan_Set_Param(zoltan_, "CHECK_HYPERGRAPH",          "1"           );
+
+  for (auto&& c : zoltan_config_) {
+    if (c.second != "") {
+      Zoltan_Set_Param(zoltan_, c.first.c_str(), c.second.c_str());
+    }
+  }
+
   Zoltan_Set_Param(zoltan_, "LB_METHOD",                 "HYPERGRAPH"  );
   Zoltan_Set_Param(zoltan_, "HYPERGRAPH_PACKAGE",        "PHG"         );
   Zoltan_Set_Param(zoltan_, "NUM_GID_ENTRIES",           "1"           );
@@ -149,9 +429,6 @@ void ZoltanLB::setParams() {
   Zoltan_Set_Param(zoltan_, "OBJ_WEIGHT_DIM",            "1"           );
   Zoltan_Set_Param(zoltan_, "EDGE_WEIGHT_DIM",           "1"           );
   Zoltan_Set_Param(zoltan_, "NUM_GLOBAL_PARTS",          gparts.c_str());
-  Zoltan_Set_Param(zoltan_, "LB_APPROACH",               "REPARTITION" );
-  Zoltan_Set_Param(zoltan_, "PHG_EDGE_WEIGHT_OPERATION", "max"         );
-  Zoltan_Set_Param(zoltan_, "IMBALANCE_TOL",             "1.00"        );
 }
 
 std::unique_ptr<ZoltanLB::Graph> ZoltanLB::makeGraph() {
@@ -185,6 +462,12 @@ std::unique_ptr<ZoltanLB::Graph> ZoltanLB::makeGraph() {
     int idx = 0;
     for (auto&& obj : load_objs) {
       graph->vertex_gid[idx++] = obj;
+
+      debug_print(
+        lb, node,
+        "makeVertexGraph: vertex_id={}: obj={:x}\n",
+        idx - 1, obj
+      );
     }
   }
 
@@ -203,14 +486,12 @@ std::unique_ptr<ZoltanLB::Graph> ZoltanLB::makeGraph() {
     // Only get communication edges between vertices/migratable elements
     // Insert local comm objs into a std::set for deterministic ordering
     std::vector<balance::LBCommKey> comm_objs;
-    for (auto&& elm : *comm_data) {
-      if (elm.first.cat_ == balance::CommCategory::SendRecv) {
-        comm_objs.push_back(elm.first);
-      }
+    for (auto&& elm : load_comm_edge_id) {
+      comm_objs.push_back(elm.first);
     }
 
     // Set the number of communication edges
-    graph->num_edges = static_cast<int>(comm_objs.size());
+    graph->num_edges = comm_objs.size();
 
     // Allocate space for each edge to describe it
     graph->edge_gid = std::make_unique<ZOLTAN_ID_TYPE[]>(graph->num_edges);
@@ -219,7 +500,7 @@ std::unique_ptr<ZoltanLB::Graph> ZoltanLB::makeGraph() {
     graph->edge_weight = std::make_unique<int[]>(graph->num_edges);
 
     // Allocate space for indexing array into neighbor_gid array for each edge
-    graph->neighbor_idx = std::make_unique<int[]>(graph->num_edges);
+    graph->neighbor_idx = std::make_unique<int[]>(graph->num_edges + 1);
 
     // Allocate space indexing array for edge edge_gid[i] beginning at
     // neighbor_gid[neighbor_idx[i]]
@@ -227,25 +508,81 @@ std::unique_ptr<ZoltanLB::Graph> ZoltanLB::makeGraph() {
 
     // Set the edge weights as bytes in the communication graph
     {
+      auto const this_node = theContext()->getNode();
       int edge_idx = 0;
       int neighbor_idx = 0;
+
+      // Set offset for first edge
+      graph->neighbor_idx[edge_idx] = neighbor_idx;
+
       for (auto&& elm : comm_objs) {
-        auto iter = comm_data->find(elm);
+        auto iter = load_comm_edge_id.find(elm);
         auto bytes = iter->second;
+
+        vtAssert(
+          iter->first.edge_id_ != balance::no_element_id,
+          "Must have element ID"
+        );
+
+        graph->edge_gid[edge_idx] = iter->first.edge_id_;
         graph->edge_weight[edge_idx] = bytes;
 
-        // This edge begins at neighbor_idx
-        graph->neighbor_idx[edge_idx] = neighbor_idx;
+        debug_print(
+          lb, node,
+          "makeEdgeGraph: edge_id={}: edge_idx={}, neighbor_idx={}\n",
+          iter->first.edge_id_,
+          edge_idx,
+          neighbor_idx
+        );
+
+        auto from_node = objGetNode(iter->first.fromObjTemp());
+        auto to_node = objGetNode(iter->first.toObjTemp());
 
         // Set up the links between communicating GIDs
-        graph->neighbor_gid[neighbor_idx++] = iter->first.fromObjTemp();
-        graph->neighbor_gid[neighbor_idx++] = iter->first.toObjTemp();
+        if ((use_shared_edges_ and from_node == this_node) or not use_shared_edges_) {
+          debug_print(
+            lb, node,
+            "makeEdgeGraph: \t edge_id={}: edge_idx={}, obj={:x}\n",
+            iter->first.edge_id_, edge_idx, iter->first.fromObjTemp()
+          );
+
+          graph->neighbor_gid[neighbor_idx++] = iter->first.fromObjTemp();
+        }
+        if ((use_shared_edges_ and to_node == this_node) or not use_shared_edges_) {
+          debug_print(
+            lb, node,
+            "makeEdgeGraph: \t edge_id={}: edge_idx={}, obj={:x}\n",
+            iter->first.edge_id_, edge_idx, iter->first.toObjTemp()
+          );
+
+          graph->neighbor_gid[neighbor_idx++] = iter->first.toObjTemp();
+        }
+
+        // This edge begins at neighbor_idx
+        graph->neighbor_idx[edge_idx + 1] = neighbor_idx;
+
+        debug_print(
+          lb, node,
+          "edge_id={:x} from={:x}, to={:x}\n",
+          iter->first.edge_id_,
+          iter->first.fromObjTemp(), iter->first.toObjTemp()
+        );
 
         edge_idx++;
       }
+      graph->num_all_neighbors = neighbor_idx;
     }
+
+    debug_print(
+      lb, node,
+      "ZoltanLB: number of vertices={} edges={}, neighbors={}\n",
+      graph->num_vertices, graph->num_edges, graph->num_all_neighbors
+    );
+
+
   } else {
     graph->num_edges = 0;
+    graph->num_all_neighbors = 0;
   }
 
   return graph;
@@ -290,9 +627,9 @@ std::unique_ptr<ZoltanLB::Graph> ZoltanLB::makeGraph() {
   bool const neighbor_equal       = num_nonzeroes == graph->num_all_neighbors;
   bool const is_compressed_format = format        == ZOLTAN_COMPRESSED_EDGE;
 
-  assert(edge_equal           && "Edge count must be equal");
-  assert(neighbor_equal       && "Neighbors count must be equal");
-  assert(is_compressed_format && "Must be compressed edge format");
+  vtAssert(edge_equal,           "Edge count must be equal");
+  vtAssert(neighbor_equal,       "Neighbors count must be equal");
+  vtAssert(is_compressed_format, "Must be compressed edge format");
 
   if (!edge_equal || !neighbor_equal || !is_compressed_format) {
     *ierr = ZOLTAN_FATAL;
