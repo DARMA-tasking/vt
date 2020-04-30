2d22ef364133d7e41358f5e6bc1c5011a7436bb3
Jonathan Lifflander <jliffla@sandia.gov>
2020-04-30 10:19:01 -0700

    #584: build: use preferred style in cmake

--- cmake/define_build_types.cmake
+++ cmake/define_build_types.cmake
@@ -128,7 +128,7 @@ else()
   set(vt_feature_cmake_mimalloc "0")
 endif()
 
-if (${vt_zoltan_enabled})
+if (vt_zoltan_enabled)
   message(STATUS "Building VT with zoltan enabled")
   set(vt_feature_cmake_zoltan "1")
 else()
