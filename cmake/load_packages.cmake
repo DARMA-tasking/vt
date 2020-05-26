35c2409fc1f9502ab7f4c02573d748badd0c190e
Paul Nathan Stickney <pstickne@gmail.com>
2020-05-25 22:22:37 -0700

    #510 generate MPI wrappers as part of CMake
    - Defers generation to build time,
      as opposed to C++ source file checked into source control.
    
      Adds a perl dependency to the build. If perl is not found then
      PMPI wrappers will not be generated.

--- cmake/load_packages.cmake
+++ cmake/load_packages.cmake
@@ -5,11 +5,15 @@ include(cmake/load_local_packages.cmake)
 
 # MPI package
 include(cmake/load_mpi_package.cmake)
+
 # ZLIB package
 include(cmake/load_zlib_package.cmake)
 
 # Discover and load threading configuration
 include(cmake/load_threading_package.cmake)
+
+# Perl is used to build the PMPI wrappers
+find_package(Perl)
 
 # Doxygen package
 include(cmake/load_doxygen.cmake)
