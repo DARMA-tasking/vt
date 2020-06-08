0a5a0c8630bd240b6daa4731ce76e99e1feed461
Jonathan Lifflander <jliffla@sandia.gov>
2020-06-08 09:24:52 -0700

    #793: collective: fix inclusion bug in merge

--- src/vt/collective/collective_scope.cc
+++ src/vt/collective/collective_scope.cc
@@ -48,6 +48,7 @@
 #include "vt/config.h"
 #include "vt/collective/collective_scope.h"
 #include "vt/collective/collective_alg.h"
+#include "vt/pipe/pipe_headers.h"
 
 namespace vt { namespace collective {
 
