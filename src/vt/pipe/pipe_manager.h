8c03e2e72a7f561b31f14b142093c9fe2cba86f8
Jonathan Lifflander <jliffla@sandia.gov>
2020-06-16 16:04:31 -0700

    #672: docs: add doxygen for PipeManager

--- src/vt/pipe/pipe_manager.h
+++ src/vt/pipe/pipe_manager.h
@@ -65,7 +65,13 @@
 
 namespace vt { namespace pipe {
 
-
+/**
+ * \internal \struct FunctorTraits
+ *
+ * \brief Extract acceptable message type from functor
+ *
+ * Used to automatically extract the message type from a functor endpoint
+ */
 template <typename T>
 struct FunctorTraits {
   template <typename U>
@@ -74,6 +80,15 @@ struct FunctorTraits {
   static constexpr auto const has_no_msg_type = FunctorNoMsgType::value;
 };
 
+/**
+ * \struct PipeManager
+ *
+ * \brief Core VT component that provides an interface to create type-erased
+ * callbacks to many endpoint types.
+ *
+ * Allows creation of callback to many types of handlers with different
+ * modes---like send and broadcast.
+ */
 struct PipeManager
   : runtime::component::Component<PipeManager>,
     PipeManagerTL, PipeManagerTyped
@@ -82,61 +97,268 @@ struct PipeManager
   using GetMsgType = typename util::FunctorExtractor<FunctorT>::MessageType;
   using Void = V;
 
+  /**
+   * \internal \brief System constructor for making a new PipeManager component
+   */
   PipeManager();
 
   std::string name() override { return "PipeManager"; }
 
+  /**
+   * \brief Make callback to a function (including lambdas) with a context
+   * pointer to any object on this node.
+   *
+   * \warning One must ensure that the lifetime of context pointer provided to
+   * the callback persists at least as long as the last time the callback might
+   * be invoked.
+   *
+   * Example snippet:
+   *
+   * \code{.cpp}
+   *  struct DataMsg : vt::Message { };
+   *  struct Context { int val = 129; };
+   *  int main() {
+   *    auto ctx = std::make_unique<Context>();
+   *    auto cb = vt::theCB()->makeFunc<DataMsg,Context>(
+   *      ctx.get(), [](DataMsg* msg, Context* my_ctx){
+   *        // callback triggered with message and associated context
+   *      }
+   *    );
+   *    auto my_msg = vt::makeMessage<DataMsg>();
+   *    cb.send(my_msg.get());
+   *  }
+   * \endcode
+   *
+   * \param[in] ctx pointer to the object context passed to callback function
+   * \param[in] fn endpoint function that takes a message and context pointer
+   *
+   * \return a new callback
+   */
   template <typename MsgT, typename ContextT>
   Callback<MsgT> makeFunc(ContextT* ctx, FuncMsgCtxType<MsgT, ContextT> fn);
+
+  /**
+   * \brief Make a void callback to a function (including lambdas) with a
+   * context pointer to any object on this node.
+   *
+   * \warning One must ensure that the lifetime of context pointer provided to
+   * the callback persists at least as long as the last time the callback might
+   * be invoked.
+   *
+   * Example snippet:
+   *
+   * \code{.cpp}
+   *  struct Context { int val = 129; };
+   *  int main() {
+   *    auto ctx = std::make_unique<Context>();
+   *    auto cb = vt::theCB()->makeFunc<Context>(
+   *      ctx.get(), [](Context* my_ctx){
+   *        // callback triggered with associated context
+   *      }
+   *    );
+   *    cb.send();
+   *  }
+   * \endcode
+   *
+   * \param[in] ctx pointer to the object context passed to callback function
+   * \param[in] fn endpoint function that takes a context pointer
+   *
+   * \return a new callback
+   */
   template <typename ContextT>
   Callback<Void> makeFunc(ContextT* ctx, FuncCtxType<ContextT> fn);
+
+  /**
+   * \brief Make a callback to a function (including lambdas) on this node with
+   * a message.
+   *
+   * Example snippet:
+   *
+   * \code{.cpp}
+   *  struct DataMsg : vt::Message { };
+   *  int main() {
+   *    auto cb = vt::theCB()->makeFunc<DataMsg>(
+   *      [](DataMsg* msg){
+   *        // callback triggered with message
+   *      }
+   *    );
+   *    auto my_msg = vt::makeMessage<DataMsg>();
+   *    cb.send(my_msg.get());
+   *  }
+   * \endcode
+   *
+   * \param[in] fn endpoint function that takes a message
+   *
+   * \return the new callback
+   */
   template <typename MsgT>
   Callback<MsgT> makeFunc(FuncMsgType<MsgT> fn);
+
+  /**
+   * \brief Make a void callback to a function (including lambdas) on this node.
+   *
+   * Example snippet:
+   *
+   * \code{.cpp}
+   *  int main() {
+   *    auto cb = vt::theCB()->makeFunc([]{
+   *      // callback triggered with message
+   *    });
+   *    cb.send();
+   *  }
+   * \endcode
+   *
+   * \param[in] fn void endpoint function
+   *
+   * \return the new callback
+   */
   Callback<Void> makeFunc(FuncVoidType fn);
 
+  /**
+   * \brief Make a callback to a active message handler to be invoked on a
+   * certain node with a message.
+   *
+   * \param[in] node node to invoke callback on
+   *
+   * \return the new callback
+   */
   template <typename MsgT, ActiveTypedFnType<MsgT>* f>
   Callback<MsgT> makeSend(NodeType const& node);
+
+  /**
+   * \brief Make a callback to a functor handler to be invoked on a certain
+   * node with a message.
+   *
+   * \param[in] node node to invoke callback on
+   *
+   * \return the new callback
+   */
   template <typename FunctorT, typename MsgT = GetMsgType<FunctorT>>
   Callback<MsgT> makeSend(NodeType const& node);
+
+  /**
+   * \brief Make a callback to a void functor handler to be invoked on a certain
+   * node.
+   *
+   * \param[in] node node to invoke callback on
+   *
+   * \return the new callback
+   */
   template <
     typename FunctorT,
     typename = std::enable_if_t<FunctorTraits<FunctorT>::has_no_msg_type>
   >
   Callback<Void> makeSend(NodeType const& node);
+
+  /**
+   * \brief Make a callback to a particular collection element invoking a
+   * non-intrusive collection active function handler.
+   *
+   * \param[in] proxy element proxy to collection
+   *
+   * \return the new callback
+   */
   template <typename ColT, typename MsgT, ColHanType<ColT,MsgT>* f>
   Callback<MsgT> makeSend(typename ColT::ProxyType proxy);
+
+  /**
+   * \brief Make a callback to a particular collection element invoking an
+   * intrusive collection member handler.
+   *
+   * \param[in] proxy element proxy to collection
+   *
+   * \return the new callback
+   */
   template <typename ColT, typename MsgT, ColMemType<ColT,MsgT> f>
   Callback<MsgT> makeSend(typename ColT::ProxyType proxy);
+
+  /**
+   * \brief Make a callback to a particular objgroup element (node) invoking a
+   * objgroup member handler.
+   *
+   * \param[in] proxy element proxy to objgroup
+   *
+   * \return the new callback
+   */
   template <typename ObjT, typename MsgT, ObjMemType<ObjT,MsgT> f>
   Callback<MsgT> makeSend(objgroup::proxy::ProxyElm<ObjT> proxy);
 
+  /**
+   * \brief Make a callback to a active message handler with a message to be
+   * broadcast to all nodes.
+   *
+   * \return the new callback
+   */
   template <typename MsgT, ActiveTypedFnType<MsgT>* f>
   Callback<MsgT> makeBcast();
+
+  /**
+   * \brief Make a callback to a functor handler with a message to be broadcast
+   * to all nodes.
+   *
+   * \return the new callback
+   */
   template <typename FunctorT, typename MsgT = GetMsgType<FunctorT>>
   Callback<MsgT> makeBcast();
+
+  /**
+   * \brief Make a void callback to a functor handler to be broadcast to all
+   * nodes.
+   *
+   * \return the new callback
+   */
   template <
     typename FunctorT,
     typename = std::enable_if_t<FunctorTraits<FunctorT>::has_no_msg_type>
   >
   Callback<Void> makeBcast();
+
+  /**
+   * \brief Make a callback to a whole collection invoking a non-intrusive
+   * collection active function handler.
+   *
+   * \param[in] proxy proxy to collection
+   *
+   * \return the new callback
+   */
   template <typename ColT, typename MsgT, ColHanType<ColT,MsgT>* f>
   Callback<MsgT> makeBcast(ColProxyType<ColT> proxy);
+
+  /**
+   * \brief Make a callback to a whole collection invoking an intrusive
+   * collection member handler.
+   *
+   * \param[in] proxy proxy to collection
+   *
+   * \return the new callback
+   */
   template <typename ColT, typename MsgT, ColMemType<ColT,MsgT> f>
   Callback<MsgT> makeBcast(ColProxyType<ColT> proxy);
+
+  /**
+   * \brief Make a callback to a whole objgroup invoking a objgroup member
+   * handler.
+   *
+   * \param[in] proxy proxy to objgroup
+   *
+   * \return the new callback
+   */
   template <typename ObjT, typename MsgT, ObjMemType<ObjT,MsgT> f>
   Callback<MsgT> makeBcast(objgroup::proxy::Proxy<ObjT> proxy);
 
-
 public:
-  /*
-   *  Trigger and send back on a pipe that is not locally triggerable and thus
-   *  requires communication if it is "sent" off-node.
+  /**
+   * \internal \brief Trigger and send back on a pipe that is not locally
+   * triggerable and thus requires communication if it is "sent" off-node.
+   *
+   * \param[in] pipe the pipe ID
+   * \param[in] data the message to send
    */
   template <typename MsgT>
   void triggerSendBack(PipeType const& pipe, MsgT* data);
 
 private:
-  // The group ID used to indicate that the message is being used as a pipe
+  /// The group ID used to indicate that the message is being used as a pipe
   GroupType group_id_ = no_group;
 };
 
