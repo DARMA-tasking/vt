f1962883410b90fa6b6de10255f1ccd0fbc0a020
Jonathan Lifflander <jliffla@sandia.gov>
2020-06-18 15:54:55 -0700

    #672: docs: write collection inner holder doxygen

--- src/vt/vrt/collection/holders/holder.h
+++ src/vt/vrt/collection/holders/holder.h
@@ -63,6 +63,15 @@
 
 namespace vt { namespace vrt { namespace collection {
 
+/**
+ * \struct Holder
+ *
+ * \brief Hold collection elements mapped to this node for a given collection
+ *
+ * Store the unique pointers to collection elements for a given collection
+ * proxy. Provides functionality to find, add, remove, and foreach over the
+ * collection elements.
+ */
 template <typename ColT, typename IndexT>
 struct Holder {
   template <typename T, typename U>
@@ -79,32 +88,200 @@ struct Holder {
   using FuncExprType        = std::function<bool(IndexT const&)>;
   using CountType           = uint64_t;
 
+  /**
+   * \brief Check of index exists here
+   *
+   * \param[in] idx the index
+   *
+   * \return whether it exists
+   */
   bool exists(IndexT const& idx);
+
+  /**
+   * \brief Lookup the inner holder for an index
+   *
+   * \param[in] idx the index
+   *
+   * \return the inner holder with the actual collection element
+   */
   InnerHolder& lookup(IndexT const& idx);
+
+  /**
+   * \brief Insert a new element
+   *
+   * \param[in] idx the index
+   * \param[in] inner the inner holder with the collection element unique pointer
+   */
   void insert(IndexT const& idx, InnerHolder&& inner);
+
+  /**
+   * \brief Remove an element
+   *
+   * \param[in] idx the index
+   *
+   * \return unique pointer to element now removed from holder
+   */
   VirtualPtrType remove(IndexT const& idx);
+
+  /**
+   * \brief Destroy all elements
+   */
   void destroyAll();
+
+  /**
+   * \brief Check if collection has been destroyed
+   *
+   * \return whether it is destroyed
+   */
   bool isDestroyed() const;
+
+  /**
+   * \brief Cleanup delayed deleted elements
+   */
   void cleanupExists();
+
+  /**
+   * \brief Perform apply action over all collection elements
+   *
+   * \param[in] fn apply function for each element
+   *
+   * \return whether it succeeded
+   */
   bool foreach(FuncApplyType fn);
+
+  /**
+   * \brief Count number of elements
+   *
+   * \return number of elements
+   */
   typename TypedIndexContainer::size_type numElements() const;
+
+  /**
+   * \brief Count number of elements that match a expression
+   *
+   * \param[in] f apply function that returns membership of expression
+   *
+   * \return number of elements
+   */
   typename TypedIndexContainer::size_type numElementsExpr(FuncExprType f) const;
+
+  /**
+   * \brief Get current group
+   *
+   * \return the group ID
+   */
   GroupType group() const { return cur_group_; }
+
+  /**
+   * \brief Set a group
+   *
+   * \param[in] group group to set
+   */
   void setGroup(GroupType const& group) { cur_group_ = group; }
+
+  /**
+   * \brief Whether the group should be used
+   *
+   * \return whether it should be used
+   */
   bool useGroup() const { return use_group_; }
+
+  /**
+   * \brief Set whether a group should be used
+   *
+   * \param[in] use_group whether it should be used
+   */
   void setUseGroup(bool const use_group) { use_group_ = use_group; }
+
+  /**
+   * \brief Check if group is ready
+   *
+   * \return whether it is ready
+   */
   bool groupReady() const { return group_ready_; }
+
+  /**
+   * \brief Set if group is ready to be used
+   *
+   * \param[in] ready whether it is ready
+   */
   void setGroupReady(bool const ready) { group_ready_ = ready; }
+
+  /**
+   * \brief Get the root of the group
+   *
+   * \return the group root
+   */
   NodeType groupRoot() const { return group_root_; }
+
+  /**
+   * \brief Set the root of the group
+   *
+   * \param[in] root the root
+   */
   void setGroupRoot(NodeType const root) { group_root_ = root; }
+
+  /**
+   * \brief Get number of elements ready for LB
+   *
+   * \return number of ready elements
+   */
   CountType numReady() const { return elements_ready_; }
+
+  /**
+   * \brief Add number of ready elements for LB
+   *
+   * \param[in] num number of ready elements
+   */
   void addReady(CountType num = 1) { elements_ready_ += 1; }
+
+  /**
+   * \brief Clear all ready elements
+   */
   void clearReady() { elements_ready_ = 0; }
+
+  /**
+   * \brief Add a LB continuation for an element
+   *
+   * \param[in] idx the index
+   * \param[in] fn the continuation
+   */
   void addLBCont(IndexT const& idx, LBContFnType fn);
+
+  /**
+   * \brief Run LB continuations for element
+   *
+   * \param[in] idx the index
+   */
   void runLBCont(IndexT const& idx);
+
+  /**
+   * \brief Run all LB continuations
+   */
   void runLBCont();
+
+  /**
+   * \brief Add element-specific listener
+   *
+   * \param[in] fn listener function
+   *
+   * \return the registered listener entry ID
+   */
   int addListener(listener::ListenFnType<IndexT> fn);
+
+  /**
+   * \brief Remove a element listener
+   *
+   * \param[in] element the entry ID
+   */
   void removeListener(int element);
+
+  /**
+   * \brief Run all listeners
+   *
+   * \param[in] event the event type
+   * \param[in] idx the index to run on
+   */
   void applyListeners(listener::ElementEventEnum event, IndexT const& idx);
 
   friend struct CollectionManager;
