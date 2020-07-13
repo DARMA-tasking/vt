dca7745b43acbd935e7efbf9885bb8c83251ef25
Paul Nathan Stickney <pstickne@gmail.com>
2020-07-12 20:24:44 -0700

    #908 nvcc- add explicit isByteCopyable trait
    - RdmaHandle is used in serialization. However it is neither a primitive,
      nor is it marked as byte-copyable.
    
      This type is appears to be forcing serialization in VT with NVCC,
      and the rules for 'is byte copyable' in VT might need to be unified
      back with checkpoint.

--- src/vt/rdmahandle/manager.impl.h
+++ src/vt/rdmahandle/manager.impl.h
@@ -53,6 +53,8 @@ namespace vt { namespace rdma {
 namespace impl {
 
 struct HandleData {
+  using isByteCopyable = std::true_type;
+
   HandleData();
   HandleData(HandleKey in_key, std::size_t in_size, int in_count)
     : key_(in_key),
