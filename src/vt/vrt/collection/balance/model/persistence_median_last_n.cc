7f086eda09e8f61656d116cbe7840ece2cd5e629
Phil Miller <pbmille@sandia.gov>
2020-07-21 12:31:06 -0400

    #582: Add a forecast model giving the median over the last N phases

---
+++ src/vt/vrt/collection/balance/model/persistence_median_last_n.cc
@@ -0,0 +1,80 @@
+/*
+//@HEADER
+// *****************************************************************************
+//
+//                          persistence_median_last_n.cc
+//                           DARMA Toolkit v. 1.0.0
+//                       DARMA/vt => Virtual Transport
+//
+// Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// *****************************************************************************
+//@HEADER
+*/
+
+#include "vt/config.h"
+#include "vt/vrt/collection/balance/model/persistence_median_last_n.h"
+#include <utility>
+
+namespace vt { namespace vrt { namespace collection { namespace balance {
+
+PersistenceMedianLastN::PersistenceMedianLastN(std::shared_ptr<LoadModel> base, int n)
+  : ComposedModel(base)
+  , n_(n)
+{
+  vtAssert(n > 0, "Cannot take a median over no phases");
+}
+
+TimeType PersistenceMedianLastN::getWork(ElementIDType object, PhaseOffset when)
+{
+  // Retrospective queries don't call for a prospective calculation
+  if (when.phases < 0)
+    return ComposedModel::getWork(object, when);
+
+  int phases = std::min(n_, completed_phases_);
+  std::vector<TimeType> times(phases);
+  for (int i = 1; i <= phases; ++i) {
+    PhaseOffset p{-1*i, when.subphase};
+    TimeType t = ComposedModel::getWork(object, p);
+    times[i-1] = t;
+  }
+
+  std::sort(times.begin(), times.end());
+
+  if (phases % 2 == 1)
+    return times[phases / 2 + 1];
+  else
+    return (times[phases / 2] + times[phases / 2 + 1]) / 2;
+}
+
+}}}}
