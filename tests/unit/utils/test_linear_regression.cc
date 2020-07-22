f8cc0048af2b3c7b2808c6d52989dbad4592ec04
Jonathan Lifflander <jliffla@sandia.gov>
2020-07-22 14:52:27 -0700

    #582: tests: Implement test for linear regression

---
+++ tests/unit/utils/test_linear_regression.cc
@@ -0,0 +1,75 @@
+/*
+//@HEADER
+// *****************************************************************************
+//
+//                          test_linear_regression.cc
+//                           DARMA Toolkit v. 1.0.0
+//                       DARMA/vt => Virtual Transport
+//
+// Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// *****************************************************************************
+//@HEADER
+*/
+
+#include <gtest/gtest.h>
+
+#include <vt/utils/stats/linear_regression.h>
+#include "test_harness.h"
+
+#include <vector>
+
+namespace vt { namespace tests { namespace unit {
+
+using TestLinearRegression = TestHarness;
+
+TEST_F(TestLinearRegression, test_linear_regression_1) {
+  std::vector<double> x, y;
+
+  double slope = 0.5;
+  double intercept = 2;
+
+  for (int i = 1; i < 5; i++) {
+    x.emplace_back(i);
+    y.emplace_back(intercept + slope * i);
+  }
+
+  vt::util::stats::LinearRegression lin{x, y};
+  lin.compute();
+
+  EXPECT_EQ(lin.getSlope(), slope);
+  EXPECT_EQ(lin.getIntercept(), intercept);
+  EXPECT_EQ(lin.predict(5), intercept + slope * 5);
+}
+
+}}} /* end namespace vt::tests::unit */
