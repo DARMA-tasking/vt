31bb62149d9ff2a487c2f1f9f800f29b2f4b8f88
Jonathan Lifflander <jliffla@sandia.gov>
2020-07-22 13:16:16 -0700

    #914: build: improve check linker flags

--- cmake-modules/CheckLinkerFlag.cmake
+++ cmake-modules/CheckLinkerFlag.cmake
@@ -7,8 +7,6 @@ endif()
 
 # This is written because cmake does not support `check_linker_flag` until 3.18
 function(check_linker_flag flag out_var)
-  set(OLD_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
-  set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${flag}")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${flag}")
   check_cxx_compiler_flag("" ${out_var})
-  set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
 endfunction()
