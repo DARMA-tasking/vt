5bd0f1a26a2a9f0ade0dfa8814fab78298bcecf6
Jonathan Lifflander <jliffla@sandia.gov>
2020-07-23 16:15:08 -0700

    #912: CI: add run and upload in test script if enabled

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -11,4 +11,13 @@ pushd "$VT_BUILD"
 
 ctest --output-on-failure | tee cmake-output.log
 
+if test "${CODE_COVERAGE:-0}" -eq 1
+then
+    export CODECOV_TOKEN="bc653fec-7e2c-412a-8b4f-b6db6d703a02"
+    lcov --capture --directory . --output-file coverage.info
+    lcov --remove coverage.info '/usr/*' --output-file coverage.info
+    lcov --list coverage.info
+    bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
+fi
+
 popd
