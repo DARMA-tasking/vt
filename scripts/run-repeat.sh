0320b2f907714aa40c23e24660fbd8ca9b51b3b8
Jonathan Lifflander <jliffla@sandia.gov>
2020-09-28 19:57:19 -0700

    #882: debug: add script for running repeatedly for debugging

---
+++ scripts/run-repeat.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if test $# -lt 2
+then
+    echo "usage: $0 <program-name> nproc> <args>"
+    exit 1;
+fi
+
+pgm=$1
+np=$2
+
+shift
+shift
+
+i=0
+while test $? -eq 0
+do
+    file=$(mktemp)
+    echo "running to output $file : $i"
+    ##echo mpirun -n $np $pgm "$@" 2>&1
+    i=$(expr $i + 1)
+    mpirun -n $np $pgm "$@" 2>&1 > $file
+done
