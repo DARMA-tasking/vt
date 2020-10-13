e871b614df6f74d7cb4a078df4e06d97cfdb069a
Jakub Domagala <domagala.jacob@gmail.com>
2020-10-13 18:15:47 +0200

    #1098: Set default value of vt_ci_build to '0' inside CMake

--- cmake/define_build_types.cmake
+++ cmake/define_build_types.cmake
@@ -151,8 +151,14 @@ else()
   message(STATUS "Building VT with user MPI prevention guards disabled")
   set(vt_feature_cmake_mpi_access_guards "0")
 endif()
-message(STATUS "CI_BUILD = ${vt_ci_build}")
-set(vt_feature_cmake_ci_build ${vt_ci_build})
+
+if(${vt_ci_build})
+  set(vt_feature_cmake_ci_build "1")
+else()
+  set(vt_feature_cmake_ci_build "0")
+endif()
+
+message(STATUS "CI_BUILD = ${vt_feature_cmake_ci_build}")
 
 set(vt_feature_cmake_no_feature "0")
 set(vt_feature_cmake_production "0")
@@ -195,7 +201,7 @@ foreach(loop_build_type ${VT_CONFIG_TYPES})
   )
 
   # assume production mode for everything except debug or CI build
-  if (loop_build_type STREQUAL "debug" OR ${vt_ci_build})
+  if (loop_build_type STREQUAL "debug" OR ${vt_feature_cmake_ci_build})
     set(vt_feature_cmake_production "0")
   else()
     set(vt_feature_cmake_production "1")
