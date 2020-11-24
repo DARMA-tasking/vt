5faf0bd69593521e84c2eda01b5cff2250d7620c
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2020-11-24 22:00:17 +0100

    #1009: memory footprinting: use develop branch for checkpoint
    This reverts commit a292ff482.

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -7,7 +7,7 @@ build_dir=${2}
 
 # Dependency versions, when fetched via git.
 detector_rev=master
-checkpoint_rev=156-footprint-non-serializable-elements
+checkpoint_rev=develop
 
 if test "${VT_DOXYGEN_ENABLED:-0}" -eq 1
 then
