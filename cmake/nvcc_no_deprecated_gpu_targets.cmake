97bebae446e4e6b8e750925b079af1f577d5a874
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2021-01-04 17:56:08 +0100

    #1184: add no-deprecated-gpu-targets flag for NVCC

---
+++ cmake/nvcc_no_deprecated_gpu_targets.cmake
@@ -0,0 +1,5 @@
+# Uses flag "-Wno-deprecated-gpu-targets" for NVCC builds
+
+if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "NVIDIA")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-gpu-targets")
+endif ()
