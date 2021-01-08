2027e6b2f18c994c74c5f87c86ffab341f4f2811
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2021-01-08 18:50:13 +0100

    #1184: fix checking if chosen compiler is nvcc

--- cmake/nvcc_no_deprecated_gpu_targets.cmake
+++ cmake/nvcc_no_deprecated_gpu_targets.cmake
@@ -1,5 +1,5 @@
 # Add flag "-Wno-deprecated-gpu-targets" for NVCC builds
 
-if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "NVIDIA")
+if($ENV{CXX} MATCHES "nvcc_wrapper")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-gpu-targets")
-endif ()
+endif()
