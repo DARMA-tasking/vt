23b17029a3f7b61a676ac2bd77fea69ff5ecbc13
Jakub Domagala <domagala.jacob@gmail.com>
2021-03-04 19:46:06 +0100

    #1272: scheduler: Remove the unwanted API for triggering the scheduler

--- src/vt/runtime/runtime.h
+++ src/vt/runtime/runtime.h
@@ -191,11 +191,6 @@ struct Runtime {
   void computeAndPrintDiagnostics();
 
   /**
-   * \internal \brief Run the scheduler once
-   */
-  void runScheduler();
-
-  /**
    * \brief Abort--die immediately after spitting out error message
    *
    * \param[in] abort_str the error message
