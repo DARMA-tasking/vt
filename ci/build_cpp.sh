29aa20ae976c1e5f1115c2869df2587c74570fe6
Jakub Domagala <domagala.jacob@gmail.com>
2021-03-08 20:20:14 +0100

    #1297: CI: Add vt_tests_num_nodes CMake flag to set maximum number of ranks/nodes for tests and examples

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -130,6 +130,7 @@ cmake -G "${CMAKE_GENERATOR:-Ninja}" \
       -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH:-}" \
       -DCMAKE_INSTALL_PREFIX="$VT_BUILD/install" \
       -Dvt_ci_build="${VT_CI_BUILD:-0}" \
+      -Dvt_tests_num_nodes="${VT_TESTS_NUM_NODES:-2}" \
       "$VT"
 cmake_conf_ret=$?
 
