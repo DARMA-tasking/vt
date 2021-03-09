1f7339540137b2f2fcc3376b1e7fddae2653f429
Phil Miller <pbmille@sandia.gov>
2021-03-09 11:33:06 -0700

    #1311: Fix cmake to not add duplicate -std=c++14 flag that bothers nvcc

--- cmake-modules/SetCXXCompilerFlags.cmake
+++ cmake-modules/SetCXXCompilerFlags.cmake
@@ -3,28 +3,26 @@
 
 macro(set_darma_compiler_flags)
 
-set(CXX_STANDARD_FLAGS)
+set_property(DIRECTORY PROPERTY CXX_STANDARD 14)
+
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
   # 4.9.3 complains about std::min not being constexpr
-  set(CXX_STANDARD_FLAGS -std=c++1y)
   if (NOT (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 5 OR CMAKE_CXX_COMPILER_VERSION VERSION_EQUAL 5))
     message("${PROJECT_NAME} currently requires g++ 5 or greater.  If you need it to work with 4.9, please complain.")
   endif ()
 elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
-  set(CXX_STANDARD_FLAGS -std=c++1y -ftemplate-depth=900)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftemplate-depth=900")
   if (APPLE)
-    list(APPEND CXX_STANDARD_FLAGS -stdlib=libc++ -DCLI11_EXPERIMENTAL_OPTIONAL=0)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -DCLI11_EXPERIMENTAL_OPTIONAL=0")
   endif ()
   if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 7 OR CMAKE_CXX_COMPILER_VERSION VERSION_EQUAL 7)
-    list(APPEND CXX_STANDARD_FLAGS -DCLI11_EXPERIMENTAL_OPTIONAL=0)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DCLI11_EXPERIMENTAL_OPTIONAL=0")
   endif()
 elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Intel")
+  # No-op
   # 16.0.3 complains about std::min not being constexpr
-  set(${CXX_STANDARD_FLAGS} -std=c++14)
 else ()
   message(FATAL_ERROR "Your C++ compiler may not support C++14.")
 endif ()
 
 endmacro()
-
-
