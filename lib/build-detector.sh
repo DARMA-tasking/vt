27413a5db0356cb5e64581abcdc977ffee06126d
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2021-03-31 22:01:47 +0200

    #1355: CI: remove obsolete files

--- lib/build-detector.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cmake ../detector \
-      -DCMAKE_INSTALL_PREFIX=`pwd` \
-      -DCMAKE_CXX_COMPILER=clang++-mp-3.9 \
-      -DCMAKE_C_COMPILER=clang-mp-3.9 \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=true
