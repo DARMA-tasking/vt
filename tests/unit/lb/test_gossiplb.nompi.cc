7da1ddef56b195379631c8a6f7e7dfedd52b4a44
Nicole Lemaster Slattengren <nlslatt@sandia.gov>
2021-04-12 09:41:35 -0700

    #1369: tests: duplicate less code

--- tests/unit/lb/test_gossiplb.nompi.cc
+++ tests/unit/lb/test_gossiplb.nompi.cc
@@ -56,10 +56,10 @@ using ElementIDStruct = vt::vrt::collection::balance::ElementIDStruct;
 using ElementIDType = vt::vrt::collection::balance::ElementIDType;
 using ObjectOrdering = vt::vrt::collection::lb::ObjectOrderEnum;
 
-void setupProblem(
-  std::unordered_map<ElementIDStruct, TimeType> &cur_objs,
-  TimeType &my_load
+TimeType setupProblem(
+  std::unordered_map<ElementIDStruct, TimeType> &cur_objs
 ) {
+  // total load of 29 seconds
   cur_objs.emplace(ElementIDStruct{3,0,0}, 4.0);
   cur_objs.emplace(ElementIDStruct{5,0,0}, 5.0);
   cur_objs.emplace(ElementIDStruct{2,0,0}, 9.0);
@@ -67,161 +67,138 @@ void setupProblem(
   cur_objs.emplace(ElementIDStruct{1,0,0}, 6.0);
   cur_objs.emplace(ElementIDStruct{4,0,0}, 3.0);
 
-  my_load = 0;
+  // compute the load for this processor
+  TimeType my_load = 0;
   for (auto &obj : cur_objs) {
-    my_load += vt::vrt::collection::lb::BaseLB::loadMilli(obj.second);
+    my_load += obj.second;
   }
+  return my_load;
 }
 
-TEST_F(TestGossipLB, test_gossiplb_ordering_elmid) {
-  std::unordered_map<ElementIDStruct, TimeType> cur_objs;
-  TimeType my_load = 0;
-  setupProblem(cur_objs, my_load);
-
-  ObjectOrdering order = ObjectOrdering::ElmID;
-  TimeType over_avg = vt::vrt::collection::lb::BaseLB::loadMilli(4.5);
-  TimeType target_load = my_load - over_avg;
-  std::vector<ElementIDType> soln = {0, 1, 2, 3, 4, 5};
-
+void orderAndVerify(
+  ObjectOrdering order,
+  const std::unordered_map<ElementIDStruct, TimeType> &cur_objs,
+  TimeType my_load_ms, TimeType target_load_ms,
+  const std::vector<ElementIDType> &soln
+) {
+  // have GossipLB order the objects
   auto ordered_objs = vt::vrt::collection::lb::GossipLB::orderObjects(
-    order, cur_objs, my_load, target_load
+    order, cur_objs, my_load_ms, target_load_ms
   );
 
+  // verify correctness of the returned ordering
   int i = 0;
   for (auto obj_id : ordered_objs) {
     EXPECT_EQ(obj_id.id, soln[i++]);
   }
 }
 
-TEST_F(TestGossipLB, test_gossiplb_ordering_leastmigrations) {
+void orderUsingOverloadAndVerify(
+  ObjectOrdering order, TimeType over_avg_sec /* my_load-target_load */,
+  const std::vector<ElementIDType> &soln
+) {
   std::unordered_map<ElementIDStruct, TimeType> cur_objs;
-  TimeType my_load = 0;
-  setupProblem(cur_objs, my_load);
+  TimeType my_load_ms = vt::vrt::collection::lb::BaseLB::loadMilli(
+    setupProblem(cur_objs)
+  );
 
+  // we know how overloaded we are and need to compute the target load
+  TimeType target_load_ms = my_load_ms -
+    vt::vrt::collection::lb::BaseLB::loadMilli(over_avg_sec);
+
+  orderAndVerify(order, cur_objs, my_load_ms, target_load_ms, soln);
+}
+
+void orderUsingTargetLoadAndVerify(
+  ObjectOrdering order, TimeType target_load_sec,
+  const std::vector<ElementIDType> &soln
+) {
+  std::unordered_map<ElementIDStruct, TimeType> cur_objs;
+  TimeType my_load_ms = vt::vrt::collection::lb::BaseLB::loadMilli(
+    setupProblem(cur_objs)
+  );
+
+  // we were given the target load directly but need to convert units
+  TimeType target_load_ms = vt::vrt::collection::lb::BaseLB::loadMilli(
+    target_load_sec
+  );
+
+  orderAndVerify(order, cur_objs, my_load_ms, target_load_ms, soln);
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+TEST_F(TestGossipLB, test_gossiplb_ordering_elmid) {
+  ObjectOrdering order = ObjectOrdering::ElmID;
+  TimeType over_avg = 4.5;
+  // result will be independent of over_avg
+  std::vector<ElementIDType> soln = {0, 1, 2, 3, 4, 5};
+
+  orderUsingOverloadAndVerify(order, over_avg, soln);
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+TEST_F(TestGossipLB, test_gossiplb_ordering_leastmigrations_intermediate) {
   ObjectOrdering order = ObjectOrdering::LeastMigrations;
-  TimeType over_avg = vt::vrt::collection::lb::BaseLB::loadMilli(4.5);
-  TimeType target_load = my_load - over_avg;
+  TimeType over_avg = 4.5;
   // single_obj_load will be 5.0
   // load order will be 5.0, 4.0, 3.0, 2.0, 6.0, 9.0
   std::vector<ElementIDType> soln = {5, 3, 4, 0, 1, 2};
 
-  auto ordered_objs = vt::vrt::collection::lb::GossipLB::orderObjects(
-    order, cur_objs, my_load, target_load
-  );
-
-  int i = 0;
-  for (auto obj_id : ordered_objs) {
-    EXPECT_EQ(obj_id.id, soln[i++]);
-  }
+  orderUsingOverloadAndVerify(order, over_avg, soln);
 }
 
 TEST_F(TestGossipLB, test_gossiplb_ordering_leastmigrations_largest) {
-  std::unordered_map<ElementIDStruct, TimeType> cur_objs;
-  TimeType my_load = 0;
-  setupProblem(cur_objs, my_load);
-
   ObjectOrdering order = ObjectOrdering::LeastMigrations;
-  TimeType over_avg = vt::vrt::collection::lb::BaseLB::loadMilli(12.5);
-  TimeType target_load = my_load - over_avg;
+  TimeType over_avg = 12.5;
   // single_obj_load will be 9.0
   // load order will be 9.0, 6.0, 5.0, 4.0, 3.0, 2.0
   std::vector<ElementIDType> soln = {2, 1, 5, 3, 4, 0};
 
-  auto ordered_objs = vt::vrt::collection::lb::GossipLB::orderObjects(
-    order, cur_objs, my_load, target_load
-  );
-
-  int i = 0;
-  for (auto obj_id : ordered_objs) {
-    EXPECT_EQ(obj_id.id, soln[i++]);
-  }
+  orderUsingOverloadAndVerify(order, over_avg, soln);
 }
 
 TEST_F(TestGossipLB, test_gossiplb_ordering_leastmigrations_smallest) {
-  std::unordered_map<ElementIDStruct, TimeType> cur_objs;
-  TimeType my_load = 0;
-  setupProblem(cur_objs, my_load);
-
   ObjectOrdering order = ObjectOrdering::LeastMigrations;
-  TimeType over_avg = vt::vrt::collection::lb::BaseLB::loadMilli(1.5);
-  TimeType target_load = my_load - over_avg;
+  TimeType over_avg = 1.5;
   // single_obj_load will be 2.0
   // load order will be 2.0, 3.0, 4.0, 5.0, 6.0, 9.0
   std::vector<ElementIDType> soln = {0, 4, 3, 5, 1, 2};
 
-  auto ordered_objs = vt::vrt::collection::lb::GossipLB::orderObjects(
-    order, cur_objs, my_load, target_load
-  );
-
-  int i = 0;
-  for (auto obj_id : ordered_objs) {
-    EXPECT_EQ(obj_id.id, soln[i++]);
-  }
+  orderUsingOverloadAndVerify(order, over_avg, soln);
 }
 
-TEST_F(TestGossipLB, test_gossiplb_ordering_smallestobjects) {
-  std::unordered_map<ElementIDStruct, TimeType> cur_objs;
-  TimeType my_load = 0;
-  setupProblem(cur_objs, my_load);
+///////////////////////////////////////////////////////////////////////////
 
+TEST_F(TestGossipLB, test_gossiplb_ordering_smallestobjects_intermediate) {
   ObjectOrdering order = ObjectOrdering::SmallestObjects;
-  TimeType over_avg = vt::vrt::collection::lb::BaseLB::loadMilli(4.5);
-  TimeType target_load = my_load - over_avg;
+  TimeType over_avg = 4.5;
   // marginal_obj_load will be 3.0
   // load order will be 3.0, 2.0, 4.0, 5.0, 6.0, 9.0
   std::vector<ElementIDType> soln = {4, 0, 3, 5, 1, 2};
 
-  auto ordered_objs = vt::vrt::collection::lb::GossipLB::orderObjects(
-    order, cur_objs, my_load, target_load
-  );
-
-  int i = 0;
-  for (auto obj_id : ordered_objs) {
-    EXPECT_EQ(obj_id.id, soln[i++]);
-  }
+  orderUsingOverloadAndVerify(order, over_avg, soln);
 }
 
 TEST_F(TestGossipLB, test_gossiplb_ordering_smallestobjects_largest) {
-  std::unordered_map<ElementIDStruct, TimeType> cur_objs;
-  TimeType my_load = 0;
-  setupProblem(cur_objs, my_load);
-
   ObjectOrdering order = ObjectOrdering::SmallestObjects;
-  TimeType target_load = vt::vrt::collection::lb::BaseLB::loadMilli(0.5);
+  TimeType target_load = 0.5;
   // marginal_obj_load will be 9.0
   // load order will be 9.0, 6.0, 5.0, 4.0, 3.0, 2.0
   std::vector<ElementIDType> soln = {2, 1, 5, 3, 4, 0};
 
-  auto ordered_objs = vt::vrt::collection::lb::GossipLB::orderObjects(
-    order, cur_objs, my_load, target_load
-  );
-
-  int i = 0;
-  for (auto obj_id : ordered_objs) {
-    EXPECT_EQ(obj_id.id, soln[i++]);
-  }
+  orderUsingTargetLoadAndVerify(order, target_load, soln);
 }
 
 TEST_F(TestGossipLB, test_gossiplb_ordering_smallestobjects_smallest) {
-  std::unordered_map<ElementIDStruct, TimeType> cur_objs;
-  TimeType my_load = 0;
-  setupProblem(cur_objs, my_load);
-
   ObjectOrdering order = ObjectOrdering::SmallestObjects;
-  TimeType over_avg = vt::vrt::collection::lb::BaseLB::loadMilli(1.5);
-  TimeType target_load = my_load - over_avg;
+  TimeType over_avg = 1.5;
   // marginal_obj_load will be 2.0
   // load order will be 2.0, 3.0, 4.0, 5.0, 6.0, 9.0
   std::vector<ElementIDType> soln = {0, 4, 3, 5, 1, 2};
 
-  auto ordered_objs = vt::vrt::collection::lb::GossipLB::orderObjects(
-    order, cur_objs, my_load, target_load
-  );
-
-  int i = 0;
-  for (auto obj_id : ordered_objs) {
-    EXPECT_EQ(obj_id.id, soln[i++]);
-  }
+  orderUsingOverloadAndVerify(order, over_avg, soln);
 }
 
 }}} // end namespace vt::tests::unit
