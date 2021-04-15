d5fde1813e2fba7a220cbccb1b472df81a6566c2
Jonathan Lifflander <jliffla@sandia.gov>
2021-04-15 15:07:29 -0700

    #1362: tests: adjust for removal of serialize

--- tests/unit/location/test_location_common.h
+++ tests/unit/location/test_location_common.h
@@ -98,9 +98,7 @@ struct LongMsg : vt::LocationRoutedMsg<int, vt::Message> {
 struct SerialMsg : ShortMsg {
   SerialMsg(int in_entity, vt::NodeType in_from)
     : ShortMsg(in_entity, in_from)
-  {
-    setSerialize(true);
-  }
+  { }
 };
 
 template <typename MsgT>
@@ -149,15 +147,13 @@ void verifyCacheConsistency(
   for (int iter = 0; iter < nb_rounds; ++iter) {
     // create an entity message to route
     auto msg = vt::makeMessage<MsgT>(entity, my_node);
-    // check if should be serialized or not
-    bool serialize = msg->getSerialize();
 
     // perform the checks only after all entity messages have been
     // correctly delivered
     runInEpochCollective([&]{
       if (my_node not_eq home) {
         // route entity message
-        vt::theLocMan()->virtual_loc->routeMsg<MsgT>(entity, home, msg, serialize);
+        vt::theLocMan()->virtual_loc->routeMsg<MsgT>(entity, home, msg);
       }
     });
 
@@ -171,8 +167,8 @@ void verifyCacheConsistency(
       vt_debug_print(
         normal, location,
         "verifyCacheConsistency: iter={}, entityID={}, home={}, bytes={}, "
-        "in cache={}, serialize={}\n",
-        iter, entity, msg->from_, sizeof(*msg), is_entity_cached, serialize
+        "in cache={}\n",
+        iter, entity, msg->from_, sizeof(*msg), is_entity_cached
       );
 
       if (not is_eager) {
