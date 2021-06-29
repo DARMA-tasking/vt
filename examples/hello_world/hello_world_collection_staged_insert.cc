8af8f110d75d2f4e1901a77d6185f51d3c6a9f99
Jakub Domagala <domagala.jacob@gmail.com>
2021-06-29 12:15:56 +0200

    #1482: license: Update license year to 2019-2021 and apply it on all files that include the license

--- examples/hello_world/hello_world_collection_staged_insert.cc
+++ examples/hello_world/hello_world_collection_staged_insert.cc
@@ -5,7 +5,7 @@
 //                   hello_world_collection_staged_insert.cc
 //                       DARMA/vt => Virtual Transport
 //
-// Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+// Copyright 2019-2021 National Technology & Engineering Solutions of Sandia, LLC
 // (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
 // Government retains certain rights in this software.
 //
--- tests/unit/scheduler/test_high_level_calls.cc
+++ tests/unit/scheduler/test_high_level_calls.cc
@@ -5,7 +5,7 @@
 //                           test_high_level_calls.cc
 //                       DARMA/vt => Virtual Transport
 //
-// Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+// Copyright 2019-2021 National Technology & Engineering Solutions of Sandia, LLC
 // (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
 // Government retains certain rights in this software.
 //
