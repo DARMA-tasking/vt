12fb7f1abb605adee26c610feeff172f28696e83
Jonathan Lifflander <jliffla@sandia.gov>
2021-06-30 09:47:06 -0700

    #1490: vrt_coll: implement new restoreFromFileInPlace

--- src/vt/vrt/collection/manager.impl.h
+++ src/vt/vrt/collection/manager.impl.h
@@ -3161,6 +3161,86 @@ void CollectionManager::checkpointToFile(
 }
 
 template <typename ColT>
+/*static*/ void CollectionManager::restoreHandler(RestoreMigrateMsg<ColT>* msg) {
+  auto idx = msg->idx_;
+  auto node = msg->to_node_;
+  auto proxy = msg->proxy_;
+  theCollection()->migrate(proxy(idx), node);
+}
+
+template <typename ColT>
+void CollectionManager::restoreFromFileInPlace(
+  CollectionProxyWrapType<ColT> proxy, typename ColT::IndexType range,
+  std::string const& file_base
+) {
+  using IndexType = typename ColT::IndexType;
+  using DirectoryType = CollectionDirectory<IndexType>;
+
+  auto proxy_bits = proxy.getProxy();
+
+  auto metadata_file_name = makeMetaFilename<IndexType>(file_base, false);
+
+  if (access(metadata_file_name.c_str(), F_OK) == -1) {
+    // file doesn't exist, try looking in sub-directory
+    metadata_file_name = makeMetaFilename<IndexType>(file_base, true);
+  }
+
+  if (access(metadata_file_name.c_str(), F_OK) == -1) {
+    throw std::runtime_error("Collection directory file cannot be found");
+  }
+
+  auto directory = checkpoint::deserializeFromFile<DirectoryType>(
+    metadata_file_name
+  );
+
+  runInEpochCollective([&]{
+    for (auto&& elm : directory->elements_) {
+      auto idx = elm.idx_;
+      auto file_name = elm.file_name_;
+
+      if (proxy(idx).tryGetLocalPtr() == nullptr) {
+        auto mapped_node = getMappedNode<ColT, IndexType>(proxy, idx);
+        vtAssertExpr(mapped_node != uninitialized_destination);
+        auto this_node = theContext()->getNode();
+
+        auto msg = makeMessage<RestoreMigrateMsg<ColT>>(this_node, idx, proxy);
+        theMsg()->sendMsg<
+          RestoreMigrateMsg<ColT>, restoreHandler<ColT>
+        >(mapped_node, msg);
+      }
+    }
+  });
+
+  for (auto&& elm : directory->elements_) {
+    auto idx = elm.idx_;
+    auto file_name = elm.file_name_;
+    vtAssertExpr(proxy(idx).tryGetLocalPtr() != nullptr);
+
+    auto holder = findColHolder<ColT, IndexType>(proxy_bits);
+    vtAssertExpr(holder != nullptr);
+
+    auto elm_holder = findElmHolder<ColT,IndexType>(proxy_bits);
+    auto const elm_exists = elm_holder->exists(idx);
+    vtAssertExpr(elm_exists);
+
+#if 0
+    auto col_ptr = checkpoint::deserializeFromFile<ColT>(file_name);
+    col_ptr->stats_.resetPhase();
+
+    auto map_han = UniversalIndexHolder<>::getMap(proxy_bits);
+    elm_holder->remove(idx);
+    elm_holder->insert(idx, typename Holder<ColT, IndexType>::InnerHolder{
+      std::move(col_ptr), map_han, range
+    });
+#else
+    auto ptr = elm_holder->lookup(idx).getCollection();
+    checkpoint::deserializeInPlaceFromFile<ColT>(file_name, static_cast<ColT*>(ptr));
+    ptr->stats_.resetPhase();
+#endif
+  }
+}
+
+template <typename ColT>
 CollectionManager::CollectionProxyWrapType<ColT>
 CollectionManager::restoreFromFile(
   typename ColT::IndexType range, std::string const& file_base
