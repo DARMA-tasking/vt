670a498ff710c3b199c91258ddfb2037ca0351cf
Jonathan Lifflander <jliffla@sandia.gov>
2021-08-04 15:22:57 -0700

    #1512: cmake: check for when find_package exits early when part of parent

--- cmake/load_local_packages.cmake
+++ cmake/load_local_packages.cmake
@@ -7,24 +7,20 @@ include(cmake/local_package.cmake)
 
 if (EXISTS "${PROJECT_LIB_DIR}/detector")
   add_subdirectory(${PROJECT_LIB_DIR}/detector)
-  set(vt_find_detector_dep 0)
 else()
   # require directories for these packages
   require_pkg_directory(detector "VT detector library")
   # find these required packages locally
   find_package_local(detector "${detector_DIR}" detector)
-  set(vt_find_detector_dep 1)
 endif()
 
 if (EXISTS "${PROJECT_LIB_DIR}/checkpoint")
   add_subdirectory(${PROJECT_LIB_DIR}/checkpoint)
-  set(vt_find_checkpoint_dep 0)
 else()
   # require directories for these packages
   require_pkg_directory(checkpoint "VT checkpoint library")
   # find these required packages locally
   find_package_local(checkpoint "${checkpoint_DIR}" checkpoint)
-  set(vt_find_checkpoint_dep 1)
 endif()
 
 set(CHECKPOINT_LIBRARY vt::lib::checkpoint)
