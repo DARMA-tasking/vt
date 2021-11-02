faeaa4e14cbf2353486540f57eca86b909236e2e
Nicolas Morales <nmmoral@sandia.gov>
2021-11-02 10:20:49 -0700

    #1476: epoch_guard: fix license headers

--- src/vt/termination/epoch_guard.h
+++ src/vt/termination/epoch_guard.h
@@ -2,7 +2,7 @@
 //@HEADER
 // *****************************************************************************
 //
-//                                 epoch_guard.h
+//                                epoch_guard.h
 //                       DARMA/vt => Virtual Transport
 //
 // Copyright 2019-2021 National Technology & Engineering Solutions of Sandia, LLC
--- tests/unit/termination/test_epoch_guard.cc
+++ tests/unit/termination/test_epoch_guard.cc
@@ -2,7 +2,7 @@
 //@HEADER
 // *****************************************************************************
 //
-//                                 test_epoch_guard.cc
+//                             test_epoch_guard.cc
 //                       DARMA/vt => Virtual Transport
 //
 // Copyright 2019-2021 National Technology & Engineering Solutions of Sandia, LLC
