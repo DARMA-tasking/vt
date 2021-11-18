2be3b00e267e0895a46a7ee6ad9fd0cae6dcc1be
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2021-11-18 15:55:18 +0100

    #1602: use target_compile_options for enabling warnings

--- cmake/turn_on_warnings.cmake
+++ cmake/turn_on_warnings.cmake
@@ -1,26 +1,25 @@
 include(CheckCXXCompilerFlag)
 
 function(enable_cxx_compiler_flag_if_supported flag)
-  string(FIND "${CMAKE_CXX_FLAGS}" "${flag}" flag_already_set)
-  if(flag_already_set EQUAL -1)
-    check_cxx_compiler_flag("${flag}" flag_supported)
-    if(flag_supported)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" PARENT_SCOPE)
-    endif()
-    unset(flag_supported CACHE)
+  check_cxx_compiler_flag("${flag}" flag_supported)
+  if(flag_supported)
+    target_compile_options(${vt_target} PUBLIC ${flag})
   endif()
+  unset(flag_supported CACHE)
 endfunction()
 
-if(NOT hasParent)
-  enable_cxx_compiler_flag_if_supported("-Wall")
-  enable_cxx_compiler_flag_if_supported("-pedantic")
-  enable_cxx_compiler_flag_if_supported("-Wshadow")
-  enable_cxx_compiler_flag_if_supported("-Wno-unknown-pragmas")
-  enable_cxx_compiler_flag_if_supported("-Wsign-compare")
-  # Not really a warning, is still diagnostic related..
-  enable_cxx_compiler_flag_if_supported("-ftemplate-backtrace-limit=100")
+function(turn_on_warnings vt_target)
+  if(NOT hasParent)
+    enable_cxx_compiler_flag_if_supported("-Wall")
+    enable_cxx_compiler_flag_if_supported("-pedantic")
+    enable_cxx_compiler_flag_if_supported("-Wshadow")
+    enable_cxx_compiler_flag_if_supported("-Wno-unknown-pragmas")
+    enable_cxx_compiler_flag_if_supported("-Wsign-compare")
+    # Not really a warning, is still diagnostic related..
+    enable_cxx_compiler_flag_if_supported("-ftemplate-backtrace-limit=100")
 
-  if (vt_werror_enabled)   # Treat warning as errors
-    enable_cxx_compiler_flag_if_supported("-Werror")
+    if (vt_werror_enabled)   # Treat warning as errors
+      enable_cxx_compiler_flag_if_supported("-Werror")
+    endif()
   endif()
-endif()
+endfunction()
