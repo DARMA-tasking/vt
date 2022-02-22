82e299ba44d4b46ff201a6c564561ff571abe519
Nicolas Morales <nmmoral@sandia.gov>
2022-02-21 16:53:59 -0800

    #1677: cmake: run generator for git configurator as a custom command dependency of VT

--- cmake/build_git_info.cmake
+++ cmake/build_git_info.cmake
@@ -33,4 +33,13 @@ set(HEAD_FILE "${GIT_DIR}/HEAD")
 message(STATUS "Git HEAD file: \"${HEAD_FILE}\"")
 
 find_package(Git REQUIRED)
-execute_process(COMMAND ${CMAKE_COMMAND} -DIN_FILE=${PROJECT_BASE_DIR}/vt_git_revision.cc.in -DOUT_FILE=${PROJECT_BIN_DIR}/src/vt/configs/generated/vt_git_revision.cc -DGIT_EXECUTABLE=${GIT_EXECUTABLE} -DGIT_DIR=${GIT_DIR} -DHEAD_FILE=${HEAD_FILE} -P ${CMAKE_CURRENT_LIST_DIR}/run-git.cmake)
+#execute_process(COMMAND ${CMAKE_COMMAND} -DIN_FILE=${PROJECT_BASE_DIR}/vt_git_revision.cc.in -DOUT_FILE=${PROJECT_BIN_DIR}/src/vt/configs/generated/vt_git_revision.cc -DGIT_EXECUTABLE=${GIT_EXECUTABLE} -DGIT_DIR=${GIT_DIR} -DHEAD_FILE=${HEAD_FILE} -P ${CMAKE_CURRENT_LIST_DIR}/run-git.cmake)
+
+set(VT_GIT_CONFIG_FILE "${PROJECT_BIN_DIR}/src/vt/configs/generated/vt_git_revision.cc")
+add_custom_command(
+        OUTPUT ${VT_GIT_CONFIG_FILE}
+        COMMAND ${CMAKE_COMMAND} -DIN_FILE=${PROJECT_BASE_DIR}/vt_git_revision.cc.in -DOUT_FILE=${VT_GIT_CONFIG_FILE} -DGIT_EXECUTABLE=${GIT_EXECUTABLE} -DGIT_DIR=${GIT_DIR} -DHEAD_FILE=${HEAD_FILE} -P ${CMAKE_CURRENT_LIST_DIR}/run-git.cmake
+        DEPENDS ${HEAD_FILE}
+        )
+
+target_sources(${VIRTUAL_TRANSPORT_LIBRARY} PRIVATE ${VT_GIT_CONFIG_FILE})
