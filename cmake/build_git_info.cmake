2cf7f5ec3fbf0420b3d803251fd9b894fa1d79c9
Nicolas Morales <nmmoral@sandia.gov>
2022-02-21 16:31:13 -0800

    #1677: cmake: Move git configuration detection into a standalone cmake script

--- cmake/build_git_info.cmake
+++ cmake/build_git_info.cmake
@@ -1,34 +1,36 @@
+function(get_git_dir _git_dir)
+    set(GIT_PARENT_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+    set(GIT_DIR "${GIT_PARENT_DIR}/.git")
+    while(NOT EXISTS "${GIT_DIR}")	# .git dir not found, search parent directories
+        set(GIT_PREVIOUS_PARENT "${GIT_PARENT_DIR}")
+        get_filename_component(GIT_PARENT_DIR ${GIT_PARENT_DIR} PATH)
+        if(GIT_PARENT_DIR STREQUAL GIT_PREVIOUS_PARENT)
+            # We have reached the root directory, we are not in git
+            set(${_refspecvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
+            set(${_hashvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
+            return()
+        endif()
+        set(GIT_DIR "${GIT_PARENT_DIR}/.git")
+    endwhile()
+    # check if this is a submodule
+    if(NOT IS_DIRECTORY ${GIT_DIR})
+        file(READ ${GIT_DIR} submodule)
+        string(REGEX REPLACE "gitdir: (.*)\n$" "\\1" GIT_DIR_RELATIVE ${submodule})
+        get_filename_component(SUBMODULE_DIR ${GIT_DIR} PATH)
+        get_filename_component(GIT_DIR ${SUBMODULE_DIR}/${GIT_DIR_RELATIVE} ABSOLUTE)
+    endif()
 
-include(GetGitRevisionDescription)
+    set(${_git_dir} ${GIT_DIR} PARENT_SCOPE)
+endfunction()
 
-get_git_head_revision(GIT_REFSPEC GIT_SHA1)
+get_git_dir(GIT_DIR)
 
-# set some variables related to GIT state information
-get_git_head_revision(GIT_REFSPEC GIT_SHA1)
-git_describe(GIT_EXACT_TAG --tags --abbrev=0 --all)
-git_describe(GIT_DESCRIPTION --abbrev=10 --always --tags --long --all)
-git_local_changes(GIT_CLEAN_STATUS)
+if(NOT EXISTS "${GIT_DIR}/HEAD")
+    message(FATAL_ERROR "no such file: \"${GIT_DIR}/HEAD\"")
+endif()
+set(HEAD_FILE "${GIT_DIR}/HEAD")
 
-message(STATUS "REF:${GIT_REFSPEC}")
-message(STATUS "REF:${GIT_SHA1}")
-message(STATUS "REF:${GIT_DESCRIPTION}")
-message(STATUS "REF:${GIT_CLEAN_STATUS}")
-message(STATUS "REF:${GIT_EXACT_TAG}")
+message(STATUS "Git HEAD file: \"${HEAD_FILE}\"")
 
-configure_file(
-  ${PROJECT_BASE_DIR}/vt_git_revision.cc.in
-  ${PROJECT_BIN_DIR}/src/vt/configs/generated/vt_git_revision.cc
-  @ONLY
-)
-
-# install(
-#   FILES            "${PROJECT_BINARY_DIR}/${cur_build_type}/cmake_config.h"
-#   DESTINATION      include
-#   CONFIGURATIONS   ${cur_build_type}
-# )
-
-# configure_file(
-#   "${PROJECT_SOURCE_DIR}/vt_git_revision.cc.in"
-#   "${CMAKE_CURRENT_BINARY_DIR}/vt_git_revision.cc"
-#   @ONLY
-# )
+find_package(Git REQUIRED)
+execute_process(COMMAND ${CMAKE_COMMAND} -DIN_FILE=${PROJECT_BASE_DIR}/vt_git_revision.cc.in -DOUT_FILE=${PROJECT_BIN_DIR}/src/vt/configs/generated/vt_git_revision.cc -DGIT_EXECUTABLE=${GIT_EXECUTABLE} -DGIT_DIR=${GIT_DIR} -DHEAD_FILE=${HEAD_FILE} -P ${CMAKE_CURRENT_LIST_DIR}/run-git.cmake)
