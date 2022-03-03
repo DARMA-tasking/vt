0b74ed163fdd7ea39e0e71d0af7b8adb7506b0ad
Nicolas Morales <nmmoral@sandia.gov>
2022-03-03 09:55:22 -0800

    #1692: make build script not error out when running cmake if .git folder is not available

--- cmake/run-git.cmake
+++ cmake/run-git.cmake
@@ -14,7 +14,10 @@
 message(STATUS "Reading head file ${HEAD_FILE}")
 message(STATUS "Using git executable at \"${GIT_EXECUTABLE}\"")
 
-get_filename_component(ROOT_DIR ${GIT_DIR} DIRECTORY)
+set(ROOT_DIR)
+if (GIT_DIR)
+    get_filename_component(ROOT_DIR ${GIT_DIR} DIRECTORY)
+endif()
 
 set(GIT_SHA1)
 
@@ -39,7 +42,7 @@ execute_process(COMMAND
         GIT_SHA1
         OUTPUT_STRIP_TRAILING_WHITESPACE)
 if(NOT res EQUAL 0)
-    message(FATAL_ERROR "could not get the git sha1")
+    message(STATUS "could not get the git sha1")
 endif()
 
 message(STATUS "GIT_SHA1: \"${GIT_SHA1}\"")
@@ -57,7 +60,7 @@ execute_process(COMMAND
         GIT_EXACT_TAG
         OUTPUT_STRIP_TRAILING_WHITESPACE)
 if(NOT res EQUAL 0)
-    message(FATAL_ERROR "could not get the exact git tag")
+    message(STATUS "could not get the exact git tag")
 endif()
 
 message(STATUS "GIT_EXACT_TAG: \"${GIT_EXACT_TAG}\"")
@@ -75,7 +78,7 @@ execute_process(COMMAND
         GIT_DESCRIPTION
         OUTPUT_STRIP_TRAILING_WHITESPACE)
 if(NOT res EQUAL 0)
-    message(FATAL_ERROR "could not get the description")
+    message(STATUS "could not get the description")
 endif()
 
 message(STATUS "GIT_DESCRIPTION: \"${GIT_DESCRIPTION}\"")
