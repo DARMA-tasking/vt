63321b255baf7570f42dd349392967dbd438e73c
Sean McGovern <st.mcgovern@gmail.com>
2022-03-21 22:21:46 +0000

    #1423: remove maximimum ref-count check

--- src/vt/messaging/envelope/envelope_ref.impl.h
+++ src/vt/messaging/envelope/envelope_ref.impl.h
@@ -60,7 +60,7 @@ inline void envelopeRef(Env& env) {
     "instead of a MsgPtr/makeMessage/makeSharedMessage construct."
   );
   vtAssertInfo(
-    envp->ref >= 0 and envp->ref < 100,
+    envp->ref >= 0,
     "Bad ref-count on message ref-increment. "
     "Message ref-count must never be negative and cannnot exceed limit (100).",
     static_cast<RefType>(envp->ref)
