8164b4d99c172f77e0f793c9df17a621c9b387f7
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2022-05-05 22:43:42 +0200

    #1765: CMake: set diagnostic related flags from single file

--- cmake/turn_on_warnings.cmake
+++ cmake/turn_on_warnings.cmake
@@ -1,5 +1,11 @@
 include(CheckCXXCompilerFlag)
 
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  add_compile_options(-fdiagnostics-color=always)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  add_compile_options(-fcolor-diagnostics)
+endif()
+
 macro(add_cxx_compiler_flag_if_supported flag)
   check_cxx_compiler_flag(${flag} flag_supported)
   if(flag_supported)
