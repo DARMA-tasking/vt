6a9bcd6d99079819f75970034646c60e41427aea
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2022-05-05 21:04:44 +0200

    #1765: only enable sanitizers for vt target

--- cmake-modules/SetCXXCompilerFlags.cmake
+++ cmake-modules/SetCXXCompilerFlags.cmake
@@ -19,19 +19,21 @@ elseif (NOT ${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
   message(FATAL_ERROR "Your C++ compiler may not support C++14.")
 endif ()
 
-if (vt_asan_enabled)
-  list(APPEND TARGET_PUBLIC_CXX_FLAGS -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls)
+if ("${vt_target}" STREQUAL "${VIRTUAL_TRANSPORT_LIBRARY}")
+  if (vt_asan_enabled)
+    list(APPEND TARGET_PUBLIC_CXX_FLAGS -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls)
+  endif()
+
+  if (vt_ubsan_enabled)
+    add_definitions(-DVT_UBSAN_ENABLED)
+    list(APPEND TARGET_PUBLIC_CXX_FLAGS -fsanitize=undefined -fno-omit-frame-pointer)
+  endif()
 endif()
 
-if (vt_ubsan_enabled)
-  add_definitions(-DVT_UBSAN_ENABLED)
-  list(APPEND TARGET_PUBLIC_CXX_FLAGS -fsanitize=undefined -fno-omit-frame-pointer)
-endif()
-
-message(DEBUG "Target ${vt_target} public compile options: ${TARGET_CXX_FLAGS}")
+message(DEBUG "Target ${vt_target} public compile options: ${TARGET_PUBLIC_CXX_FLAGS}")
 target_compile_options(${vt_target} PUBLIC ${TARGET_PUBLIC_CXX_FLAGS})
 
-message(DEBUG "Target ${vt_target} private compile options: ${TARGET_CXX_FLAGS}")
+message(DEBUG "Target ${vt_target} private compile options: ${TARGET_PRIVATE_CXX_FLAGS}")
 target_compile_options(${vt_target} PRIVATE ${TARGET_PRIVATE_CXX_FLAGS})
 
 endmacro()
