a21fd290bd7a05fc2545061fc1cf51b03a9846b9
Jacob Domagala <domagala.jacob@gmail.com>
2022-06-17 00:35:36 +0200

    #1849: tests: Fix licenses

--- tests/unit/active/test_async_op_mpi.cc
+++ tests/unit/active/test_async_op_mpi.cc
@@ -2,7 +2,7 @@
 //@HEADER
 // *****************************************************************************
 //
-//                               test_async_op.cc
+//                             test_async_op_mpi.cc
 //                       DARMA/vt => Virtual Transport
 //
 // Copyright 2019-2021 National Technology & Engineering Solutions of Sandia, LLC
