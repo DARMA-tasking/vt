7eaf398c963541656e2a407356e826161c1acc30
Jacob Domagala <domagala.jacob@gmail.com>
2022-09-27 18:18:58 +0200

    #1933: Utils: Update Doxygen for FileSpec

--- src/vt/utils/file_spec/spec.h
+++ src/vt/utils/file_spec/spec.h
@@ -67,9 +67,9 @@ inline std::string GetSpecName(FileSpecType type) {
   }
 }
 /**
- * \struct FileSpec spec.h vt/trace/file_spec/spec.h
+ * \struct FileSpec spec.h vt/utils/file_spec/spec.h
  *
- * \brief Parses trace spec file when available and tests when its enabled. A
+ * \brief Parses spec file when available and tests when its enabled. A
  * single node parses the specification; all others receive the spec from a
  * broadcast.
  *
@@ -80,7 +80,7 @@ inline std::string GetSpecName(FileSpecType type) {
  * 200 -5 5
  * """
  *
- * This specifies that tracing will be enabled on the following phases:
+ * This specifies that tracing/lb will be enabled on the following phases:
  * {
  *   [0,10], # phase 0 with offsets 0,+10 (subsumes [0,3] from %100 -3 3)
  *   [97,103] # any phase % 100 with offset -3,+3
@@ -94,10 +94,10 @@ inline std::string GetSpecName(FileSpecType type) {
  * start with "%100" or two entries that start with "100" would be invalid and
  * trigger a parsing error. But having a "%100" and "100" entry is valid.
  *
- * Whether tracing is enabled is calculated as an OR across all specification
+ * Whether tracing/lb is enabled is calculated as an OR across all specification
  * entries. Thus, if a given phase is contained in any spec line, it is
  * enabled. Note that 0 % 100 = 0. Therefore, if the above example did not
- * contain the first line, tracing would be enabled as:
+ * contain the first line, tracing/lb would be enabled as:
  *
  * {
  *   [0,3], # any phase mod 100 from -3,+3
@@ -190,11 +190,11 @@ public:
   static ProxyType construct(FileSpecType type);
 
   /**
-   * \brief Check entire spec to see if tracing is enabled on any of the entries
+   * \brief Check entire spec to see if it is enabled on any of the entries
    *
    * \param[in] in_phase the phase to check
    *
-   * \return whether tracing is enabled
+   * \return whether it is enabled
    */
   bool checkEnabled(SpecIndex in_phase);
 
