ba37cd8967245cc9f98130686cd3ac566e8539d4
Jonathan Lifflander <jliffla@sandia.gov>
2022-10-31 15:58:33 -0700

    #2005: examples: write polymorphic example

---
+++ examples/collection/polymorphic_collection.cc
@@ -0,0 +1,198 @@
+/*
+//@HEADER
+// *****************************************************************************
+//
+//                            migrate_collection.cc
+//                       DARMA/vt => Virtual Transport
+//
+// Copyright 2019-2021 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// *****************************************************************************
+//@HEADER
+*/
+
+#include <vt/transport.h>
+
+/// [Polymorphic collection example]
+static constexpr int32_t const default_num_elms = 16;
+struct InitialConsTag{};
+struct ColMsg;
+
+struct Hello : vt::Collection<Hello, vt::Index1D> {
+  checkpoint_virtual_serialize_root()
+
+  Hello(InitialConsTag) {}
+
+  explicit Hello(checkpoint::SERIALIZE_CONSTRUCT_TAG) {}
+
+  template <typename Serializer>
+  void serialize(Serializer& s) {
+    vt::Collection<Hello, vt::Index1D>::serialize(s);
+    s | test_val;
+  }
+
+  void doWork(ColMsg* msg);
+
+  double test_val = 0.0;
+};
+
+struct ColMsg : vt::CollectionMessage<Hello> {
+  explicit ColMsg(vt::NodeType const& in_from_node)
+    : from_node(in_from_node)
+  { }
+
+  vt::NodeType from_node = vt::uninitialized_destination;
+};
+
+template <typename T>
+struct HelloTyped : Hello {
+  checkpoint_virtual_serialize_derived_from(Hello)
+
+  HelloTyped(InitialConsTag);
+
+  HelloTyped(checkpoint::SERIALIZE_CONSTRUCT_TAG)
+    : Hello(checkpoint::SERIALIZE_CONSTRUCT_TAG{})
+  {}
+
+  template <typename Serializer>
+  void serialize(Serializer& s) {
+    s | my_vec;
+  }
+
+  std::vector<T> my_vec;
+};
+
+template <>
+HelloTyped<int>::HelloTyped(InitialConsTag)
+  : Hello(InitialConsTag{})
+{
+  for (int i = 0; i < 100; i++) {
+    my_vec.push_back(i+200);
+  }
+}
+
+template <>
+HelloTyped<double>::HelloTyped(InitialConsTag)
+  : Hello(InitialConsTag{})
+{
+  for (int i = 0; i < 100; i++) {
+    my_vec.push_back(i + 0.5);
+  }
+}
+
+void Hello::doWork(ColMsg* msg) {
+  vt_print(
+    gen, "idx={}: val={}, type={}\n",
+    getIndex(), test_val, typeid(*this).name()
+  );
+
+  auto hi = dynamic_cast<HelloTyped<int>*>(this);
+  auto hd = dynamic_cast<HelloTyped<double>*>(this);
+
+  if (hi) {
+    for (int i = 0; i < 100; i++) {
+      vtAssert(hi->my_vec[i] == i+200, "Must be equal");
+    }
+  } else if (hd) {
+    for (int i = 0; i < 100; i++) {
+      vtAssert(hd->my_vec[i] == i+0.5, "Must be equal");
+    }
+  } else {
+    vtAbort("FAIL: wrong type of collection element\n");
+  }
+}
+
+
+static void migrateToNext(ColMsg* msg, Hello* col) {
+  vt::NodeType this_node = vt::theContext()->getNode();
+  vt::NodeType num_nodes = vt::theContext()->getNumNodes();
+  vt::NodeType next_node = (this_node + 1) % num_nodes;
+
+  fmt::print("{}: migrateToNext: idx={}\n", this_node, col->getIndex());
+  col->migrate(next_node);
+}
+
+int main(int argc, char** argv) {
+  vt::initialize(argc, argv);
+
+  vt::NodeType this_node = vt::theContext()->getNode();
+
+  int32_t num_elms = default_num_elms;
+  if (argc > 1) {
+    num_elms = atoi(argv[1]);
+  }
+
+  auto range = vt::Index1D(num_elms);
+
+  std::vector<std::tuple<vt::Index1D, std::unique_ptr<Hello>>> elms;
+  if (this_node % 2 == 0) {
+    for (int i = 0; i < num_elms/2; i++) {
+      vt::Index1D idx{i};
+      elms.emplace_back(idx, std::make_unique<HelloTyped<int>>(InitialConsTag{}));
+    }
+  } else {
+    for (int i = num_elms/2; i < num_elms; i++) {
+      vt::Index1D idx{i};
+      elms.emplace_back(idx, std::make_unique<HelloTyped<double>>(InitialConsTag{}));
+    }
+  }
+
+  auto proxy = vt::makeCollection<Hello>("Polymorphic Collection")
+    .bounds(range)
+    .listInsertHere(std::move(elms))
+    .wait();
+
+  for (int p = 0; p < 10; p++) {
+    vt::runInEpochCollective([&]{
+      proxy.broadcastCollective<ColMsg, &Hello::doWork>(this_node);
+    });
+    vt::runInEpochCollective([&]{
+      proxy.broadcastCollective<ColMsg, migrateToNext>(this_node);
+    });
+    vt::runInEpochCollective([&]{
+      proxy.broadcastCollective<ColMsg, &Hello::doWork>(this_node);
+    });
+  }
+
+  for (int p = 0; p < 10; p++) {
+    vt::runInEpochCollective([&]{
+      proxy.broadcastCollective<ColMsg, &Hello::doWork>(this_node);
+    });
+    vt::thePhase()->nextPhaseCollective();
+  }
+
+  vt::finalize();
+
+  return 0;
+}
+/// [Polymorphic collection example]
