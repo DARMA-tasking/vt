de82cf0f044fb14e391ac4fa0cc0edb23ece8dcd
Jonathan Lifflander <jliffla@sandia.gov>
2022-11-17 15:02:54 -0800

    add script to run

---
+++ examples/run_mem_lb.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+root_dir=$1
+bin=$2
+
+for dir in $root_dir/challenging_*
+do
+    if [ -d "$dir" ]
+    then
+        result=$($bin $dir/*json | grep 'Final:')
+        problem=$(basename $dir)
+        echo "Problem: $problem; $result"
+    fi
+done
