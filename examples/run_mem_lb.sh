76ee3cae4e9699e9e7bf1363e4e3cc6fd1d04478
Jonathan Lifflander <jliffla@sandia.gov>
2022-11-17 16:22:46 -0800

    more fixes, cleanup

--- examples/run_mem_lb.sh
+++ examples/run_mem_lb.sh
@@ -3,12 +3,25 @@
 root_dir=$1
 bin=$2
 
+function runLB() {
+    dir=$1
+    result=$($bin $dir/*json | grep 'Result:')
+    problem=$(basename $dir)
+    echo "Problem: $problem; $result"
+}
+
+for dir in $root_dir/user-defined*
+do
+    if [ -d "$dir" ]
+    then
+        runLB $dir
+    fi
+done
+
 for dir in $root_dir/challenging_*
 do
     if [ -d "$dir" ]
     then
-        result=$($bin $dir/*json | grep 'Result:')
-        problem=$(basename $dir)
-        echo "Problem: $problem; $result"
+        runLB $dir
     fi
 done
