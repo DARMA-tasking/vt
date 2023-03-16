53bbd5de6f81e6044221d90a799ad67bb3ef0ef9
Jonathan Lifflander <jliffla@sandia.gov>
2023-03-16 09:48:40 -0700

    #2102: tests: fix license

--- tests/unit/collection/test_promote.cc
+++ tests/unit/collection/test_promote.cc
@@ -2,7 +2,7 @@
 //@HEADER
 // *****************************************************************************
 //
-//                                test_promote.cc
+//                               test_promote.cc
 //                       DARMA/vt => Virtual Transport
 //
 // Copyright 2019-2021 National Technology & Engineering Solutions of Sandia, LLC
