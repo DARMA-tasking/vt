3c4d0e3b3e2bbd365250fce972a3dec64f3dd6b5
Nicolas Morales <nmmoral@sandia.gov>
2023-05-16 14:02:53 -0700

    #2147: fix test file license

--- tests/unit/trace/test_runnable_context_trace.cc
+++ tests/unit/trace/test_runnable_context_trace.cc
@@ -2,7 +2,7 @@
 //@HEADER
 // *****************************************************************************
 //
-//                          test_trace_spec_reader.cc
+//                        test_runnable_context_trace.cc
 //                       DARMA/vt => Virtual Transport
 //
 // Copyright 2019-2021 National Technology & Engineering Solutions of Sandia, LLC
