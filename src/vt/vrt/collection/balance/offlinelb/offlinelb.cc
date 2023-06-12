55e0a8d80f8bffd49889c43466b57136d0cef5de
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2023-06-12 16:52:57 +0200

    #2074: Disallow runing of OfflineLB for skipped phases.

--- src/vt/vrt/collection/balance/offlinelb/offlinelb.cc
+++ src/vt/vrt/collection/balance/offlinelb/offlinelb.cc
@@ -59,7 +59,7 @@ void OfflineLB::runLB(LoadType) {
   for (auto&& elm : distro) {
     migrateObjectTo(elm, theContext()->getNode());
   }
-  theLBDataReader()->clearDistro(nextPhase);
+  theLBDataReader()->clearDistro(phase_ + 1);
 }
 
 }}}} /* end namespace vt::vrt::collection::lb */
