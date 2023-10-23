02cd75b9c1dc4380fef50f55b5d13eea6e337757
Jonathan Lifflander <jliffla@sandia.gov>
2023-10-23 10:15:28 -0700

    #2205: examples: rewrite jacobi

--- examples/collection/jacobi1d_vt.cc
+++ examples/collection/jacobi1d_vt.cc
@@ -62,6 +62,7 @@
 
 #include <vt/transport.h>
 #include <vt/runnable/invoke.h>
+#include <vt/messaging/collection_chain_set.h>
 
 #include <cstdlib>
 #include <cassert>
@@ -69,23 +70,18 @@
 
 static constexpr std::size_t const default_nrow_object = 8;
 static constexpr std::size_t const default_num_objs = 4;
+static constexpr std::size_t const check_conv_freq = 1;
 static constexpr double const default_tol = 1.0e-02;
 
-struct NodeObj {
-  bool is_finished_ = false;
-  void workFinishedHandler() { is_finished_ = true; }
-  bool isWorkFinished() { return is_finished_; }
-};
+struct NodeObj;
+
 using NodeObjProxy = vt::objgroup::proxy::Proxy<NodeObj>;
 
 struct LinearPb1DJacobi : vt::Collection<LinearPb1DJacobi,vt::Index1D> {
-
 private:
-
   std::vector<double> tcur_, told_;
   std::vector<double> rhs_;
   size_t iter_ = 0;
-  size_t msgReceived_ = 0, totalReceive_ = 0;
   size_t numObjs_ = 1;
   size_t numRowsPerObject_ = 1;
   size_t maxIter_ = 8;
@@ -95,53 +91,11 @@ public:
 
   explicit LinearPb1DJacobi() :
     tcur_(), told_(), rhs_(), iter_(0),
-    msgReceived_(0), totalReceive_(0),
     numObjs_(1), numRowsPerObject_(1), maxIter_(8)
   { }
 
-
-  using BlankMsg = vt::CollectionMessage<LinearPb1DJacobi>;
-
-  struct LPMsg : vt::CollectionMessage<LinearPb1DJacobi> {
-
-    size_t numObjects = 0;
-    size_t nRowPerObject = 0;
-    size_t iterMax = 0;
-    NodeObjProxy objProxy;
-
-    LPMsg() = default;
-
-    LPMsg(const size_t nobjs, const size_t nrow, const size_t itMax, NodeObjProxy proxy) :
-      numObjects(nobjs), nRowPerObject(nrow), iterMax(itMax), objProxy(proxy)
-    { }
-
-  };
-
-  void checkCompleteCB(double normRes) {
-    //
-    // Only one object for the reduction will visit
-    // this function
-    //
-
-    auto const iter_max_reached = iter_ > maxIter_;
-    auto const norm_res_done = normRes < default_tol;
-
-    if (iter_max_reached or norm_res_done) {
-      auto const to_print = iter_max_reached ?
-        "\n Maximum Number of Iterations Reached. \n\n" :
-        fmt::format("\n Max-Norm Residual Reduced by {} \n\n", default_tol);
-
-      fmt::print(to_print);
-
-      // Notify all nodes that computation is finished
-      objProxy_.broadcast<&NodeObj::workFinishedHandler>();
-    } else {
-      fmt::print(" ## ITER {} >> Residual Norm = {} \n", iter_, normRes);
-    }
-  }
-
-  void doIteration() {
-
+  void kernel() {
+    vt_print(gen, "kernel {}\n", this->getIndex());
     iter_ += 1;
 
     //
@@ -160,112 +114,59 @@ public:
     }
 
     std::copy(tcur_.begin(), tcur_.end(), told_.begin());
+  }
 
-    //
-    // Compute the maximum entries among the rows on this object
-    // We do not take into account the "ghost" entries
-    // as they may be "out of date".
-    //
-
+  double computeMaxNorm() {
+    // Compute the maximum entries among the rows on this object We do not take
+    // into account the "ghost" entries as they may be "out of date".
     double maxNorm = 0.0;
-
     for (size_t ii = 1; ii < tcur_.size()-1; ++ii) {
       double val = tcur_[ii];
       maxNorm = (maxNorm > std::fabs(val)) ? maxNorm : std::fabs(val);
     }
-
-    auto proxy = this->getCollectionProxy();
-    proxy.reduce<&LinearPb1DJacobi::checkCompleteCB, vt::collective::MaxOp>(
-      proxy[0], maxNorm
-    );
-
+    return maxNorm;
   }
 
+  void reduceMaxNorm(vt::Callback<double> cb) {
+    vt_print(gen, "reduceMaxNorm {}\n", this->getIndex());
 
-  struct VecMsg : vt::CollectionMessage<LinearPb1DJacobi> {
-    using MessageParentType = vt::CollectionMessage<LinearPb1DJacobi>;
-    vt_msg_serialize_if_needed_by_parent_or_type1(vt::IdxBase);
+    auto proxy = this->getCollectionProxy();
+    proxy.reduce<vt::collective::MaxOp>(cb, computeMaxNorm());
+  }
 
-    VecMsg() = default;
-
-    VecMsg(vt::IdxBase const& in_index, double const& ref) :
-      vt::CollectionMessage<LinearPb1DJacobi>(),
-      from_index(in_index), val(ref)
-    { }
-
-    template <typename Serializer>
-    void serialize(Serializer& s) {
-      MessageParentType::serialize(s);
-      s | from_index;
-      s | val;
+  void sendLeft() {
+    // Send the values to the left
+    auto proxy = this->getCollectionProxy();
+    auto idx = this->getIndex();
+    vt_print(gen, "sendLeft {}\n", idx);
+    if (idx.x() > 0) {
+      proxy[idx.x() - 1].send<&LinearPb1DJacobi::exchange>(idx.x(), told_[1]);
     }
+  }
 
-    vt::IdxBase from_index = 0;
-    double val = 0.0;
-  };
+  void sendRight() {
+    // Send the values to the right
+    auto proxy = this->getCollectionProxy();
+    auto idx = this->getIndex();
+    vt_print(gen, "sendRight {}\n", idx);
+    if (size_t(idx.x()) < numObjs_ - 1) {
+      proxy[idx.x() + 1].send<&LinearPb1DJacobi::exchange>(idx.x(), told_[numRowsPerObject_]);
+    }
+  }
 
-  void exchange(VecMsg *msg) {
+  void exchange(vt::IdxBase from_index, double val) {
     // Receive and treat the message from a neighboring object.
-
-    const vt::IdxBase myIdx = getIndex().x();
-
-    if (myIdx > msg->from_index) {
-      this->told_[0] = msg->val;
-      msgReceived_ += 1;
+    auto idx = this->getIndex();
+    vt_print(gen, "exchange {}, from={}\n", idx, from_index);
+    if (idx.x() > from_index) {
+      told_[0] = val;
     }
-
-    if (myIdx < msg->from_index) {
-      this->told_[numRowsPerObject_ + 1] = msg->val;
-      msgReceived_ += 1;
-    }
-
-    // Check whether this 'object' has received all the expected messages.
-    if (msgReceived_ == totalReceive_) {
-      msgReceived_ = 0;
-      doIteration();
-    }
-
-  }
-
-  void doIter(BlankMsg *msg) {
-
-    //
-    // Treat the particular case of 1 object
-    // where no communication is needed.
-    // Without this treatment, the code would not iterate.
-    //
-
-    if (numObjs_ == 1) {
-      doIteration();
-      return;
-    }
-    //---------------------------------------
-
-    //
-    // Routine to send information to a different object
-    //
-
-    vt::IdxBase const myIdx = getIndex().x();
-
-    //--- Send the values to the left
-    auto proxy = this->getCollectionProxy();
-    if (myIdx > 0) {
-      proxy[myIdx - 1].send<VecMsg, &LinearPb1DJacobi::exchange>(
-        myIdx, told_[1]
-      );
-    }
-
-    //--- Send values to the right
-    if (size_t(myIdx) < numObjs_ - 1) {
-      proxy[myIdx + 1].send<VecMsg, &LinearPb1DJacobi::exchange>(
-        myIdx, told_[numRowsPerObject_]
-      );
+    if (idx.x() < from_index) {
+      told_[numRowsPerObject_ + 1] = val;
     }
   }
 
-
-  void init() {
-
+  void initializeData() {
     tcur_.assign(numRowsPerObject_ + 2, 0.0);
     told_.assign(numRowsPerObject_ + 2, 0.0);
     rhs_.assign(numRowsPerObject_ + 2, 0.0);
@@ -280,39 +181,118 @@ public:
       tcur_[ii] = sin(nf * M_PI * x0 * x0);
     }
 
-    totalReceive_ = 2;
-
     if (myIdx == 0) {
       tcur_[0] = 0.0;
-      totalReceive_ -= 1;
     }
 
     if (myIdx == numObjs_ - 1) {
       tcur_[numRowsPerObject_+1] = 0.0;
-      totalReceive_ -= 1;
     }
 
     std::copy(tcur_.begin(), tcur_.end(), told_.begin());
-
   }
 
 
-  void init(LPMsg* msg) {
-    numObjs_ = msg->numObjects;
-    numRowsPerObject_ = msg->nRowPerObject;
-    maxIter_ = msg->iterMax;
-    objProxy_ = msg->objProxy;
+  void init(size_t numObjects, size_t nRowPerObject, size_t iterMax, NodeObjProxy objProxy) {
+    numObjs_ = numObjects;
+    numRowsPerObject_ = nRowPerObject;
+    maxIter_ = iterMax;
+    objProxy_ = objProxy;
 
     // Initialize the starting vector
-    init();
+    initializeData();
   }
 
 };
 
-bool isWorkDone( vt::objgroup::proxy::Proxy<NodeObj> const& proxy){
-  auto const this_node = vt::theContext()->getNode();
-  return proxy[this_node].invoke<&NodeObj::isWorkFinished>();
-}
+struct NodeObj {
+  using ChainSetType = vt::messaging::CollectionChainSet<vt::Index1D>;
+
+  NodeObj(
+    vt::CollectionProxy<LinearPb1DJacobi> in_proxy, std::size_t in_num_objs,
+    std::size_t in_max_iter
+  ) : proxy_(in_proxy),
+      num_objs_(in_num_objs),
+      max_iter_(in_max_iter)
+  { }
+
+  void reducedNorm(double normRes) {
+    converged_ = normRes < default_tol;
+    fmt::print(
+      "## ITER {} >> Residual Norm = {}, conv={} \n",
+      cur_iter_, normRes, converged_
+    );
+  }
+
+  void setup(vt::objgroup::proxy::Proxy<NodeObj> in_proxy) {
+    chains_ = std::make_unique<ChainSetType>(proxy_);
+    chains_->startTasks();
+    this_proxy_ = in_proxy;
+  }
+
+  void runToConvergence() {
+    while (not converged_ and cur_iter_ < max_iter_) {
+      vt_print(gen, "runToConvergence: cur_iter_={}, max_iter_={}\n", cur_iter_, max_iter_);
+
+      auto xl = chains_->taskCollective("exchange left", [&](auto idx, auto t) {
+        if (prev_kernel) {
+          t->dependsOn(idx, *prev_kernel);
+        }
+        return proxy_[idx].template send<&LinearPb1DJacobi::sendLeft>();
+      });
+
+      auto xr = chains_->taskCollective("exchange right", [&](auto idx, auto t) {
+        if (prev_kernel) {
+          t->dependsOn(idx, *prev_kernel);
+        }
+        return proxy_[idx].template send<&LinearPb1DJacobi::sendRight>();
+      });
+
+      auto kernel = chains_->taskCollective("kernel", [&](auto idx, auto t) {
+        if (idx.x() != 0) {
+          auto left = vt::Index1D(idx.x() - 1);
+          t->dependsOn(left, *xr);
+        }
+        if (static_cast<std::size_t>(idx.x()) != num_objs_ - 1) {
+          auto right = vt::Index1D(idx.x() + 1);
+          t->dependsOn(right, *xl);
+        }
+        return proxy_[idx].template send<&LinearPb1DJacobi::kernel>();
+      });
+      prev_kernel = kernel;
+
+      if (cur_iter_++ % check_conv_freq == 0) {
+        chains_->taskCollective("checkConv", [&](auto idx, auto t) {
+          t->dependsOn(idx, *kernel);
+          auto cb = vt::theCB()->makeBcast<&NodeObj::reducedNorm>(this_proxy_);
+          return proxy_[idx].template send<&LinearPb1DJacobi::reduceMaxNorm>(cb);
+        });
+
+        chains_->waitForTasks();
+        chains_->startTasks();
+      }
+    }
+
+    if (not converged_) {
+      chains_->waitForTasks();
+      fmt::print("Maximum Number of Iterations Reached without convergence.\n");
+    } else {
+      fmt::print("Convergence is reached at iteration {}.\n", cur_iter_);
+    }
+
+    chains_->phaseDone();
+  }
+
+private:
+  vt::CollectionProxy<LinearPb1DJacobi> proxy_;
+  std::size_t num_objs_ = 0;
+  std::size_t max_iter_ = 0;
+  std::size_t cur_iter_ = 0;
+  std::unique_ptr<ChainSetType> chains_;
+  vt::task::TaskCollective<vt::Index1D>* prev_kernel = nullptr;
+  bool converged_ = false;
+  vt::objgroup::proxy::Proxy<NodeObj> this_proxy_;
+};
 
 int main(int argc, char** argv) {
 
@@ -354,34 +334,31 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  // Object group of all nodes that take part in computation
-  // Used to determine whether the computation is finished
-  auto grp_proxy = vt::theObjGroup()->makeCollective<NodeObj>("examples_jacobi1d");
-
   // Create the decomposition into objects
   using BaseIndexType = typename vt::Index1D::DenseIndexType;
   auto range = vt::Index1D(static_cast<BaseIndexType>(num_objs));
 
-  auto col_proxy = vt::makeCollection<LinearPb1DJacobi>("examples_jacobi1d")
+  auto col_proxy = vt::makeCollection<LinearPb1DJacobi>("jacobi1d")
     .bounds(range)
     .bulkInsert()
     .wait();
 
-  vt::runInEpochCollective([col_proxy, grp_proxy, num_objs, numRowsPerObject, maxIter]{
-    col_proxy.broadcastCollective<LinearPb1DJacobi::LPMsg, &LinearPb1DJacobi::init>(
+  // Object group of all nodes that take part in computation
+  // Used to determine whether the computation is finished
+  auto grp_proxy = vt::theObjGroup()->makeCollective<NodeObj>(
+    "jacobi1d", col_proxy, num_objs, maxIter
+  );
+
+  vt::runInEpochCollective([=]{
+    col_proxy.broadcastCollective<&LinearPb1DJacobi::init>(
       num_objs, numRowsPerObject, maxIter, grp_proxy
     );
   });
 
-  while(!isWorkDone(grp_proxy)){
-    vt::runInEpochCollective([col_proxy]{
-      col_proxy.broadcastCollective<
-        LinearPb1DJacobi::BlankMsg, &LinearPb1DJacobi::doIter
-      >();
-    });
+  grp_proxy.get()->setup(grp_proxy);
+  grp_proxy.get()->runToConvergence();
 
-    vt::thePhase()->nextPhaseCollective();
-  }
+  //vt::thePhase()->nextPhaseCollective();
 
   vt::finalize();
 
