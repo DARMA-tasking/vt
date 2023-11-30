acd2f359025bec19f987746c17dfecc8131a9100
Nicole Lemaster Slattengren <nlslatt@sandia.gov>
2023-11-30 11:30:22 -0800

    #2223: phase: improve phase summary on phase zero

--- src/vt/phase/phase_manager.cc
+++ src/vt/phase/phase_manager.cc
@@ -395,14 +395,15 @@ void PhaseManager::printSummary(vrt::collection::lb::PhaseInfo* last_phase_info)
           }
         }
       }
-    } else if (last_phase_info->phase == 0) {
-       // ran the lb on a phase that may have included initialization costs
-       vt_print(
-         phase,
-         "Consider skipping LB on phase 0 if it is not representative of "
-         "future phases\n"
-       );
     } else {
+      if (last_phase_info->phase == 0) {
+        // ran the lb on a phase that may have included initialization costs
+        vt_print(
+          phase,
+          "Consider skipping LB on phase 0 if it is not representative of "
+          "future phases\n"
+        );
+      }
       if (last_phase_info->migration_count > 0) {
         auto speedup = compute_speedup(
           last_phase_info->max_load, last_phase_info->max_load_post_lb
