136de2e069c65f88ab5c05eb652e03dd955b14ef
Jacob Domagala <domagala.jacob@gmail.com>
2023-12-28 19:04:05 +0100

    #1891: Update find_package_local to work with package_ROOT

--- cmake/load_local_packages.cmake
+++ cmake/load_local_packages.cmake
@@ -8,10 +8,8 @@ include(cmake/local_package.cmake)
 if (EXISTS "${PROJECT_LIB_DIR}/checkpoint")
   add_subdirectory(${PROJECT_LIB_DIR}/checkpoint)
 else()
-  # require directories for these packages
-  require_pkg_directory(checkpoint "VT checkpoint library")
   # find these required packages locally
-  find_package_local(checkpoint "${checkpoint_DIR}" checkpoint)
-endif()
+  find_package_local(checkpoint checkpoint)
+  endif()
 
 set(CHECKPOINT_LIBRARY vt::lib::checkpoint)
