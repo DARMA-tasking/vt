1e590befcf8e0b2637c0f64999bde1cec7678b56
Nicole Lemaster Slattengren <nlslatt@sandia.gov>
2024-04-09 15:19:35 -0600

    #2201: tools: NOT to merge: add paper reproducer input and script

---
+++ tools/1959-tasks/simulate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z $2 ]; then
+  echo "Error: missing arguments"
+  echo "Syntax: $0 <vt_src_dir> <vt_build_dir>"
+  exit 1
+fi
+
+vt_src_dir=$1
+vt_build_dir=$2
+
+mpiexec --n 14 ${vt_build_dir}/tools/workload_replay/simulate_replay 0 1 --vt_lb --vt_lb_file_name="${vt_src_dir}/tools/1959-tasks/ccm-lb-delta-1e-11.config" --vt_lb_data_in --vt_lb_data_dir_in="${vt_src_dir}/tools/1959-tasks" --vt_debug_level=terse --vt_debug_phase
