d45867632e7102c409e3f7b068434d153c793d3b
Jacob Domagala <domagala.jacob@gmail.com>
2024-04-26 12:12:17 +0200

    #2275: Don't print memory usage in perf tests when not needed

--- tests/perf/common/test_harness.h
+++ tests/perf/common/test_harness.h
@@ -174,6 +174,7 @@ private:
 protected:
   bool gen_file_ = false;
   bool verbose_ = false;
+  bool print_memory_use_ = false;
   uint32_t num_runs_ = 20;
   uint32_t current_run_ = 0;
   std::vector<char*> custom_args_ = {};
