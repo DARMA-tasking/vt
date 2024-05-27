c85bc8b2b505fb30abc3504ea87a367e81602969
Jacob Domagala <domagala.jacob@gmail.com>
2024-05-27 23:09:43 +0200

    #2288: Update script with license generation

--- scripts/generate_header_guards_and_license.py
+++ scripts/generate_header_guards_and_license.py
@@ -40,6 +40,47 @@ def find_existing_guards(lines):
         return ifndef_start, endif_line
     return None, None
 
+
+def generate_license(file_path, license_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    with open(license_path, 'r') as license_file:
+        license_file = license_file.readlines()
+
+    file_name = os.path.basename(file_path)
+    lenright = int((80 - 2 - len(file_name))/2)
+    license_file[3] = f"//{' ' :<{lenright}}{file_name}\n"
+
+    # Remove leading empty lines
+    non_empty_lines_start = 0
+    for i, line in enumerate(lines):
+        if line.strip() != '':
+            non_empty_lines_start = i
+            break
+
+    trimmed_lines = lines[non_empty_lines_start:]
+
+    existing_license_start = -1
+    existing_license_end = -1
+    for i, line in enumerate(trimmed_lines):
+        if line.startswith("//@HEADER"):
+            if existing_license_start == -1:
+                existing_license_start = i
+            elif existing_license_end == -1:
+                existing_license_end = i
+                break
+
+    if existing_license_start != -1:
+        updated_file = trimmed_lines[:existing_license_start] + license_file + trimmed_lines[existing_license_end + 1:]
+    else:
+        updated_file = ['/*\n'] + license_file + ['*/\n'] + trimmed_lines
+
+    with open(file_path, 'w') as file:
+        file.writelines(updated_file)
+
+
+
 def generate_header_guard(file_path, root):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -77,13 +118,16 @@ def generate_header_guard(file_path, root):
 def main():
     parser = ap.ArgumentParser()
     parser.add_argument("--src_dir", "-s", dest='src_dir', required=True)
+    parser.add_argument("--license", "-l", dest='license_path', required=True)
     args = parser.parse_args()
 
-    base_dir = os.path.expanduser(args.src_dir)
-    for root, _, files in os.walk(base_dir):
+    src_dir_abs = os.path.abspath(os.path.expanduser(args.src_dir))
+    license_path_abs = os.path.abspath(os.path.expanduser(args.license_path))
+    for root, _, files in os.walk(src_dir_abs):
         for file in files:
             if file.endswith('.h'):
-                generate_header_guard(os.path.join(root, file), base_dir)
+                generate_license(os.path.join(root, file), license_path_abs)
+                generate_header_guard(os.path.join(root, file), src_dir_abs)
 
 if __name__ == '__main__':
     main()
