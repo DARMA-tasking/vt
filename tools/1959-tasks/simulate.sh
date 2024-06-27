f91fd87b1793d63452e2fb7a7341a39b1e912f35
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2024-06-27 16:25:40 +0200

    #2201: Revert "tools: NOT to merge: add alternative in paper reproducer script"
    This reverts commit 0a967017c07f31110a81be17413ffe4eef2e4ba0.

--- tools/1959-tasks/simulate.sh
+++ tools/1959-tasks/simulate.sh
@@ -9,7 +9,4 @@ fi
 vt_src_dir=$1
 vt_build_dir=$2
 
-mpiexec --n 14 "${vt_build_dir}"/tools/workload_replay/simulate_replay 0 1 --vt_lb --vt_lb_file_name="${vt_src_dir}/tools/1959-tasks/ccm-lb-delta-1e-11.config" --vt_lb_data_in --vt_lb_data_dir_in="${vt_src_dir}/tools/1959-tasks" --vt_debug_level=terse --vt_debug_phase
-
-# Or, if you don't want to use an LB config file:
-#mpiexec --n 14 ${vt_build_dir}/tools/workload_replay/simulate_replay 0 1 --vt_lb --vt_lb_run_lb_first_phase --vt_lb_name="TemperedLB" --vt_lb_args="transfer=SwapClusters knowledge=Complete rollback=false iters=12 memory_threshold=66000000000.0 delta=0.00000000001" --vt_lb_data_in --vt_lb_data_dir_in="${vt_src_dir}/tools/1959-tasks" --vt_debug_level=terse --vt_debug_phase
+mpiexec --n 14 ${vt_build_dir}/tools/workload_replay/simulate_replay 0 1 --vt_lb --vt_lb_file_name="${vt_src_dir}/tools/1959-tasks/ccm-lb-delta-1e-11.config" --vt_lb_data_in --vt_lb_data_dir_in="${vt_src_dir}/tools/1959-tasks" --vt_debug_level=terse --vt_debug_phase
