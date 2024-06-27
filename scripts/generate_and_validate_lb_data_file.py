1f3d8fbf1165d04b11a9c5e6f7bb2ec62fe67055
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2024-06-27 16:47:07 +0200

    #2291: Update python script to use output of the lb_data_file_generator example

--- scripts/generate_and_validate_lb_data_file.py
+++ scripts/generate_and_validate_lb_data_file.py
@@ -1,29 +1,15 @@
-import subprocess
 import argparse
 import json
 import sys
 from deepdiff import DeepDiff
 
-def generate(vt_build, out_file_name):
-    """
-    Runs vt lb_iter example to generate LBDatafile
-    """
-    exe_path = vt_build + "/examples/collection/lb_iter"
-    out_dir = "--vt_lb_data_dir=" + vt_build
-    out_file = "--vt_lb_data_file=" + out_file_name
-
-    args = (exe_path, "1", "1.0", "1", "--vt_lb", "--vt_lb_interval=1", "--vt_lb_name=RotateLB", "--vt_lb_data", "--vt_lb_data_compress=false", out_dir, out_file)
-    return_code = subprocess.call(args)
-    if return_code != 0:
-        sys.exit(return_code)
-
-def compare(vt_build, file_to_validate, reference_file):
+def compare(file_to_validate, reference_file):
     """
     Compares file to validate wih reference
     """
     print("Comparing '" + file_to_validate + "' with reference file '" + reference_file + "'.")
 
-    with open(vt_build + "/" + file_to_validate) as val_file, open(reference_file) as ref_file:
+    with open(file_to_validate) as val_file, open(reference_file) as ref_file:
         to_validate = json.load(val_file)
         reference = json.load(ref_file)
         diff = DeepDiff(to_validate, reference, report_repetition=True, math_epsilon=0.1)
@@ -40,19 +26,11 @@ def compare(vt_build, file_to_validate, reference_file):
 
 def main():
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--generate", "-g", dest='generate', required=False, action='store_true')
-    group.add_argument("--compare", "-c", dest='compare', required=False, action='store_true')
-
-    parser.add_argument("--vt-build-dir", "-b", dest='vt_build_dir', required=True)
-    parser.add_argument("--file-name", "-f", dest='file_name', required=True)
+    parser.add_argument("--file-to-check", "-f", dest='file', required=True)
     parser.add_argument("--reference-file", "-r", dest='reference_file', required=False)
     args = parser.parse_args()
 
-    if args.generate:
-        generate(args.vt_build_dir, args.file_name)
-    if args.compare:
-        compare(args.vt_build_dir, args.file_name, args.reference_file)
+    compare(args.file, args.reference_file)
 
 
 if __name__ == '__main__':
