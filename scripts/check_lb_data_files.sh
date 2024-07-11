f16a03724026f64da42a48d557b195b5dfc28f8c
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2024-07-11 16:11:31 +0200

    #2291: Update scripts for the output to be less noisy

--- scripts/check_lb_data_files.sh
+++ scripts/check_lb_data_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+set -eo pipefail
 
 path_to_vt_build_dir=${1}
 path_to_vt_src_dir=${2}
@@ -8,7 +8,6 @@ cd "$path_to_vt_build_dir" || exit 1
 
 function run_schema_validator() {
     file=$1
-    echo ""
     echo "Running schema validator on: $file"
     if python3 "${path_to_vt_src_dir}/scripts/JSON_data_files_validator.py" --file_path="$file"
     then
@@ -19,23 +18,14 @@ function run_schema_validator() {
     fi
 }
 
-find . -iname "*.json" | grep -v "compile_commands" | while read f
-do
-    run_schema_validator "$f"
-done
-
-find "${path_to_vt_src_dir}/examples" -iname "*.json" | while read f
-do
-    run_schema_validator "$f"
-done
-
-find . -iname "*.json.br" | while read f
+find . "${path_to_vt_src_dir}/examples" -iname "*.json" -o -iname "*.json.br" \
+    | grep -v "compile_commands" | while read f
 do
     run_schema_validator "$f"
 done
 
 # Compare output of the lb_data_file_generator example with reference file
-if ! python3 "${path_to_vt_src_dir}/scripts/generate_and_validate_lb_data_file.py" \
+if ! python3 "${path_to_vt_src_dir}/scripts/compare_lb_data_file.py" \
     -f "${path_to_vt_build_dir}/examples/lb_data/lb_data_file_generator_1_LBDatafile.0.json" \
     -r "${path_to_vt_src_dir}/examples/lb_data/lb_data_file_example.json"
 then
