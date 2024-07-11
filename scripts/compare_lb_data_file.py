df3886cf74b6e962f689d29e345a50f6cb921e3e
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2024-07-11 16:11:31 +0200

    #2291: Update scripts for the output to be less noisy

--- scripts/compare_lb_data_file.py
+++ scripts/compare_lb_data_file.py
@@ -7,8 +7,6 @@ def compare(file_to_validate, reference_file):
     """
     Compares file to validate wih reference
     """
-    print("Comparing '" + file_to_validate + "' with reference file '" + reference_file + "'.")
-
     with open(file_to_validate) as val_file, open(reference_file) as ref_file:
         to_validate = json.load(val_file)
         reference = json.load(ref_file)
@@ -16,18 +14,19 @@ def compare(file_to_validate, reference_file):
         is_valid = not len(diff.affected_paths)
 
         if not is_valid:
+            sys.stderr.write("Comparing '" + file_to_validate + "' with reference file '" + reference_file + "'... Failed!\n")
             sys.stderr.write("Detected differences:\n")
             json.dump(str(diff), sys.stderr, indent=4)
             sys.stderr.write("\n")
             sys.stderr.flush()
             sys.exit(1)
         else:
-            print("Comparison OK.")
+            print("Comparing '" + file_to_validate + "' with reference file '" + reference_file + "'... Status OK.")
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file-to-check", "-f", dest='file', required=True)
-    parser.add_argument("--reference-file", "-r", dest='reference_file', required=False)
+    parser.add_argument("--reference-file", "-r", dest='reference_file', required=True)
     args = parser.parse_args()
 
     compare(args.file, args.reference_file)
