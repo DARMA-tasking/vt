7cc2c3571a239f74786e8e09465ce1cf510c0df0
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2024-08-12 15:25:37 +0200

    #2335: Remove separate files for spack CI

--- scripts/check_workflow_generation.sh
+++ scripts/check_workflow_generation.sh
@@ -4,11 +4,7 @@ path_to_vt=${1}
 
 cd "$path_to_vt" || exit 1
 
-# Check workflow for Azure CI
 ./scripts/generate-workflow.pl ./scripts/workflow-azure-template.yml ./scripts/workflows-azure.ini
-
-# Check workflow for Spack package job
-./scripts/generate-workflow.pl ./scripts/spack-workflow-template.yml ./scripts/spack-workflow-azure.ini
 
 result=$(git diff --name-only)
 
