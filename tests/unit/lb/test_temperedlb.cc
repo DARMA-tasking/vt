dbd41e6c5aee652fac9a57ed10483f74fd3dcee9
Caleb Schilly <cwschilly@gmail.com>
2024-08-13 13:52:06 -0700

    #2201: tests: add tests for temperedLB with load, load+memory, and load+memory+homing cases

---
+++ tests/unit/lb/test_temperedlb.cc
@@ -0,0 +1,87 @@
+#include <vt/transport.h>
+#include <vt/vrt/collection/balance/read_lb.h>
+#include <vt/vrt/collection/balance/workload_replay.h>
+
+#include "test_helpers.h"
+#include "test_parallel_harness.h"
+
+namespace vt { namespace tests { namespace unit { namespace lb {
+
+#if vt_check_enabled(lblite)
+
+using TestTemperedLB = TestParallelHarness;
+
+std::string writeTemperedLBConfig(std::string transfer_strategy,
+                                  bool mem_constraints,
+                                  double delta = 0.0,
+                                  double beta = 0.0,
+                                  double gamma = 0.0) {
+    int this_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &this_rank);
+    auto config_file = getUniqueFilename();
+    if (this_rank == 0) {
+        std::ofstream cfg_file_{config_file.c_str(), std::ofstream::out | std::ofstream::trunc};
+        cfg_file_ << "0 TemperedLB transfer=" << transfer_strategy << 
+                     " alpha=1.0" << 
+                     " beta=" << beta <<
+                     " gamma=" << gamma << 
+                     " delta=" << delta;
+        if (mem_constraints) {
+            cfg_file_ << " memory_threshold=20.0";
+        }
+        cfg_file_.close();
+    }
+    return config_file;
+}
+
+void runTemperedLBTest(std::string config_file, double expected_imb = 0.0) {
+    // Set configuration
+    theConfig()->vt_lb = true;
+    theConfig()->vt_lb_name = "TemperedLB";
+    theConfig()->vt_lb_data_in = true;
+    theConfig()->vt_lb_file_name = config_file;
+    theConfig()->vt_lb_data_file_in="synthetic-dataset-blocks.%p.json";
+    theConfig()->vt_lb_data_dir_in="synthetic-blocks-data";
+
+    // Replay load balancing
+    int initial_phase = 0;
+    int phases_to_run = 1;
+    int phase_mod = 0;
+    vt::vrt::collection::balance::replay::replayWorkloads(
+        initial_phase, phases_to_run, phase_mod);
+
+    // Get information for the last phase (this problem only has one)
+    auto phase_info = theLBManager()->getPhaseInfo();
+
+    // Assert that temperedLB found the correct imbalance
+    auto imb = (phase_info->max_load / phase_info->avg_load) - 1;
+    EXPECT_EQ(imb, expected_imb);
+
+    // Clear the LB config ahead of next test
+    vrt::collection::balance::ReadLBConfig::clear();
+}
+
+TEST_F(TestTemperedLB, test_load_only) {
+    auto cfg = writeTemperedLBConfig("Original", false);
+    runTemperedLBTest(cfg);
+}
+
+TEST_F(TestTemperedLB, test_load_and_memory_swapclusters) {
+    auto cfg = writeTemperedLBConfig("SwapClusters", true);
+    runTemperedLBTest(cfg);
+}
+
+TEST_F(TestTemperedLB, test_load_memory_homing_swapclusters) {
+    auto cfg = writeTemperedLBConfig("SwapClusters", true, 1.0);
+    runTemperedLBTest(cfg);
+}
+
+TEST_F(TestTemperedLB, test_load_memory_homing_comms) {
+    auto cfg = writeTemperedLBConfig("SwapClusters", true, 1.0, 1.0);
+    double expected_imbalance = 0.25; // placeholder for value from MILP
+    runTemperedLBTest(cfg, expected_imbalance);
+}
+
+#endif
+
+}}}} /* end namespace vt::tests::unit::lb */
