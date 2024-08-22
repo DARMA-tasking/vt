9c86660554b04cc05a44bbbcfeb2978a9b2df459
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2024-08-22 15:52:48 +0200

    #1934: Update unit tests to work with new implementation of the buffer

--- tests/unit/utils/test_circular_phases_buffer.nompi.cc
+++ tests/unit/utils/test_circular_phases_buffer.nompi.cc
@@ -54,25 +54,23 @@ struct Dummy {
   int x;
 };
 
-void validatePresentPhases(
-  util::container::CircularPhasesBuffer<Dummy>& buffer,
-  std::vector<PhaseType> expected) {
+using CircularBufferType = util::container::CircularPhasesBuffer<Dummy>;
+
+void validatePresentPhases(CircularBufferType& buffer, std::vector<PhaseType> expected) {
   for (auto&& phase : expected) {
     EXPECT_TRUE(buffer.contains(phase)) << "Phase: " << phase;
-    EXPECT_EQ(phase * phase, buffer.at(phase).x) << "Phase: " << phase;
+    EXPECT_EQ(phase, buffer.at(phase).x) << "Phase: " << phase;
   }
 }
 
-void validateMissingPhases(
-  util::container::CircularPhasesBuffer<Dummy>& buffer,
-  std::vector<PhaseType> expected) {
+void validateMissingPhases(CircularBufferType& buffer, std::vector<PhaseType> expected) {
   for (auto&& phase : expected) {
     EXPECT_FALSE(buffer.contains(phase)) << "Phase: " << phase;
   }
 }
 
 TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_empty) {
-  util::container::CircularPhasesBuffer<Dummy> buffer;
+  CircularBufferType buffer;
 
   EXPECT_FALSE(buffer.contains(0));
   EXPECT_EQ(std::size_t{0}, buffer.size());
@@ -112,39 +110,65 @@ TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_empty) {
   EXPECT_EQ(std::size_t{4}, buffer.size());
   EXPECT_FALSE(buffer.empty());
 
-  buffer.reset();
+  buffer.clear();
 
   EXPECT_FALSE(buffer.contains(0));
   EXPECT_EQ(std::size_t{0}, buffer.size());
   EXPECT_TRUE(buffer.empty());
 }
 
-TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_store) {
-  util::container::CircularPhasesBuffer<Dummy> buffer{10};
+TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_single_element) {
+  CircularBufferType buffer{1};
 
-  buffer.store(2, {2 * 2});
+  EXPECT_EQ(false, buffer.contains(0));
+
+  for (int i = 0; i < 5; i++) {
+    buffer.store(i, {i});
+    EXPECT_EQ(i == 0, buffer.contains(0));
+    EXPECT_EQ(i == 1, buffer.contains(1));
+    EXPECT_EQ(i == 2, buffer.contains(2));
+    EXPECT_EQ(i == 3, buffer.contains(3));
+    EXPECT_EQ(i == 4, buffer.contains(4));
+    EXPECT_EQ(i == 5, buffer.contains(5));
+    EXPECT_EQ(i == 6, buffer.contains(5));
+  }
+}
+
+TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_multi_elements) {
+  CircularBufferType buffer{5};
+
+  for (int i = 0; i < 15; i++) {
+    buffer.store(i, {i});
+  }
+  std::vector<PhaseType> finalOutput = {10, 11, 12, 13, 14};
+  validatePresentPhases(buffer, finalOutput);
+}
+
+TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_store) {
+  CircularBufferType buffer;
+
+  buffer.store(2, {2});
+  validatePresentPhases(buffer, {2});
+  validateMissingPhases(buffer, {0, 1});
+
+  buffer.resize(10);
+
   validatePresentPhases(buffer, {2});
   validateMissingPhases(buffer, {0, 1});
 
   // store series of elements
   for (int i = 3; i < 15; i++) {
-    buffer.store(i, {i * i});
+    buffer.store(i, {i});
   }
   std::vector<PhaseType> finalOutput = {5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
   validatePresentPhases(buffer, finalOutput);
 }
 
-TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_resize_continuous) {
-  util::container::CircularPhasesBuffer<Dummy> buffer{1};
-
-  buffer[0] = {0};
-  validatePresentPhases(buffer, {0});
-
-  buffer.resize(10);
-  validatePresentPhases(buffer, {0});
+TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_resize) {
+  CircularBufferType buffer{10};
 
   for (int i = 0; i <= 15; i++) {
-    buffer[i] = {i * i};
+    buffer[i] = {i};
   }
   validatePresentPhases(buffer, {6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
 
@@ -153,7 +177,7 @@ TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_resize_continuous) 
   validateMissingPhases(buffer, {6, 7, 8, 9, 10});
 
   for (int i = 15; i <= 32; i++) {
-    buffer[i] = {i * i};
+    buffer[i] = {i};
   }
   validatePresentPhases(buffer, {28, 29, 30, 31, 32});
   validateMissingPhases(buffer, {11, 12, 13, 14, 15});
@@ -161,51 +185,43 @@ TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_resize_continuous) 
   buffer.resize(9);
 
   for (int i = 33; i <= 35; i++) {
-    buffer[i] = {i * i};
+    buffer[i] = {i};
   }
   validatePresentPhases(buffer, {28, 29, 30, 31, 32, 33, 34, 35});
 
   buffer.resize(1);
   validatePresentPhases(buffer, {35});
   validateMissingPhases(buffer, {28, 29, 30, 31, 32, 33, 34});
+
+  buffer.resize(0);
+  validateMissingPhases(buffer, {35});
 }
 
 TEST_F(TestCircularPhasesBuffer, test_circular_phases_buffer_forward_iter) {
-  util::container::CircularPhasesBuffer<Dummy> buffer;
+  CircularBufferType buffer{0};
 
-  for (auto&& ele : buffer) {
-    // This will never be called.
-    EXPECT_EQ(true, false) << "Unexpected phase: " << ele.first;
-  }
+  for (auto& ele : buffer) { EXPECT_EQ(123, ele.x); }
+  for (auto&& ele : buffer) { EXPECT_EQ(123, ele.x); }
 
+  buffer.resize(3);
   buffer[0] = {0};
   buffer[1] = {1};
-  buffer[2] = {4};
+  buffer[2] = {2};
 
-  validatePresentPhases(buffer, {0, 1, 2});
-
-  buffer[3] = {9};
-
-  validatePresentPhases(buffer, {0, 1, 2, 3});
-
-  buffer[4] = {16};
-  buffer[5] = {25};
-  buffer[6] = {36};
-
-  validatePresentPhases(buffer, {0, 1, 2, 3, 4, 5, 6});
-
-  buffer.clear();
-
-  for (auto&& ele : buffer) {
-    // This will never be called.
-    EXPECT_EQ(true, false) << "Unexpected phase: " << ele.first;
+  {
+    int iter = 0;
+    for (auto&& ele : buffer) {
+      EXPECT_EQ(iter, ele.x);
+      iter++;
+    }
   }
 
-  buffer.resize(0);
-
-  for (auto&& ele : buffer) {
-    // This will never be called.
-    EXPECT_EQ(true, false) << "Unexpected phase: " << ele.first;
+  {
+    int iter = 0;
+    for (auto& ele : buffer) {
+      EXPECT_EQ(iter, ele.x);
+      iter++;
+    }
   }
 }
 
