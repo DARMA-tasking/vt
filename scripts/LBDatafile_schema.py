69df764a402bfb1634d35e337ef032cf9f9bbc90
Caleb Schilly <cwschilly@gmail.com>
2024-09-05 15:48:40 -0400

    #2342: update schema to support seq_id in tasks field when id is not bit-encoded; add unit test

--- scripts/LBDatafile_schema.py
+++ scripts/LBDatafile_schema.py
@@ -1,5 +1,11 @@
 from schema import And, Optional, Schema
 
+def validate_id_and_seq_id(field):
+    """Ensure that either seq_id or id is provided."""
+    if 'seq_id' not in field and 'id' not in field:
+        raise ValueError('Either id (bit-encoded) or seq_id must be provided.')
+    return field
+
 LBDatafile_schema = Schema(
     {
         Optional('type'): And(str, "LBDatafile", error="'LBDatafile' must be chosen."),
@@ -30,15 +36,16 @@ LBDatafile_schema = Schema(
                 'id': int,
                 'tasks': [
                     {
-                        'entity': {
+                        'entity': And({
                             Optional('collection_id'): int,
                             'home': int,
-                            'id': int,
+                            Optional('id'): int,
+                            Optional('seq_id'): int,
                             Optional('index'): [int],
                             'type': str,
                             'migratable': bool,
                             Optional('objgroup_id'): int
-                        },
+                        }, validate_id_and_seq_id),
                         'node': int,
                         'resource': str,
                         Optional('subphases'): [
@@ -55,25 +62,27 @@ LBDatafile_schema = Schema(
                 Optional('communications'): [
                     {
                         'type': str,
-                        'to': {
+                        'to': And({
                             'type': str,
-                            'id': int,
+                            Optional('id'): int,
+                            Optional('seq_id'): int,
                             Optional('home'): int,
                             Optional('collection_id'): int,
                             Optional('migratable'): bool,
                             Optional('index'): [int],
                             Optional('objgroup_id'): int,
-                        },
+                        }, validate_id_and_seq_id),
                         'messages': int,
-                        'from': {
+                        'from': And({
                             'type': str,
-                            'id': int,
+                            Optional('id'): int,
+                            Optional('seq_id'): int,
                             Optional('home'): int,
                             Optional('collection_id'): int,
                             Optional('migratable'): bool,
                             Optional('index'): [int],
                             Optional('objgroup_id'): int,
-                        },
+                        }, validate_id_and_seq_id),
                         'bytes': float
                     }
                 ],
