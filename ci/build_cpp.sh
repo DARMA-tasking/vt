585e85a25b89ce5a57214fa4975076b78e8251cf
Pierre Pebay <pebay.pierre@gmail.com>
2024-12-12 16:28:22 +0100

    #2302: Fix build script quotes

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -133,7 +133,7 @@ then
         export PAPI_BUILD=${build_dir}/papi
         mkdir -p "$PAPI_BUILD"
         CC="${CC:-cc}" F77="${F77:-gfortran}" ./configure --prefix="$PAPI_BUILD/install"
-        make ${dashj} && make install
+        make "${dashj}" && make install
     fi
 fi
 
