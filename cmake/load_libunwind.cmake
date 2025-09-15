b622f5c8e41b7d6138b62fc5c3ad4a1831acfd0e
Jonathan Lifflander <jliffla@sandia.gov>
2025-09-15 10:44:34 -0700

    #2474: cmake: fix cmake warning with version > 3.12

--- cmake/load_libunwind.cmake
+++ cmake/load_libunwind.cmake
@@ -1,10 +1,16 @@
 set(vt_feature_cmake_libunwind "0")
 
+cmake_policy(PUSH)
+
+cmake_policy(SET CMP0144 NEW)
+
 if(NOT DEFINED LIBUNWIND_ROOT)
     set(LIBUNWIND_ROOT "/usr")
 endif()
 
 find_package(libunwind)
+
+cmake_policy(POP)
 
 if(LIBUNWIND_FOUND)
     set(vt_feature_cmake_libunwind "1")
